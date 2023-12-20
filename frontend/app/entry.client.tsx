7e57b27caf68a2391a0b1ae894c6f577655698c6
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-12-20 13:53:06 -0330

    Add basic i18n support

--- frontend/app/entry.client.tsx
+++ frontend/app/entry.client.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default-member */
+
 /**
  * By default, Remix will handle hydrating your app on the client for you.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
@@ -8,11 +10,46 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
-});
+import i18n from 'i18next';
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
+import I18NextHttpBackend from 'i18next-http-backend';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+
+import { getNamespaces } from '~/utils/locale-utils';
+
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <I18nextProvider i18n={i18n}>
+          <RemixBrowser />
+        </I18nextProvider>
+      </StrictMode>
+    );
+  });
+}
+
+if (!i18n.isInitialized) {
+  i18n
+    .use(I18nextBrowserLanguageDetector)
+    .use(I18NextHttpBackend)
+    .use(initReactI18next)
+    .init({
+      debug: process.env.NODE_ENV === 'development',
+      detection: { order: ['path'] },
+      fallbackLng: 'en',
+      interpolation: { escapeValue: false },
+      ns: getNamespaces(window.__remixRouteModules),
+      supportedLngs: ['en', 'fr'],
+    })
+    .then(() => {
+      if (typeof requestIdleCallback === 'function') {
+        requestIdleCallback(hydrate);
+      } else {
+        // Safari doesn't support requestIdleCallback
+        // https://caniuse.com/requestidlecallback
+        setTimeout(hydrate, 1);
+      }
+    });
+}
---
+++ frontend/app/routes/$locale._index.tsx
@@ -0,0 +1,39 @@
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useTranslation } from "react-i18next";
+
+import { LanguageSwitcher } from "~/components/language-switcher";
+import { getLocale } from "~/utils/locale-utils";
+import { getFixedT } from "~/utils/locale-utils.server";
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  return [
+    { title: data?.pageTitle },
+  ];
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const locale = getLocale(request.url);
+  const { pathname } = new URL(request.url);
+
+  if (pathname !== '/' && locale === undefined) {
+    throw new Response('Not found', { status: 404, statusText: 'Not found' });
+  }
+
+  const t = await getFixedT(request, 'common');
+  return json({ pageTitle: t('index.page-title') });
+};
+
+export const handle = {
+  i18nNamespaces: ['common'],
+};
+
+export default function Index() {
+  const { t } = useTranslation('common');
+
+  return (
+    <>
+      <h1>{t('index.page-title')}</h1>
+      <LanguageSwitcher />
+    </>
+  );
+}
