780d5817f588833d2477c2e260eac72a4a63287d
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-01-05 12:38:50 -0330

    Prototyping application error handling. (#26)

---
+++ frontend/app/routes/error.tsx
@@ -0,0 +1,7 @@
+/**
+ * Error route used to test error handling.
+ * TODO :: GjB :: eventually remove
+ */
+export default function () {
+  throw new Error('Keyboard not responding. Press any key to continue.');
+}
