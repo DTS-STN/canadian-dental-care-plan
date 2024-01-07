d8e27ddc1ebf2ad52e5fd9624d3b161916eee683
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-01-07 09:00:22 -0330

    Fix broken tests

--- frontend/__tests__/loaders/index.test.tsx
+++ frontend/__tests__/loaders/index.test.tsx
@@ -3,13 +3,13 @@ import { loader } from '~/routes/_gcweb-application+/_index';
 describe('Loader for _index in english', () => {
   test('gets page title', async () => {
     let request = new Request('http://localhost:3000/?lang=en');
-    expect((await (await loader({ request, params: {}, context: {} })).json())?.pageTitle).toBe('Canadian Dental Care Plan');
+    expect((await (await loader({ request, params: {}, context: {} })).json())?.pageTitle).toBe('Home');
   });
 });
 
 describe('Loader for _index in french', () => {
   test('gets page title', async () => {
     let request = new Request('http://localhost:3000/?lang=fr');
-    expect((await (await loader({ request, params: {}, context: {} })).json())?.pageTitle).toBe('Régime canadien de soins dentaires');
+    expect((await (await loader({ request, params: {}, context: {} })).json())?.pageTitle).toBe('Accueil');
   });
 });
--- frontend/e2e/example.spec.ts
+++ frontend/e2e/example.spec.ts
@@ -2,11 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('should navigate to index', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('a[href="/en"]')).toBeVisible();
-});
 
-test('should navigate from index to home', async ({ page }) => {
-  await page.goto('/');
-  await page.getByText(/english/i).click();
-  await expect(page).toHaveURL('/en');
+  const screenId = await page.locator('span[property="identifier"]').textContent();
+  expect(screenId).toBe('CDCP-0001');
 });
