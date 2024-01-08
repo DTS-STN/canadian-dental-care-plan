7ce79f57eebbe5251a0d136c25f8ea520088865d
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-01-08 12:53:35 -0400

    TypeScript definitions for i18next

--- frontend/app/utils/locale-utils.ts
+++ frontend/app/utils/locale-utils.ts
@@ -1,9 +1,9 @@
-import { type UNSAFE_RouteModules as RouteModules } from '@remix-run/react';
-
-import { createInstance } from 'i18next';
+import { type Namespace, createInstance } from 'i18next';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import I18NextHttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+
+import { type RouteHandle } from '~/types';
 
 /**
  * Returns all namespaces required by the given routes by examining the route's i18nNamespaces handle property.
@@ -11,11 +11,14 @@ import { initReactI18next } from 'react-i18next';
  * @param {RouteModules} routeModules - Object containing route modules
  * @see https://remix.run/docs/en/main/route/handle
  */
-export function getNamespaces(routeModules: RouteModules) {
+export function getNamespaces(routeModules: Record<string, { handle?: unknown }>) {
   const namespaces = new Set(
     Object.values(routeModules)
-      .filter((route) => (route.handle as { i18nNamespaces: string })?.i18nNamespaces !== undefined)
-      .flatMap((route) => (route.handle as { i18nNamespaces: string }).i18nNamespaces),
+      .map((route) => route.handle)
+      .filter((handle): handle is RouteHandle => handle !== undefined)
+      .map((handle) => handle.i18nNamespaces)
+      .filter((i18nNamespaces): i18nNamespaces is Namespace => i18nNamespaces !== undefined)
+      .flatMap((i18nNamespaces) => i18nNamespaces),
   );
 
   return [...namespaces];
--- frontend/remix.env.d.ts
+++ frontend/remix.env.d.ts
@@ -1,2 +1,20 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node" />
+
+// i18next - import all namespaces (for the default language, only)
+import type common from '../public/locales/en/common.json';
+import type gcweb from '../public/locales/en/gcweb.json';
+
+/**
+ * @see https://www.i18next.com/overview/typescript
+ */
+declare module 'i18next' {
+  // Extend CustomTypeOptions
+  interface CustomTypeOptions {
+    // custom resources type
+    resources: {
+      common: typeof common;
+      gcweb: typeof gcweb;
+    };
+  }
+}
