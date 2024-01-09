2f50b5a8c5ef67130b2526d4bd896d8647915da7
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-01-09 10:58:09 -0330

    Extract language switcher to component (#37)

---
+++ frontend/app/components/language-switcher.tsx
@@ -0,0 +1,26 @@
+import { Link, type LinkProps, useHref } from '@remix-run/react';
+
+import { useTranslation } from 'react-i18next';
+
+import { useClientEnv } from '~/components/client-env';
+import { getAltLanguage } from '~/utils/locale-utils';
+
+export type LanguageSwitcherProps = Omit<LinkProps, 'to' | 'reloadDocument'>;
+
+/**
+ * Component that can be used to switch from one language to another.
+ * (ie: 'en' → 'fr'; 'fr' → 'en')
+ */
+export function LanguageSwitcher({ children, ...props }: LanguageSwitcherProps) {
+  const { i18n } = useTranslation(['gcweb']);
+
+  const langParam = useClientEnv('LANG_QUERY_PARAM') ?? 'lang';
+  const langValue = getAltLanguage(i18n.language);
+  const to = useHref(`?${langParam}=${langValue}`);
+
+  return (
+    <Link data-testid="language-switcher" reloadDocument to={to} {...props}>
+      {children}
+    </Link>
+  );
+}
