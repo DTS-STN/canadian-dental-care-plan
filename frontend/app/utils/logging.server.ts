6678452912f382689b6bb2c18717c1310f082817
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-01-12 15:23:14 -0330

    Early-init logger (#60)

--- frontend/app/utils/logging.server.ts
+++ frontend/app/utils/logging.server.ts
@@ -1,12 +1,18 @@
-import { type Logger, createLogger, format, transports } from 'winston';
+import winston, { type Logger, createLogger, format, transports } from 'winston';
+import { z } from 'zod';
 
-import { getEnv } from './env.server';
+// ['error', 'warn', 'info', 'verbose', 'debug', 'silly']
+const logLevels = Object.keys(winston.config.npm.levels);
+
+const logLevel = z
+  .string()
+  .refine((val) => logLevels.includes(val), { message: 'Invalid log level', path: ['process.env.LOG_LEVEL'] })
+  .default('info')
+  .parse(process.env.LOG_LEVEL);
 
 export const getLogger = (category: string): Logger => {
-  const { LOG_LEVEL } = getEnv();
-
   return createLogger({
-    level: LOG_LEVEL,
+    level: logLevel,
     format: format.combine(
       format.align(),
       format.timestamp(),
