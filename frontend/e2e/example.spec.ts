3782f7e00b4dd1c6a0152d4fad05d96d1e67c9f4
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-01-13 20:13:04 -0330

    Prettier format

--- frontend/e2e/example.spec.ts
+++ frontend/e2e/example.spec.ts
@@ -1,11 +1,10 @@
-import { expect, test } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
+import { expect, test } from '@playwright/test';
 
 test.describe('homepage', () => {
-
   test('should navigate to index', async ({ page }) => {
     await page.goto('/');
-  
+
     const screenId = await page.locator('span[property="identifier"]').textContent();
     expect(screenId).toBe('CDCP-0001');
   });
