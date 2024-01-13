9bb772e2d651fdf632eaf273f9f29a9157fde0bd
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-01-13 16:32:25 -0330

    Playing with types

--- frontend/app/routes/_gcweb-app._index.tsx
+++ frontend/app/routes/_gcweb-app._index.tsx
@@ -3,23 +3,30 @@ import { Link, useLoaderData } from '@remix-run/react';
 
 import { type Namespace } from 'i18next';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
 import { getFixedT } from '~/utils/locale-utils.server';
 
 const i18nNamespaces: Namespace = ['common'];
 
+const userSchema = z.object({ firstName: z.string(), lastName: z.string() });
+type User = z.infer<typeof userSchema>;
+
+async function getUser() {
+  const response = await fetch('https://api.example.com/user/00000000-0000-0000-0000-00000000000');
+  return userSchema.parse(await response.json()) as User;
+}
+
 export async function loader({ request }: LoaderFunctionArgs) {
   const t = await getFixedT(request, i18nNamespaces);
 
-  const res = await fetch('https://api.example.com/user/00000000-0000-0000-0000-00000000000');
-  const user = (await res.json()) as { firstName: string; lastName: string };
-
-  return json<LoaderFunctionData & { user: typeof user }>({
+  // TODO :: GjB :: figure out a cleaner way to type this
+  return json<LoaderFunctionData & { user: User }>({
     breadcrumbs: [{ label: t('common:index.breadcrumbs.home') }],
-    i18nNamespaces,
+    i18nNamespaces: [],
     pageIdentifier: 'CDCP-0001',
     pageTitle: t('common:index.page-title'),
-    user,
+    user: await getUser(),
   });
 }
 
