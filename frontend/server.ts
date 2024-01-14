07a454cc5efad98a1ff6a827c3024bf62b358377
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-01-14 10:16:37 -0330

    Clean up server.ts

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,160 +1,86 @@
-import { type RequestHandler, createRequestHandler } from '@remix-run/express';
+import { createRequestHandler } from '@remix-run/express';
 import { type ServerBuild, broadcastDevReady, installGlobals } from '@remix-run/node';
 
 import chokidar from 'chokidar';
 import compression from 'compression';
 import express from 'express';
-import getPort from 'get-port';
 import morgan from 'morgan';
 import fs from 'node:fs';
-import os from 'node:os';
 import path from 'node:path';
 import url from 'node:url';
 import sourceMapSupport from 'source-map-support';
-import { createLogger, format, transports } from 'winston';
 
-// TODO :: GjB :: figure out how to import the logger from ./app/utils/
-function formatCategory(category: string, size: number) {
-  const str = category.padStart(size);
-  return str.length <= size ? str : `...${str.slice(-size + 3)}`;
-}
+import { getEnv } from '~/utils/env.server';
+import { getLogger } from '~/utils/logging.server';
 
-// TODO :: GjB :: figure out how to import the logger from ./app/utils/
-const logger = createLogger({
-  format: format.combine(
-    format.timestamp(),
-    format.printf((info) => `${info.timestamp} ${info.level.toUpperCase().padStart(7)} --- [${formatCategory('server', 25)}]: ${info.message}`),
-  ),
-  transports: [new transports.Console()],
-});
-
-const BUILD_PATH = './build/index.js';
-const VERSION_PATH = './build/version.txt';
-const require = { cache: {} as Record<string, unknown> };
-
-//
-// Remix native server code below...
-// copied from: https://github.com/remix-run/remix/blob/remix@2.5.0/packages/remix-serve/cli.ts
-//
-
+// set the node environment to production if one hasn't yet been set
 process.env.NODE_ENV = process.env.NODE_ENV ?? 'production';
 
-sourceMapSupport.install({
-  retrieveSourceMap: function (source) {
-    let match = source.startsWith('file://');
-    if (match) {
-      let filePath = url.fileURLToPath(source);
-      let sourceMapPath = `${filePath}.map`;
-      if (fs.existsSync(sourceMapPath)) {
-        return {
-          url: source,
-          map: fs.readFileSync(sourceMapPath, 'utf8'),
-        };
-      }
-    }
-    return null;
-  },
-});
-installGlobals();
+const BUILD_PATH = path.resolve('./build/index.js');
+const VERSION_PATH = path.resolve('./build/version.txt');
 
+const log = getLogger('server');
+const { NODE_ENV, PORT } = getEnv();
+const isDevMode = NODE_ENV === 'development';
+
+installSourceMapSupport();
+installGlobals();
 run();
 
-function parseNumber(raw?: string) {
-  if (raw === undefined) return undefined;
-  let maybe = Number(raw);
-  if (Number.isNaN(maybe)) return undefined;
-  return maybe;
-}
-
 async function run() {
-  let port = parseNumber(process.env.PORT) ?? (await getPort({ port: 3000 }));
+  let build = await importBuild();
 
-  let buildPath = path.resolve(BUILD_PATH);
-  let versionPath = path.resolve(VERSION_PATH);
-
-  async function reimportServer() {
-    Object.keys(require.cache).forEach((key) => {
-      if (key.startsWith(buildPath)) {
-        delete require.cache[key];
-      }
-    });
-
-    let stat = fs.statSync(buildPath);
-
-    // use a timestamp query parameter to bust the import cache
-    return import(url.pathToFileURL(buildPath).href + '?t=' + stat.mtimeMs);
+  // function to reload the build when the server is updated
+  async function onBuildUpdate() {
+    build = await importBuild();
+    if (isDevMode) broadcastDevReady(build);
   }
 
-  function createDevRequestHandler(initialBuild: ServerBuild): RequestHandler {
-    let build = initialBuild;
-    async function handleServerUpdate() {
-      // 1. re-import the server build
-      build = await reimportServer();
-      // 2. tell Remix that this app server is now up-to-date and ready
-      broadcastDevReady(build);
-    }
-
-    chokidar.watch(versionPath, { ignoreInitial: true }).on('add', handleServerUpdate).on('change', handleServerUpdate);
-
-    // wrap request handler to make sure its recreated with the latest build for every request
-    return async (req, res, next) => {
-      try {
-        return createRequestHandler({
-          build,
-          mode: 'development',
-        })(req, res, next);
-      } catch (error) {
-        next(error);
-      }
-    };
+  // express listen event handler
+  function onServerListen() {
+    log.info(`Server listening on http://localhost:${PORT}/`);
+    if (isDevMode) broadcastDevReady(build);
   }
 
-  let build: ServerBuild = await reimportServer();
+  // fire onBuildUpdate() when build changes
+  // prettier-ignore
+  chokidar.watch(VERSION_PATH, { ignoreInitial: true })
+    .on('add', onBuildUpdate)
+    .on('change', onBuildUpdate);
 
-  let onListen = () => {
-    let address =
-      process.env.HOST ||
-      Object.values(os.networkInterfaces())
-        .flat()
-        .find((ip) => String(ip?.family).includes('4') && !ip?.internal)?.address;
-
-    if (!address) {
-      console.log(`[remix-serve] http://localhost:${port}`);
-    } else {
-      console.log(`[remix-serve] http://localhost:${port} (http://${address}:${port})`);
-    }
-    if (process.env.NODE_ENV === 'development') {
-      void broadcastDevReady(build);
-    }
-  };
-
-  let app = express();
+  const app = express();
   app.disable('x-powered-by');
   app.use(compression());
-  app.use(
-    build.publicPath,
-    express.static(build.assetsBuildDirectory, {
-      immutable: true,
-      maxAge: '1y',
-    }),
-  );
-
+  app.use(build.publicPath, express.static(build.assetsBuildDirectory, { immutable: true, maxAge: '1y' }));
   app.use(express.static('public', { maxAge: '1h' }));
-  app.use(morgan('tiny', { stream: { write: (str) => logger.info(str) } }));
+  app.use(morgan(isDevMode ? 'dev' : 'tiny', { stream: { write: (str) => log.info(str) } }));
+  app.all('*', createRequestHandler({ build, mode: NODE_ENV }));
 
-  app.all(
-    '*',
-    process.env.NODE_ENV === 'development'
-      ? createDevRequestHandler(build)
-      : createRequestHandler({
-          build,
-          mode: process.env.NODE_ENV,
-        }),
-  );
+  const server = app.listen(PORT, onServerListen);
+  ['SIGTERM', 'SIGINT'].forEach((signal) => process.once(signal, () => server?.close(console.error)));
+}
 
-  let server = process.env.HOST ? app.listen(port, process.env.HOST, onListen) : app.listen(port, onListen);
+function installSourceMapSupport() {
+  sourceMapSupport.install({
+    retrieveSourceMap: (source) => {
+      const match = source.startsWith('file://');
 
-  ['SIGTERM', 'SIGINT'].forEach((signal) => {
-    process.once(signal, () => server?.close(console.error));
+      if (match) {
+        const sourceMapPath = `${url.fileURLToPath(source)}.map`;
+
+        if (fs.existsSync(sourceMapPath)) {
+          return { url: source, map: fs.readFileSync(sourceMapPath, 'utf8') };
+        }
+      }
+
+      return null;
+    },
   });
+}
+
+function importBuild() {
+  const stat = fs.statSync(BUILD_PATH);
+  const buildUrl = url.pathToFileURL(BUILD_PATH).href;
+  // use a timestamp query parameter to bust the import cache
+  return import(`${buildUrl}?t=${stat.mtimeMs}`) as Promise<ServerBuild>;
 }
