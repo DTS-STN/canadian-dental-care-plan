049bbb3fb60af626e9d1acb2899bccd209a2bfde
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-01-16 21:41:16 -0330

    Add test for <ClientEnv> component

---
+++ frontend/__tests__/components/client-env.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+
+import { describe, expect, it } from 'vitest';
+
+import { ClientEnv } from '~/components/client-env';
+
+describe('<ClientEnv>', () => {
+  it('should render the script tag with the correct innerHTML', () => {
+    const nonce = 'https://open.spotify.com/artist/3BNDjSD67jJE4fhxX1b2OV?si=T1lAx_1fQe6axdydq43XEA';
+
+    const env = {
+      I18NEXT_DEBUG: true,
+      LANG_QUERY_PARAM: 'locale',
+      JAVASCRIPT_ENABLED: false,
+    };
+
+    const { container } = render(<ClientEnv env={env} nonce={nonce} />);
+
+    expect(container.innerHTML).toEqual(`<script nonce="${nonce}">window.env = ${JSON.stringify(env)}</script>`);
+  });
+});
--- frontend/app/components/client-env.tsx
+++ frontend/app/components/client-env.tsx
@@ -4,6 +4,10 @@ import { type PublicEnv } from '~/utils/env.server';
 
 export type ClientEnvProps = ScriptProps & { env: PublicEnv };
 
+/**
+ * The <ClientEnv> component is used to provide the browser access to the
+ * environment variables that are considered publicly safe (via `window.env`).
+ */
 export function ClientEnv({ env, ...props }: ClientEnvProps) {
-  return <script {...props} suppressHydrationWarning dangerouslySetInnerHTML={{ __html: `window.env = ${JSON.stringify(env)}` }} />;
+  return <script {...props} dangerouslySetInnerHTML={{ __html: `window.env = ${JSON.stringify(env)}` }} suppressHydrationWarning />;
 }
