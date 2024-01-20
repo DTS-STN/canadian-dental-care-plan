6383912b9633b36fdfc2f2417f4ead4874fa183d
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-01-20 12:16:14 -0400

    Add InputForm and InputLabel UI components

---
+++ frontend/app/components/input-field.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+
+import { InputLabel } from '~/components/input-label';
+
+export interface InputFieldProps extends Omit<React.ComponentProps<'input'>, 'aria-describedby' | 'aria-invalid' | 'aria-labelledby' | 'aria-required' | 'type'> {
+  errorMessage?: string;
+  helpMessage?: React.ReactNode;
+  helpMessageSecondary?: React.ReactNode;
+  id: string;
+  label: string;
+  name: string;
+  type?: 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'url';
+}
+
+export const InputField = (props: InputFieldProps) => {
+  const { errorMessage, className, helpMessage, helpMessageSecondary, id, label, required, type, ...restInputProps } = props;
+
+  const inputHelpMessageId = `input-${id}-help`;
+  const inputHelpMessageSecondaryId = `input-${id}-help-secondary`;
+  const inputWrapperId = `input-${id}`;
+  const inputLabelId = `input-${id}-label`;
+
+  function getAriaDescribedby() {
+    const ariaDescribedby = [];
+    if (helpMessage) ariaDescribedby.push(inputHelpMessageId);
+    if (helpMessageSecondary) ariaDescribedby.push(inputHelpMessageSecondaryId);
+    return ariaDescribedby.length > 0 ? ariaDescribedby.join(' ') : undefined;
+  }
+
+  return (
+    <div id={inputWrapperId} className="form-group" data-testid="input-field">
+      <InputLabel id={inputLabelId} htmlFor={id} required={required} errorMessage={errorMessage}>
+        {label}
+      </InputLabel>
+      {helpMessage && (
+        <div className="mb-1.5 max-w-prose text-base text-gray-600" id={inputHelpMessageId}>
+          {helpMessage}
+        </div>
+      )}
+      <input aria-describedby={getAriaDescribedby()} aria-invalid={!!errorMessage} aria-labelledby={inputLabelId} aria-required={required} className={clsx('form-control', className)} id={id} {...restInputProps} />
+      {helpMessageSecondary && (
+        <div className="mt-1.5 max-w-prose text-base text-gray-600" id={inputHelpMessageSecondaryId}>
+          {helpMessageSecondary}
+        </div>
+      )}
+    </div>
+  );
+};
--- frontend/app/routes/_gcweb-app.personal-information.address.edit.tsx
+++ frontend/app/routes/_gcweb-app.personal-information.address.edit.tsx
@@ -1,40 +1,65 @@
-import { Form, Link, useLoaderData } from '@remix-run/react';
+import { type ActionFunctionArgs, type LoaderFunctionArgs, json, redirect } from '@remix-run/node';
+import { Form, Link, useActionData, useLoaderData } from '@remix-run/react';
 
-import { type LoaderFunctionArgs, type ActionFunctionArgs, json, redirect } from '@remix-run/node';
+import { z } from 'zod';
 
-import { getUserService } from '~/services/user-service.server';
+import { InputField } from '~/components/input-field';
 import { getSessionService } from '~/services/session-service.server';
+import { getUserService } from '~/services/user-service.server';
 import { getEnv } from '~/utils/env.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const env = getEnv();
   const userService = getUserService({ env });
   const userId = await userService.getUserId();
-  return json({
-    user: await userService.getUserInfo(userId),
-  });
+  const userInfo = await userService.getUserInfo(userId);
+
+  if (!userInfo) {
+    throw new Response(null, { status: 404, statusText: 'User Info Not Found' });
+  }
+
+  return json({ userInfo });
 }
 
 export async function action({ request }: ActionFunctionArgs) {
+  const formDataSchema = z.object({
+    homeAddress: z.string().min(1),
+    mailingAddress: z.string().min(1),
+  });
+
   const formData = Object.fromEntries(await request.formData());
+  const parsedDataResult = formDataSchema.safeParse(formData);
+
+  if (!parsedDataResult.success) {
+    return json({
+      errors: parsedDataResult.error.format(),
+      formData: formData as Partial<z.infer<typeof formDataSchema>>,
+    });
+  }
 
   const { getSession, commitSession } = await getSessionService().createSessionStorage();
   const session = await getSession(request.headers.get('Cookie'));
+  session.set('newAddress', parsedDataResult.data);
 
-  session.set('newAddress', {homeAddress: formData.homeAddress.toString(), mailingAddress: formData.mailingAddress.toString()});
-  console.log(formData);
-
-  return redirect('/personal-information/address/confirm', { headers: {
-    "Set-Cookie": await commitSession(session),
-  }});
+  return redirect('/personal-information/address/confirm', {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    },
+  });
 }
 
 export default function ChangeAddress() {
-  const { user } = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+  const { userInfo } = useLoaderData<typeof loader>();
 
   const defaultValues = {
-    homeAddress: user?.homeAddress ?? '',
-    mailingAddress: user?.mailingAddress ?? ''
+    homeAddress: actionData?.formData.homeAddress ?? userInfo.homeAddress ?? '',
+    mailingAddress: actionData?.formData.mailingAddress ?? userInfo.mailingAddress ?? '',
+  };
+
+  const errorMessages = {
+    homeAddress: actionData?.errors.homeAddress?._errors[0],
+    mailingAddress: actionData?.errors.mailingAddress?._errors[0],
   };
 
   return (
@@ -43,27 +68,19 @@ export default function ChangeAddress() {
         Change address
       </h1>
       <Form method="post">
-        <div className="form-group">
-          <label htmlFor="home-address" className="required">
-            Home address <strong className="text-danger">(required)</strong>
-          </label>
-          <input id="home-address" name="homeAddress" className="form-control" size={60} defaultValue={defaultValues.homeAddress}/>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="mailing-address" className="required">
-            Mailing address <strong className="text-danger">(required)</strong>
-          </label>
-          <input id="mailing-address" name="mailingAddress" className="form-control" size={60} defaultValue={defaultValues.mailingAddress}/>
-        </div>
-
+        <InputField id="home-address" label="Home address" name="homeAddress" className="!w-full lg:!w-1/2" required defaultValue={defaultValues.homeAddress} errorMessage={errorMessages.homeAddress} />
+        <InputField id="mailing-address" label="Mailing address" name="mailingAddress" className="!w-full lg:!w-1/2" required defaultValue={defaultValues.mailingAddress} errorMessage={errorMessages.mailingAddress} />
         <div className="form-group">
           <ul className="list-inline lst-spcd">
             <li>
-              <button id="change-button" className="btn btn-primary btn-lg">Change</button>
+              <button id="change-button" className="btn btn-primary btn-lg">
+                Change
+              </button>
             </li>
             <li>
-              <Link id="cancel-button" to="/personal-information" className="btn btn-default btn-lg">Cancel</Link>
+              <Link id="cancel-button" to="/personal-information" className="btn btn-default btn-lg">
+                Cancel
+              </Link>
             </li>
           </ul>
         </div>
