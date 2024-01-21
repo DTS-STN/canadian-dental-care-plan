1d75ccd2bdba81861c858682818540a2d0c4e750
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-01-20 23:21:36 -0330

    Format project

--- frontend/app/routes/_gcweb-app.personal-information.address.confirm.tsx
+++ frontend/app/routes/_gcweb-app.personal-information.address.confirm.tsx
@@ -1,9 +1,8 @@
+import { type LoaderFunctionArgs, json } from '@remix-run/node';
 import { Form, Link, useLoaderData } from '@remix-run/react';
 
-import { type LoaderFunctionArgs, json } from '@remix-run/node';
-
-import { getUserService } from '~/services/user-service.server';
 import { getSessionService } from '~/services/session-service.server';
+import { getUserService } from '~/services/user-service.server';
 import { getEnv } from '~/utils/env.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -14,85 +13,81 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const session = await getSession(request.headers.get('Cookie'));
   return json({
     userInfo: await getUserInfo(userId),
-    newAddress: await session.get('newAddress')
+    newAddress: await session.get('newAddress'),
   });
-} 
+}
 
 export default function ConfirmAddress() {
-  const loaderData  = useLoaderData<typeof loader>();
+  const loaderData = useLoaderData<typeof loader>();
 
   return (
-	<>
-	  <h1 id="wb-cont" property="name">
+    <>
+      <h1 id="wb-cont" property="name">
         Change address
       </h1>
       <p className="h3">Confirm</p>
       <Form method="post">
         <strong>Change of Home Address</strong>
         <div className="row mrgn-tp-sm">
-			<div className="col-sm-6">
-			    <section className="panel panel-info">
-				    <header className="panel-heading">
-					    <h3 className="panel-title">
-							From:
-						</h3>
-					</header>
-					<div className="panel-body">
-						<p>{loaderData.userInfo?.homeAddress}</p>
-					</div>
-				</section>
-			    </div>
+          <div className="col-sm-6">
+            <section className="panel panel-info">
+              <header className="panel-heading">
+                <h3 className="panel-title">From:</h3>
+              </header>
+              <div className="panel-body">
+                <p>{loaderData.userInfo?.homeAddress}</p>
+              </div>
+            </section>
+          </div>
 
-			<div className="col-sm-6">
-				<section className="panel panel-info">
-					<header className="panel-heading">
-						<h3 className="panel-title">
-							To:
-						</h3>
-					</header>
-					<div className="panel-body">
-						<p>{loaderData.newAddress?.homeAddress}</p>
-					</div>
-				</section>
-			</div>          
-		</div>
+          <div className="col-sm-6">
+            <section className="panel panel-info">
+              <header className="panel-heading">
+                <h3 className="panel-title">To:</h3>
+              </header>
+              <div className="panel-body">
+                <p>{loaderData.newAddress?.homeAddress}</p>
+              </div>
+            </section>
+          </div>
+        </div>
 
         <strong>Change of Mailing Address</strong>
         <div className="row mrgn-tp-sm">
-			<div className="col-sm-6">
-			    <section className="panel panel-info">
-				    <header className="panel-heading">
-					    <h3 className="panel-title">
-							From:
-					    </h3>
-				    </header>
-				    <div className="panel-body">
-                		<p>{loaderData.userInfo?.mailingAddress}</p>
-					</div>
-				</section>
-			</div>
+          <div className="col-sm-6">
+            <section className="panel panel-info">
+              <header className="panel-heading">
+                <h3 className="panel-title">From:</h3>
+              </header>
+              <div className="panel-body">
+                <p>{loaderData.userInfo?.mailingAddress}</p>
+              </div>
+            </section>
+          </div>
 
-			<div className="col-sm-6">
-			    <section className="panel panel-info">
-				    <header className="panel-heading">
-					    <h3 className="panel-title">
-							To:
-					    </h3>
-				    </header>
-				    <div className="panel-body">
-            		    <p>{loaderData.newAddress?.mailingAddress}</p>
-					</div>
-				</section>
-			</div>          
-		</div>
+          <div className="col-sm-6">
+            <section className="panel panel-info">
+              <header className="panel-heading">
+                <h3 className="panel-title">To:</h3>
+              </header>
+              <div className="panel-body">
+                <p>{loaderData.newAddress?.mailingAddress}</p>
+              </div>
+            </section>
+          </div>
+        </div>
 
         <div className="form-group">
           <ul className="list-inline lst-spcd">
             <li>
-              <button id="confirm-button" className="btn btn-primary btn-lg">Confirm</button>
+              <button id="confirm-button" className="btn btn-primary btn-lg">
+                Confirm
+              </button>
             </li>
             <li>
-              <Link id="cancel-button" to="/personal-information/address/edit" className="btn btn-default btn-lg">Cancel</Link>
+              <Link id="cancel-button" to="/personal-information/address/edit" className="btn btn-default btn-lg">
+                Cancel
+              </Link>
             </li>
           </ul>
         </div>
--- frontend/app/routes/_gcweb-app.update-phone-number.tsx
+++ frontend/app/routes/_gcweb-app.update-phone-number.tsx
@@ -2,9 +2,9 @@ import { type ActionFunctionArgs, type LoaderFunctionArgs, json, redirect } from
 import { Form, Link, useActionData, useLoaderData } from '@remix-run/react';
 
 import { z } from 'zod';
+
 import { PhoneNumber } from '~/components/phone-number';
 import { getSessionService } from '~/services/session-service.server';
-
 import { getUserService } from '~/services/user-service.server';
 import { getEnv } from '~/utils/env.server';
 
@@ -18,7 +18,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-
   const isPhoneNumber = (val: string) => val.match(/\([0-9]{3}\) [0-9]{3}-[0-9]{4}/);
 
   const formDataSchema = z.object({
@@ -37,16 +36,17 @@ export async function action({ request }: ActionFunctionArgs) {
 
   const { getSession, commitSession } = await getSessionService().createSessionStorage();
   const session = await getSession(request.headers.get('Cookie'));
-  
-  session.set('newPhoneNumber', parsedDataResult.data.phoneNumber)
 
-  return redirect('/update-phone-number-confirm', { headers: {
-    "Set-Cookie": await commitSession(session),
-  }});
+  session.set('newPhoneNumber', parsedDataResult.data.phoneNumber);
+
+  return redirect('/update-phone-number-confirm', {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    },
+  });
 }
 
 export default function UpdateInfo() {
-
   const actionData = useActionData<typeof action>();
   const loaderData = useLoaderData<typeof loader>();
   const fieldErrors = actionData?.errors;
@@ -59,11 +59,13 @@ export default function UpdateInfo() {
       <p>Please update your phone number below.</p>
       <Form method="post">
         <div className="form-group">
-          <PhoneNumber editMode phoneNumber={actionData?.formData.phoneNumber ?? loaderData.userInfo?.phoneNumber} fieldErrors={fieldErrors?.phoneNumber?._errors}/>
+          <PhoneNumber editMode phoneNumber={actionData?.formData.phoneNumber ?? loaderData.userInfo?.phoneNumber} fieldErrors={fieldErrors?.phoneNumber?._errors} />
         </div>
         <div className="form-group">
           <button className="btn btn-primary btn-lg mrgn-rght-sm">Save</button>
-          <Link id="cancelButton" to="/update-info" className="btn btn-default btn-lg">Cancel</Link> 
+          <Link id="cancelButton" to="/update-info" className="btn btn-default btn-lg">
+            Cancel
+          </Link>
         </div>
       </Form>
     </>
