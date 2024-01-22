51ab9c41c245c7cf39b07caebe8bcc5b2011c945
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-01-22 09:14:23 -0330

    Uniqifying input field test ids (#122)

--- frontend/__tests__/components/input-label.test.tsx
+++ frontend/__tests__/components/input-label.test.tsx
@@ -2,58 +2,60 @@ import { getByTestId, render, screen } from '@testing-library/react';
 
 import { afterEach, describe, expect, it } from 'vitest';
 
-//import { axe, toHaveNoViolations } from 'jest-axe';
 import { InputLabel } from '~/components/input-label';
 
+//import { axe, toHaveNoViolations } from 'jest-axe';
 //expect.extend(toHaveNoViolations);
 
 vi.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: string) => key }),
+  useTranslation: () => ({
+    t: (key: string) => key,
+  }),
 }));
 
-describe('InputLabel', async () => {
+describe('InputLabel', () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
   it('should render', () => {
-    render(<InputLabel id="input-label">input label</InputLabel>);
+    render(<InputLabel id="id">input label</InputLabel>);
 
-    const actualLabel = screen.getByTestId('input-label');
+    const actualLabel = screen.getByTestId('input-label-id');
     expect(actualLabel).toBeInTheDocument();
-    expect(actualLabel.id).toBe('input-label');
+    expect(actualLabel.id).toBe('id');
 
-    expect(() => getByTestId(actualLabel, 'input-label-required')).toThrow();
-    expect(() => getByTestId(actualLabel, 'input-label-error-message')).toThrow();
+    expect(() => getByTestId(actualLabel, 'input-label-required-id')).toThrow();
+    expect(() => getByTestId(actualLabel, 'input-label-error-message-id')).toThrow();
   });
 
   it('should render with required text', () => {
     render(
-      <InputLabel id="input-label" required>
+      <InputLabel id="id" required>
         input label
       </InputLabel>,
     );
 
-    const actualLabel = screen.getByTestId('input-label');
+    const actualLabel = screen.getByTestId('input-label-id');
     expect(actualLabel).toBeInTheDocument();
 
-    const actualRequired = getByTestId(actualLabel, 'input-label-required');
-    expect(actualRequired.textContent).toBe(' (input-label.required)');
+    const actualRequired = getByTestId(actualLabel, 'input-label-required-id');
+    expect(actualRequired.textContent).toBe('(input-label.required)');
 
-    expect(() => getByTestId(actualLabel, 'input-label-error-message')).toThrow();
+    expect(() => getByTestId(actualLabel, 'input-label-error-message-id')).toThrow();
   });
 
   it('should render with error message', () => {
     render(
-      <InputLabel id="input-label" errorMessage="error message">
+      <InputLabel id="id" errorMessage="error message">
         input label
       </InputLabel>,
     );
 
-    const actualLabel = screen.getByTestId('input-label');
+    const actualLabel = screen.getByTestId('input-label-id');
     expect(actualLabel).toBeInTheDocument();
 
-    const actualErrorMessage = getByTestId(actualLabel, 'input-label-error-message');
+    const actualErrorMessage = getByTestId(actualLabel, 'input-label-error-message-id');
     expect(actualErrorMessage).toBeInTheDocument();
     expect(actualErrorMessage.textContent).toBe('error message');
 
