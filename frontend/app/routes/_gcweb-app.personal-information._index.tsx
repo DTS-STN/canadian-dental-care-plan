12d3707a1b6db00b1aaa794daf2aa891550a705e
Frank <48450599+Fbasham@users.noreply.github.com>
2024-01-22 12:13:54 -0500

    add preferred language view (#125)

--- frontend/app/routes/_gcweb-app.personal-information._index.tsx
+++ frontend/app/routes/_gcweb-app.personal-information._index.tsx
@@ -1,10 +1,12 @@
 import { type LoaderFunctionArgs, json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
+
+import { useTranslation } from 'react-i18next';
 
 import { userService } from '~/services/user-service.server';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 
-const i18nNamespaces = getTypedI18nNamespaces('common');
+const i18nNamespaces = getTypedI18nNamespaces('common', 'personal-information');
 
 export const handle = {
   breadcrumbs: [{ labelI18nKey: 'common:personal-information.breadcrumbs.home', to: '/' }, { labelI18nKey: 'common:personal-information.page-title' }],
@@ -22,10 +24,29 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 export default function PersonalInformationIndex() {
   const { user } = useLoaderData<typeof loader>();
+  const { t } = useTranslation(i18nNamespaces);
   return (
     <>
-      <p>Personal information! 😎</p>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <h1 id="wb-cont" property="name">
+        {t('common:personal-information.page-title')}
+      </h1>
+      <p>{t('personal-information:index.on-file')}</p>
+      <dl>
+        <dt>{t('personal-information:index.first-name')}</dt>
+        <dd>{user?.firstName}</dd>
+        <dt>{t('personal-information:index.last-name')}</dt>
+        <dd>{user?.lastName}</dd>
+        <dt>{t('personal-information:index.phone-number')}</dt>
+        <dd>{user?.phoneNumber}</dd>
+        <dt>{t('personal-information:index.home-address')}</dt>
+        <dd>{user?.homeAddress}</dd>
+        <dt>{t('personal-information:index.mailing-address')}</dt>
+        <dd>{user?.mailingAddress}</dd>
+        <dt>
+          <Link to="/personal-information/preferred-language">{t('personal-information:index.preferred-language')}</Link>
+        </dt>
+        <dd>{user?.preferredLanguage}</dd>
+      </dl>
     </>
   );
 }
---
+++ frontend/app/routes/_gcweb-app.personal-information.preferred-language.tsx
@@ -0,0 +1,44 @@
+import { type LoaderFunctionArgs, json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import { useTranslation } from 'react-i18next';
+
+import { userService } from '~/services/user-service.server';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+
+const i18nNamespaces = getTypedI18nNamespaces('common', 'personal-information');
+
+export const handle = {
+  breadcrumbs: [{ labelI18nKey: 'common:personal-information.breadcrumbs.home', to: '/' }, { labelI18nKey: 'common:personal-information.page-title', to: '/personal-information' }, { labelI18nKey: 'common:preferred-language.page-title' }],
+  i18nNamespaces,
+  pageIdentifier: 'CDCP-0004',
+  pageTitleI18nKey: 'common:preferred-language.page-title',
+} as const satisfies RouteHandleData;
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const userId = await userService.getUserId();
+  const userInfo = await userService.getUserInfo(userId);
+
+  if (userInfo === null) {
+    throw new Response(null, { status: 404 });
+  }
+
+  return json({ user: userInfo });
+}
+
+export default function PersonalInformationIndex() {
+  const { user } = useLoaderData<typeof loader>();
+  const { t } = useTranslation(i18nNamespaces);
+  return (
+    <>
+      <h1 id="wb-cont" property="name">
+        {t('common:preferred-language.page-title')}
+      </h1>
+      <p>{t('personal-information:preferred-language.on-file')}</p>
+      <dl>
+        <dt>{t('personal-information:preferred-language.language')}</dt>
+        <dd>{user.preferredLanguage}</dd>
+      </dl>
+    </>
+  );
+}
