951a2f0fc4989250884a190466fd7dba58d7393d
Olivier Roy <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-01-23 08:17:27 -0500

    AB#2778 removed capital letters in urls

--- frontend/app/routes/_gcweb-app._index.tsx
+++ frontend/app/routes/_gcweb-app._index.tsx
@@ -44,19 +44,19 @@ export default function Index() {
         <LandingPageLink title="Update Your Information" description="In this section, you can your personal information." to="/update-info">
           Update your information
         </LandingPageLink>
-        <LandingPageLink title="Upload Document" description="In this section, you can your upload documents." to="/Upload-Document">
+        <LandingPageLink title="Upload Document" description="In this section, you can your upload documents." to="/upload-document">
           Upload document for CDCP
         </LandingPageLink>
-        <LandingPageLink title="Personal Information" description="In this section, you can see your personal information." to="/Personal-Information">
+        <LandingPageLink title="Personal Information" description="In this section, you can see your personal information." to="/personal-information">
           Personal information
         </LandingPageLink>
-        <LandingPageLink title="View my Letters" description="In this section, you can see any letters that were sent for you." to="/View-Letters">
+        <LandingPageLink title="View my Letters" description="In this section, you can see any letters that were sent for you." to="/view-letters">
           View My Letters
         </LandingPageLink>
-        <LandingPageLink title="View my messages" description="In this section, you can see any messages that were sent to you." to="/Messages">
+        <LandingPageLink title="View my messages" description="In this section, you can see any messages that were sent to you." to="/messages">
           View My CDCP Messages
         </LandingPageLink>
-        <LandingPageLink title="CDCP alerts" description="In this section, you can subscribe to the email alerts for CDCO, Any time you have a new message, you will be alerted." to="/Alert-me">
+        <LandingPageLink title="CDCP alerts" description="In this section, you can subscribe to the email alerts for CDCO, Any time you have a new message, you will be alerted." to="/alert-me">
           Subscribe to CDCP email alerts
         </LandingPageLink>
       </div>
