0aa72c19dc70156b38929331d9071e67937c524a
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2024-01-24 16:16:43 -0500

    Added missing unit test

--- frontend/__tests__/actions/confirm-phone-number.test.tsx
+++ frontend/__tests__/actions/confirm-phone-number.test.tsx
@@ -15,13 +15,37 @@ vi.mock('~/services/user-service.server.ts', () => ({
       phoneNumber: '(555) 555-5555',
       preferredLanguage: 'fr',
     }),
+    updateUserInfo: vi.fn(),
   },
 }));
+
+vi.mock('~/services/session-service.server.ts', () => ({
+  sessionService: {
+    getSession: vi.fn().mockReturnValue({
+      has: vi.fn().mockReturnValueOnce(true),
+      get: vi.fn(),
+      unset: vi.fn(),
+    }),
+    commitSession: vi.fn(),
+  },
+}));
+
 
 describe('Confirm Phone Number Action', () => {
   afterEach(() => {
     vi.clearAllMocks();
     vi.resetModules();
+  });
+
+  it('Should redirect to the success page when newPhoneNumber is present', async () => {
+    let request = new Request('http://localhost:3000/personal-information/phone-number/confirm', {
+      method: 'POST',
+    });
+
+    const response = await action({ request, context: {}, params: {} });
+
+    expect(response.status).toBe(302);
+    expect(response).toEqual(redirect('/personal-information/phone-number/success', { headers: { 'Set-Cookie': 'undefined' }}));
   });
 
   it('Should redirect to homepage page when newPhoneNumber is missing', async () => {
