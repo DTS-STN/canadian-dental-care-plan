684a1bc1fee533a15e24ebdab0fec78ca7cd1527
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2024-01-24 13:33:58 -0500

    Updated associated test

--- frontend/__tests__/actions/confirm-phone-number.test.tsx
+++ frontend/__tests__/actions/confirm-phone-number.test.tsx
@@ -4,13 +4,27 @@ import { describe, expect, it, vi } from 'vitest';
 
 import { action } from '~/routes/_gcweb-app.personal-information.phone-number.confirm';
 
+vi.mock('~/services/user-service.server.ts', () => ({
+  userService: {
+    getUserId: vi.fn().mockReturnValue('00000000-0000-0000-0000-000000000000'),
+    getUserInfo: vi.fn().mockReturnValue({
+      firstName: 'John',
+      homeAddress: '123 Home Street',
+      lastName: 'Maverick',
+      mailingAddress: '123 Mailing Street',
+      phoneNumber: '(555) 555-5555',
+      preferredLanguage: 'fr',
+    }),
+  },
+}));
+
 describe('Confirm Phone Number Action', () => {
   afterEach(() => {
     vi.clearAllMocks();
     vi.resetModules();
   });
 
-  it('Should redirect', async () => {
+  it('Should redirect to homepage page when newPhoneNumber is missing', async () => {
     let request = new Request('http://localhost:3000/personal-information/phone-number/confirm', {
       method: 'POST',
     });
@@ -18,6 +32,6 @@ describe('Confirm Phone Number Action', () => {
     const response = await action({ request, context: {}, params: {} });
 
     expect(response.status).toBe(302);
-    expect(response).toEqual(redirect('/personal-information/phone-number/success'));
+    expect(response).toEqual(redirect('/'));
   });
 });
