b83495027e3176eba742388dcea235b10579d21b
Sébastien Comeau <114004123+sebastien-comeau@users.noreply.github.com>
2024-01-25 12:20:37 -0400

    Add card in progress labels (#168)

--- frontend/app/components/landing-page-link.tsx
+++ frontend/app/components/landing-page-link.tsx
@@ -1,17 +1,18 @@
-import { type ReactNode } from 'react';
+import type { ComponentProps, ReactNode } from 'react';
 
 import { Link } from '@remix-run/react';
 
-export interface LandingPageLinkProps extends Omit<React.ComponentProps<typeof Link>, 'children'> {
+export interface LandingPageLinkProps extends Omit<ComponentProps<typeof Link>, 'children' | 'title'> {
   children: ReactNode;
   description: string;
-  title: string;
+  title: ReactNode;
 }
+
 export function LandingPageLink(props: LandingPageLinkProps) {
   const { className, children, description, title, ...linkProps } = props;
   return (
     <div className="rounded-lg border p-6 shadow">
-      <h2 className="mt-0">{title}</h2>
+      <h2 className="h3 !mt-0">{title}</h2>
       <p>{description}</p>
       <Link {...linkProps}>{children}</Link>
     </div>
