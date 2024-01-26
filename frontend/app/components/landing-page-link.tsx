d01789558e2d792beecbf7ec6763889944464b33
olivierroyESDC <78967586+olivierroyESDC@users.noreply.github.com>
2024-01-26 15:51:54 -0500

    added a small change to the styling of the headers for the index page in the css (#169)

--- frontend/app/components/landing-page-link.tsx
+++ frontend/app/components/landing-page-link.tsx
@@ -11,7 +11,7 @@ export interface LandingPageLinkProps extends Omit<ComponentProps<typeof Link>, 
 export function LandingPageLink(props: LandingPageLinkProps) {
   const { className, children, description, title, ...linkProps } = props;
   return (
-    <div className="rounded-lg border p-6 shadow">
+    <div className="header-bg rounded-lg border p-6 shadow">
       <h2 className="h3 !mt-0">{title}</h2>
       <p>{description}</p>
       <Link {...linkProps}>{children}</Link>
