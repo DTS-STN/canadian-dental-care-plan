e531bdbd47b81ae1cfb67bcfa7f4200f44cf30a7
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2024-01-29 08:30:55 -0500

    Adding (required) field to edit phone form and redirecting back to /personal-information in the confirm page

--- frontend/app/routes/_gcweb-app.personal-information.phone-number.confirm.tsx
+++ frontend/app/routes/_gcweb-app.personal-information.phone-number.confirm.tsx
@@ -70,7 +70,7 @@ export default function PhoneNumberConfirm() {
         </div>
         <div className="form-group">
           <button className="btn btn-primary btn-lg mrgn-rght-sm">{t('personal-information:phone-number.confirm.button.confirm')}</button>
-          <Link id="cancelButton" to="/update-info" className="btn btn-default btn-lg">
+          <Link id="cancelButton" to="/personal-information" className="btn btn-default btn-lg">
             {t('personal-information:phone-number.confirm.button.cancel')}
           </Link>
         </div>
