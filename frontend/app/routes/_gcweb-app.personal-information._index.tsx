bc8871a83459780246092d499a2c751124e0f6ec
frank basham <frankabasham@gmail.com>
2024-01-30 07:12:51 -0500

    consolidate personal information cards

--- frontend/app/routes/_gcweb-app.personal-information._index.tsx
+++ frontend/app/routes/_gcweb-app.personal-information._index.tsx
@@ -41,33 +41,8 @@ export default function PersonalInformationIndex() {
       </h1>
       <p>{t('personal-information:index.on-file')}</p>
       <div className="grid gap-6 md:grid-cols-2">
-        <PersonalInformationSection title={t('personal-information:index.first-name')} icon="glyphicon-user">
-          {user?.firstName}
-        </PersonalInformationSection>
-        <PersonalInformationSection title={t('personal-information:index.last-name')} icon="glyphicon-user">
-          {user?.lastName}
-        </PersonalInformationSection>
-        <PersonalInformationSection
-          footer={
-            <Link id="change-home-address-button" className="btn btn-primary btn-lg" to="/personal-information/address/edit">
-              {t('personal-information:index.change-home-address')}
-            </Link>
-          }
-          title={t('personal-information:index.home-address')}
-          icon="glyphicon-map-marker"
-        >
-          {user?.homeAddress}
-        </PersonalInformationSection>
-        <PersonalInformationSection
-          footer={
-            <Link id="change-mailing-address-button" className="btn btn-primary btn-lg" to="/personal-information/address/edit">
-              {t('personal-information:index.change-mailing-address')}
-            </Link>
-          }
-          title={t('personal-information:index.mailing-address')}
-          icon="glyphicon-map-marker"
-        >
-          {user?.mailingAddress}
+        <PersonalInformationSection title={t('personal-information:index.full-name')} icon="glyphicon-user">
+          {`${user.firstName} ${user.lastName}`}
         </PersonalInformationSection>
         <PersonalInformationSection
           footer={
@@ -82,6 +57,22 @@ export default function PersonalInformationIndex() {
         </PersonalInformationSection>
         <PersonalInformationSection
           footer={
+            <Link id="change-home-address-button" className="btn btn-primary btn-lg" to="/personal-information/address/edit">
+              {t('personal-information:index.change-addresses')}
+            </Link>
+          }
+          title={t('personal-information:index.addresses')}
+          icon="glyphicon-map-marker"
+        >
+          <dl>
+            <dt>{t('personal-information:index.home-address')}</dt>
+            <dd>{user.homeAddress}</dd>
+            <dt>{t('personal-information:index.mailing-address')}</dt>
+            <dd>{user.mailingAddress}</dd>
+          </dl>
+        </PersonalInformationSection>
+        <PersonalInformationSection
+          footer={
             <Link id="change-phone-number-button" className="btn btn-primary btn-lg" to="/personal-information/phone-number/edit">
               {t('personal-information:index.change-phone-number')}
             </Link>
@@ -89,7 +80,7 @@ export default function PersonalInformationIndex() {
           title={t('personal-information:index.phone-number')}
           icon="glyphicon-earphone"
         >
-          {user?.phoneNumber}
+          {user.phoneNumber}
         </PersonalInformationSection>
       </div>
     </>
