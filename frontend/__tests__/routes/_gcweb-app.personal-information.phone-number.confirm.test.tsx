076c19421e6b94e13daedf44c8ef37822fbdf92a
AdamP-HRSDC-RHDCC <155585182+AdamP-HRSDC-RHDCC@users.noreply.github.com>
2024-01-31 05:50:55 -0500

    Added loader tests for phone number routes (#229)

--- frontend/__tests__/routes/_gcweb-app.personal-information.phone-number.confirm.test.tsx
+++ frontend/__tests__/routes/_gcweb-app.personal-information.phone-number.confirm.test.tsx
@@ -2,19 +2,14 @@ import { redirect } from '@remix-run/node';
 
 import { describe, expect, it, vi } from 'vitest';
 
-import { action } from '~/routes/_gcweb-app.personal-information.phone-number.confirm';
+import { action, loader } from '~/routes/_gcweb-app.personal-information.phone-number.confirm';
+import { sessionService } from '~/services/session-service.server';
+import { userService } from '~/services/user-service.server';
 
 vi.mock('~/services/user-service.server.ts', () => ({
   userService: {
-    getUserId: vi.fn().mockReturnValue('00000000-0000-0000-0000-000000000000'),
-    getUserInfo: vi.fn().mockReturnValue({
-      firstName: 'John',
-      homeAddress: '123 Home Street',
-      lastName: 'Maverick',
-      mailingAddress: '123 Mailing Street',
-      phoneNumber: '(555) 555-5555',
-      preferredLanguage: 'fr',
-    }),
+    getUserId: vi.fn().mockReturnValue('some-id'),
+    getUserInfo: vi.fn(),
     updateUserInfo: vi.fn(),
   },
 }));
@@ -22,7 +17,7 @@ vi.mock('~/services/user-service.server.ts', () => ({
 vi.mock('~/services/session-service.server.ts', () => ({
   sessionService: {
     getSession: vi.fn().mockReturnValue({
-      has: vi.fn().mockReturnValueOnce(true),
+      has: vi.fn(),
       get: vi.fn(),
       unset: vi.fn(),
     }),
@@ -36,8 +31,31 @@ describe('_gcweb-app.personal-information.phone-number.confirm', () => {
     vi.resetModules();
   });
 
+  describe('loader()', () => {
+    it('should return userInfo and newPhoneNumber', async () => {
+      vi.mocked(userService.getUserInfo).mockResolvedValue({ id: 'some-id', phoneNumber: '(111) 222-3333' });
+      vi.mocked((await sessionService.getSession()).get).mockResolvedValue('(444) 555-6666');
+      vi.mocked((await sessionService.getSession()).has).mockResolvedValueOnce(true);
+
+      const response = await loader({
+        request: new Request('http://localhost:3000/personal-information/phone-number/confirm'),
+        context: {},
+        params: {},
+      });
+
+      const data = await response.json();
+
+      expect(data).toEqual({
+        userInfo: { id: 'some-id', phoneNumber: '(111) 222-3333' },
+        newPhoneNumber: '(444) 555-6666',
+      });
+    });
+  });
+
   describe('action()', () => {
-    it('Should redirect to the personal information on success', async () => {
+    it('should redirect to the personal information on success', async () => {
+      vi.mocked((await sessionService.getSession()).has).mockResolvedValueOnce(true);
+
       let request = new Request('http://localhost:3000/personal-information/phone-number/confirm', {
         method: 'POST',
       });
@@ -48,7 +66,8 @@ describe('_gcweb-app.personal-information.phone-number.confirm', () => {
       expect(response).toEqual(redirect('/personal-information', { headers: { 'Set-Cookie': 'undefined' } }));
     });
 
-    it('Should redirect to homepage page when newPhoneNumber is missing', async () => {
+    it('should redirect to homepage page when newPhoneNumber is missing', async () => {
+
       let request = new Request('http://localhost:3000/personal-information/phone-number/confirm', {
         method: 'POST',
       });
