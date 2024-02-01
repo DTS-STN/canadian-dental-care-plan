8dcd25b5487d592c10c2647a0b10bb19807d8fa6
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-02-01 10:13:37 -0330

    AB#2659 -- refactoring (#237)

--- frontend/app/routes/auth.login.$providerId.tsx
+++ frontend/app/routes/auth.login.$providerId.tsx
@@ -1,8 +1,7 @@
 import { type LoaderFunctionArgs, redirect } from '@remix-run/node';
 
-import { raoidcService } from '~/services/raoidc-service.server';
+import { getRaoidcService } from '~/services/raoidc-service.server';
 import { sessionService } from '~/services/session-service.server';
-import { getLogger } from '~/utils/logging.server';
 import { generateCallbackUri } from '~/utils/raoidc-utils.server';
 
 /**
@@ -10,16 +9,10 @@ import { generateCallbackUri } from '~/utils/raoidc-utils.server';
  * back to the browser to initiate the login process.
  */
 export async function loader({ request, params }: LoaderFunctionArgs) {
-  const log = getLogger('auth.login');
-
-  if (!raoidcService) {
-    log.warn('Call to /auth/login/%s but authentication is disabled', params.providerId);
-    // TODO :: GjB :: handle this better than just throwing an error
-    throw new Error('RAOIDC service not configured');
-  }
+  const raoidcService = await getRaoidcService();
 
   const redirectUri = generateCallbackUri(new URL(request.url).origin, params.providerId);
-  const { authUrl, codeVerifier, state } = raoidcService.generateAuthRequest(redirectUri);
+  const { authUrl, codeVerifier, state } = raoidcService.generateSigninRequest(redirectUri);
 
   // codeVerifier and state will have to be validated in the callback handler
   const session = await sessionService.getSession(request.headers.get('Cookie'));
