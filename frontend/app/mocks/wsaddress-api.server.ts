997b50e487bb0696dc510145463bc93fcd785e6d
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-02-07 19:52:23 -0330

    Move WSAddress API mocks to their own file (#286)

---
+++ frontend/app/mocks/wsaddress-api.server.ts
@@ -0,0 +1,68 @@
+import { HttpResponse, http } from 'msw';
+
+import { getLogger } from '~/utils/logging.server';
+
+const log = getLogger('wsaddress-api.server');
+
+/**
+ * Server-side MSW mocks for the WSAddress API.
+ */
+export function getWSAddressApiMockHandlers() {
+  log.info('Initializing WSAddress API mock handlers');
+
+  return [
+    //
+    // Handler for POST requests to WSAddress parse service
+    //
+    http.post('https://api.example.com/address/parse', ({ params, request }) => {
+      log.debug('Handling request for [%s]', request.url);
+      return HttpResponse.json({
+        responseType: 'CA',
+        addressLine: '23 CORONATION ST',
+        city: 'ST JOHNS',
+        province: 'NL',
+        postalCode: 'A1C5B9',
+        streetNumberSuffix: 'streetNumberSuffix',
+        streetDirection: 'streetDirection',
+        unitType: 'unitType',
+        unitNumber: '000',
+        serviceAreaName: 'serviceAreaName',
+        serviceAreaType: 'serviceAreaType',
+        serviceAreaQualifier: '',
+        cityLong: 'cityLong',
+        cityShort: 'cityShort',
+        deliveryInformation: 'deliveryInformation',
+        extraInformation: 'extraInformation',
+        statusCode: 'Valid',
+        canadaPostInformation: [],
+        message: 'message',
+        addressType: 'Urban',
+        streetNumber: '23',
+        streetName: 'CORONATION',
+        streetType: 'ST',
+        serviceType: 'Unknown',
+        serviceNumber: '000',
+        country: 'CAN',
+        warnings: '',
+        functionalMessages: [{ action: 'action', message: 'message' }],
+      });
+    }),
+
+    //
+    // Handler for POST requests to WSAddress validate service
+    //
+    http.post('https://api.example.com/address/validate', ({ params, request }) => {
+      log.debug('Handling request for [%s]', request.url);
+      return HttpResponse.json({
+        responseType: 'CA',
+        statusCode: 'Valid',
+        functionalMessages: [
+          { action: 'OriginalInput', message: '111 WELLINGTON ST   OTTAWA   ON   K1A0A4   CAN' },
+          { action: 'Information', message: 'Dept = SENAT   Branch = SENAT   Lang = F' },
+        ],
+        message: '',
+        warnings: '',
+      });
+    }),
+  ];
+}
