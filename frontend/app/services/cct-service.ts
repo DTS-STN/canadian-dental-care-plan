9f49cdd183d05a22d8f4839dd351084e463f8d58
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-02-08 09:22:20 -0330

    Refactor PDF service to use moize

--- frontend/app/services/cct-service.ts
+++ frontend/app/services/cct-service.ts
@@ -1,7 +1,10 @@
+import moize from 'moize';
 import { z } from 'zod';
 
 import { getEnv } from '~/utils/env.server';
 import { getLogger } from '~/utils/logging.server';
+
+const log = getLogger('pdf-service.server');
 
 const pdfSchema = z.object({
   fileStream: z.string().optional(),
@@ -11,23 +14,27 @@ const pdfSchema = z.object({
 
 export type PdfInfo = z.infer<typeof pdfSchema>;
 
+/**
+ * Return a singleton instance (by means of memomization) of the PDF service.
+ */
+export const getPdf = moize(createPdfService, { onCacheAdd: () => log.info('Creating new PDF service') });
+
 function createPdfService() {
-  const logger = getLogger('pdf-service.server');
   const { CCT_API_BASE_URI } = getEnv();
 
-  /**
-   *
-   * @param { referenceId }
-   * @returns returns the pdf based off  @param referenceId
-   */
   async function getPdf(referenceId: string) {
     const url = `${CCT_API_BASE_URI}/cct/letters/${referenceId}`;
     const response = await fetch(url);
 
-    if (response.ok) return pdfSchema.parse(await response.json());
-    if (response.status === 404) return null;
+    if (response.ok) {
+      return pdfSchema.parse(await response.json());
+    }
 
-    logger.error('%j', {
+    if (response.status === 404) {
+      return null;
+    }
+
+    log.error('%j', {
       message: 'Failed to fetch data',
       status: response.status,
       statusText: response.statusText,
@@ -38,9 +45,5 @@ function createPdfService() {
     throw new Error(`Failed to fetch data. Status: ${response.status}, Status Text: ${response.statusText}`);
   }
 
-  return {
-    getPdf,
-  };
+  return { getPdf };
 }
-
-export const pdfService = createPdfService();
