1cd5bc17b13d3fd2009d4a9b87e22f7785f4a266
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-02-08 16:08:05 -0400

    Style PageDetails

--- frontend/app/routes/_gcweb-app.tsx
+++ frontend/app/routes/_gcweb-app.tsx
@@ -191,38 +191,34 @@ function PageDetails() {
   const { t } = useTranslation(i18nNamespaces);
 
   return (
-    <section className="pagedetails">
+    <section className="mb-8 mt-16">
       <h2 className="sr-only">{t('gcweb:page-details.page-details')}</h2>
-      <div className="row">
-        <div className="col-xs-12">
-          <dl id="wb-dtmd">
-            {!!pageIdentifier && (
-              <>
-                <dt className="float-left clear-left pr-[1ch]">{t('gcweb:page-details.screen-id')}</dt>
-                <dd className="float-left clear-right mb-0">
-                  <span property="identifier">{pageIdentifier}</span>
-                </dd>
-              </>
-            )}
-            {!!buildInfo?.buildDate && (
-              <>
-                <dt className="float-left clear-left pr-[1ch]">{t('gcweb:page-details.date-modfied')}</dt>
-                <dd className="float-left clear-right mb-0">
-                  <time property="dateModified">{buildInfo?.buildDate.slice(0, 10)}</time>
-                </dd>
-              </>
-            )}
-            {!!buildInfo?.buildVersion && (
-              <>
-                <dt className="float-left clear-left pr-[1ch]">{t('gcweb:page-details.version')}</dt>
-                <dd className="float-left clear-right mb-0">
-                  <span property="version">{buildInfo?.buildVersion}</span>
-                </dd>
-              </>
-            )}
-          </dl>
-        </div>
-      </div>
+      <dl id="wb-dtmd" className="space-y-1">
+        {!!pageIdentifier && (
+          <div className="flex gap-2">
+            <dt>{t('gcweb:page-details.screen-id')}</dt>
+            <dd>
+              <span property="identifier">{pageIdentifier}</span>
+            </dd>
+          </div>
+        )}
+        {!!buildInfo?.buildDate && (
+          <div className="flex gap-2">
+            <dt>{t('gcweb:page-details.date-modfied')}</dt>
+            <dd>
+              <time property="dateModified">{buildInfo?.buildDate.slice(0, 10)}</time>
+            </dd>
+          </div>
+        )}
+        {!!buildInfo?.buildVersion && (
+          <div className="flex gap-2">
+            <dt>{t('gcweb:page-details.version')}</dt>
+            <dd>
+              <span property="version">{buildInfo?.buildVersion}</span>
+            </dd>
+          </div>
+        )}
+      </dl>
     </section>
   );
 }
