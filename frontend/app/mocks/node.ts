57a1196a3bc722cafcbf7f8155cfcc7962e57fc2
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-02-07 21:02:39 -0330

    AB#2659 -- refactor how mocks are loaded

--- frontend/app/mocks/node.ts
+++ frontend/app/mocks/node.ts
@@ -3,6 +3,16 @@ import { setupServer } from 'msw/node';
 import { getCCTApiMockHandlers } from '~/mocks/cct-api.server';
 import { getLookupApiMockHandlers } from '~/mocks/lookup-api.server';
 import { getPowerPlatformApiMockHandlers } from '~/mocks/power-platform-api.server';
+import { getRaoidcMockHandlers } from '~/mocks/raoidc.server';
 import { getWSAddressApiMockHandlers } from '~/mocks/wsaddress-api.server';
+import { getEnv } from '~/utils/env.server';
 
-export const server = setupServer(...getCCTApiMockHandlers(), ...getLookupApiMockHandlers(), ...getPowerPlatformApiMockHandlers(), ...getWSAddressApiMockHandlers());
+const { ENABLED_MOCKS } = getEnv();
+
+export const server = setupServer(
+  ...(ENABLED_MOCKS.includes('cct') ? getCCTApiMockHandlers() : []),
+  ...(ENABLED_MOCKS.includes('lookup') ? getLookupApiMockHandlers() : []),
+  ...(ENABLED_MOCKS.includes('power-platform') ? getPowerPlatformApiMockHandlers() : []),
+  ...(ENABLED_MOCKS.includes('raoidc') ? getRaoidcMockHandlers() : []),
+  ...(ENABLED_MOCKS.includes('wsaddress') ? getWSAddressApiMockHandlers() : []),
+);
