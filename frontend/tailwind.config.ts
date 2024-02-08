a20791aeb9139a669b7d77251a3f269d770387e5
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-02-08 12:49:40 -0400

    Set container default style

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./app/**/*.{ts,tsx}'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {},
   },
   plugins: [],
