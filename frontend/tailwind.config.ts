9f650016fc7deae7b9e8b7b5deebf87aa87bd08b
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-02-08 22:40:10 -0330

    AB#2854 -- set TW screen breakpoints

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,6 +7,12 @@ export default {
       center: true,
       padding: '1rem',
     },
+    // @see https://wet-boew.github.io/wet-boew-styleguide/design/grids-en.html#responsive
+    screens: {
+      sm: '768px',
+      md: '992px',
+      lg: '1200px',
+    },
     extend: {},
   },
   plugins: [],
