6d1524028964011548ad205c37e14b7e141b7638
Frank <48450599+Fbasham@users.noreply.github.com>
2024-02-13 12:59:49 -0500

    add letters-service unit tests (#361)

---
+++ frontend/__tests__/services/letters-service.server.test.ts
@@ -0,0 +1,52 @@
+import { HttpResponse, http } from 'msw';
+import { setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll, describe, expect, expectTypeOf, it, vi } from 'vitest';
+
+import { getLetterEntities } from '~/mocks/power-platform-api.server';
+import { type LettersInfo, getLettersService } from '~/services/letters-service.server';
+
+const handlers = [
+  http.get('https://api.example.com/users/:userId/letters', ({ params }) => {
+    const letterEntities = getLetterEntities(params.userId);
+    return HttpResponse.json(letterEntities);
+  }),
+];
+
+const server = setupServer(...handlers);
+
+const letterService = getLettersService();
+
+vi.mock('~/utils/env.server.ts', () => ({
+  getEnv: vi.fn().mockReturnValue({
+    INTEROP_API_BASE_URI: 'https://api.example.com',
+  }),
+}));
+
+describe('letters-service.server.ts', () => {
+  beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
+  afterAll(() => {
+    server.close();
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('it should return letters associated with a valid user', async () => {
+    const letters = await letterService.getLetters('00000000-0000-0000-0000-000000000000');
+    expect(letters.length).toBeGreaterThan(0);
+  });
+
+  it('it should return letters that match the corresponding type', async () => {
+    const letters = await letterService.getLetters('00000000-0000-0000-0000-000000000000');
+    expectTypeOf(letters[0]).toMatchTypeOf<Partial<LettersInfo[number]>>();
+  });
+
+  it('it should not return letters associated with an invalid user', async () => {
+    const letters = await letterService.getLetters('00000000-0000-0000-0000-000000000001');
+    expect(letters.length).toBe(0);
+  });
+
+  it('it should throw and error when given an invalid user', () => {
+    expect(async () => await letterService.getLetters('invalidUserId')).rejects.toThrowError();
+  });
+});
