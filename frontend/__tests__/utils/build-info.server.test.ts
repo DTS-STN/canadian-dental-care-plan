91f353bd60829d8a959af4a1c41c911c9c5d9693
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-02-15 22:48:49 -0330

    Tests for buildinfo.server.ts (#392)

---
+++ frontend/__tests__/utils/build-info.server.test.ts
@@ -0,0 +1,34 @@
+import * as fs from 'node:fs';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+import { readBuildInfo } from '~/utils/build-info.server';
+
+vi.mock('node:fs', async () => ({
+  existsSync: vi.fn(),
+  readFileSync: vi.fn(),
+}));
+
+describe('readBuildInfo', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return undefined if the file does not exist', () => {
+    vi.mocked(fs.existsSync).mockReturnValue(false);
+    expect(readBuildInfo('build-info.json')).toBeUndefined();
+  });
+
+  it('should return a BuildInfo object if the file exists', () => {
+    const expectedBuildInfo = {
+      buildDate: '2000-01-01T00:00:00Z',
+      buildId: '0000',
+      buildRevision: '00000000',
+      buildVersion: '0.0.0-00000000-0000',
+    };
+
+    vi.mocked(fs.existsSync).mockReturnValue(true);
+    vi.mocked(fs.readFileSync).mockReturnValue(JSON.stringify(expectedBuildInfo));
+
+    expect(readBuildInfo('build-info.json')).toEqual(expectedBuildInfo);
+  });
+});
