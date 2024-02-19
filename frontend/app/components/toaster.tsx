2946917ad562eb05297ab864a3603274136e2582
Frank <48450599+Fbasham@users.noreply.github.com>
2024-02-19 08:55:36 -0500

    re-add toast message code (#430)

--- frontend/app/components/toaster.tsx
+++ frontend/app/components/toaster.tsx
@@ -1,34 +1,21 @@
-import { useEffect } from 'react';
+import { ComponentProps, forwardRef, useEffect } from 'react';
 
+import { AlertCircle as AlertCircleIcon, AlertTriangle as AlertTriangleIcon, CheckCircle2 as CheckCircle2Icon, XCircle as XCircleIcon } from 'lucide-react';
 import { MaterialDesignContent, SnackbarProvider, enqueueSnackbar } from 'notistack';
+import { useTranslation } from 'react-i18next';
 import type { ToastMessage } from 'remix-toast';
-import { styled } from 'styled-components';
 
-import { SnackbarMessage } from '~/components/snackbar-message';
+import { useI18nNamespaces } from '~/utils/route-utils';
+import { cn } from '~/utils/tw-utils';
 
 export interface ToasterProps {
   toast?: ToastMessage;
 }
 
-const StyledMaterialDesignContent = styled(MaterialDesignContent)(() => ({
-  '&.notistack-MuiContent-success': {
-    backgroundColor: '#2D7738',
-  },
-  '&.notistack-MuiContent-error': {
-    backgroundColor: '#970C0C',
-  },
-  '&.notistack-MuiContent-warning': {
-    backgroundColor: '#8D7007',
-  },
-  '&.notistack-MuiContent-info': {
-    backgroundColor: '#0676CF',
-  },
-}));
-
 export function Toaster({ toast }: ToasterProps) {
   useEffect(() => {
     if (toast?.message) {
-      enqueueSnackbar(<SnackbarMessage message={toast.message} />, { variant: toast.type });
+      enqueueSnackbar(toast.message, { variant: toast.type });
     }
   }, [toast]);
 
@@ -36,11 +23,59 @@ export function Toaster({ toast }: ToasterProps) {
     <SnackbarProvider
       anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       Components={{
-        success: StyledMaterialDesignContent,
-        error: StyledMaterialDesignContent,
-        warning: StyledMaterialDesignContent,
-        info: StyledMaterialDesignContent,
+        default: DefaultSnackbar,
+        error: ErrorSnackbar,
+        info: InfoSnackbar,
+        success: SuccessSnackbar,
+        warning: WarningSnackbar,
+      }}
+      iconVariant={{
+        error: <XCircleIcon className="me-2 h-5 w-5" />,
+        info: <AlertCircleIcon className="me-2 h-5 w-5" />,
+        success: <CheckCircle2Icon className="me-2 h-5 w-5" />,
+        warning: <AlertTriangleIcon className="me-2 h-5 w-5" />,
       }}
     />
   );
 }
+
+interface SnackbarProps extends ComponentProps<typeof MaterialDesignContent> {}
+
+const Snackbar = forwardRef<HTMLDivElement, SnackbarProps>(({ className, message, ...props }, ref) => {
+  const ns = useI18nNamespaces();
+  const { t } = useTranslation(ns);
+  const translatedMessage = t(message, { defaultValue: message });
+  return <MaterialDesignContent ref={ref} className={cn('!rounded-lg !border', className)} message={translatedMessage} {...props} />;
+});
+
+Snackbar.displayName = 'Snackbar';
+
+const DefaultSnackbar = forwardRef<HTMLDivElement, SnackbarProps>(({ className, ...props }, ref) => {
+  return <Snackbar ref={ref} className={cn(className, '!border-gray-300 !bg-gray-50 !text-gray-800')} {...props} />;
+});
+
+DefaultSnackbar.displayName = 'DefaultSnackbar';
+
+const ErrorSnackbar = forwardRef<HTMLDivElement, SnackbarProps>(({ className, ...props }, ref) => {
+  return <Snackbar ref={ref} className={cn(className, '!border-red-300 !bg-red-50 !text-red-800')} {...props} />;
+});
+
+ErrorSnackbar.displayName = 'ErrorSnackbar';
+
+const InfoSnackbar = forwardRef<HTMLDivElement, SnackbarProps>(({ className, ...props }, ref) => {
+  return <Snackbar ref={ref} className={cn(className, '!border-blue-300 !bg-blue-50 !text-blue-800')} {...props} />;
+});
+
+InfoSnackbar.displayName = 'InfoSnackbar';
+
+const SuccessSnackbar = forwardRef<HTMLDivElement, SnackbarProps>(({ className, ...props }, ref) => {
+  return <Snackbar ref={ref} className={cn(className, '!border-green-300 !bg-green-50 !text-green-800')} {...props} />;
+});
+
+SuccessSnackbar.displayName = 'SuccessSnackbar';
+
+const WarningSnackbar = forwardRef<HTMLDivElement, SnackbarProps>(({ className, ...props }, ref) => {
+  return <Snackbar ref={ref} className={cn(className, '!border-yellow-300 !bg-yellow-50 !text-yellow-800')} {...props} />;
+});
+
+WarningSnackbar.displayName = 'WarningSnackbar';
