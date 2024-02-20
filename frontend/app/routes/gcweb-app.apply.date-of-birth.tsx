c52e6278f85a50ec0efd051c65da7e606d4064bf
olivierroyESDC <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-02-20 14:04:09 -0500

    Add file for date of birth form

---
+++ frontend/app/routes/gcweb-app.apply.date-of-birth.tsx
@@ -0,0 +1,47 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { Form } from '@remix-run/react';
+
+import { useTranslation } from 'react-i18next';
+
+import { Button, ButtonLink } from '~/components/buttons';
+import { InputRadios } from '~/components/input-radios';
+import { getRaoidcService } from '~/services/raoidc-service.server';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+
+const i18nNamespaces = getTypedI18nNamespaces('intake-forms');
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const raoidcService = await getRaoidcService();
+  await raoidcService.handleSessionValidation(request);
+
+  return json({ ok: true });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const raoidcService = await getRaoidcService();
+  await raoidcService.handleSessionValidation(request);
+
+  //TODO
+  //COMPLETE ONCE THE NEXT PAGE IS ADDED
+
+  return json({ ok: true });
+}
+
+export default function EnterDoB() {
+  const { t } = useTranslation(i18nNamespaces);
+
+  return (
+    <>
+      <div>
+        <ButtonLink id="back-button" to="/apply/application-type">
+          {t('intake-forms:date-of-birth.button-back')}
+        </ButtonLink>
+        <Button id="confirm-button" variant="primary">
+          {t('intake-forms:date-of-birth.button-continue')}
+        </Button>
+      </div>
+    </>
+  );
+}
