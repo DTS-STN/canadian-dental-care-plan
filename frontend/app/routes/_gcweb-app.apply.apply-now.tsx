8e82b474d6c9bf70e07fae41716b0ce5d66da6db
olivierroyESDC <78967586+olivierroyESDC@users.noreply.github.com>
2024-02-20 10:35:25 -0500

    AB#2931 Intake start page (#445)
    * [gitops] AB#2659 -- Add RASCL logout endpoint (#444)
    
    * AB#2659 -- Add RASCL logout endpoint (#446)
    
    * add first iteration of start page for intake form
    
    * applied changes from PR feedback
    
    * applied changes from PR feedback
    
    ---------
    
    Co-authored-by: Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>

---
+++ frontend/app/routes/_gcweb-app.apply.apply-now.tsx
@@ -0,0 +1,48 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+
+import { useTranslation } from 'react-i18next';
+
+import { ButtonLink } from '~/components/buttons';
+import { getRaoidcService } from '~/services/raoidc-service.server';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+
+const i18nNamespaces = getTypedI18nNamespaces('intake-forms');
+export const handle = {
+  breadcrumbs: [{ labelI18nKey: 'intake-forms:apply-form.page-title' }],
+  i18nNamespaces,
+  pageIdentifier: 'CDCP-1111',
+  pageTitleI18nKey: 'intake-forms:apply-form.page-title',
+} as const satisfies RouteHandleData;
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const raoidcService = await getRaoidcService();
+  await raoidcService.handleSessionValidation(request);
+
+  return json({ ok: true });
+}
+
+export default function ApplyNow() {
+  const { t } = useTranslation(i18nNamespaces);
+
+  return (
+    <>
+      <p className="mb-8 text-lg text-gray-500">{t('intake-forms:apply-form.privacy-statement')}</p>
+      <p>{t('intake-forms:apply-form.privacy-message')}</p>
+
+      <p className="mb-8 text-lg text-gray-500">{t('intake-forms:apply-form.identity-information')}</p>
+      <p>{t('intake-forms:apply-form.identity-message')}</p>
+
+      <p className="mb-8 text-lg text-gray-500">{t('intake-forms:apply-form.terms-and-conditions')}</p>
+      <p>{t('intake-forms:apply-form.terms-and-conditions-message')}</p>
+
+      <p className="mb-8 text-lg text-gray-500">{t('intake-forms:apply-form.apply-now')}</p>
+      <p>{t('intake-forms:apply-form.start-note')}</p>
+
+      <ButtonLink id="apply-now-button" to="/personal-information">
+        {t('intake-forms:apply-form.start-application')}
+      </ButtonLink>
+    </>
+  );
+}
