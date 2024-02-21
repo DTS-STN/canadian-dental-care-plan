89e0133b9bbcc8bd0794528636c347e35e861859
Shawn Zhang <azuneko1110@gmail.com>
2024-02-21 15:24:33 -0500

    Application Form learning exercise - privacy page and stepper form (#452)
    * privacy page
    
    * Stepper
    
    * remove dangerouslySetInnerHTML
    
    * use useSearchParam instead of useState
    
    ---------
    
    Signed-off-by: Shawn Zhang <azuneko1110@gmail.com>

--- frontend/app/routes/_gcweb-app.privacy._index.tsx
+++ frontend/app/routes/_gcweb-app.privacy._index.tsx
@@ -1,14 +1,13 @@
-import { Link } from '@remix-run/react';
-
 import { useTranslation } from 'react-i18next';
 
+import { ButtonLink } from '~/components/buttons';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import type { RouteHandleData } from '~/utils/route-utils';
 
 const i18nNamespaces = getTypedI18nNamespaces('privacy');
 
 export const handle = {
-  breadcrumbs: [{ labelI18nKey: 'privacy:privacy-statement-title' }],
+  breadcrumbs: [{ labelI18nKey: 'privacy:privacy-statement.title' }],
   i18nNamespaces,
   pageIdentifier: 'CDCP-00015',
 } as const satisfies RouteHandleData;
@@ -18,24 +17,37 @@ export default function Privacy() {
   return (
     <>
       <section className="mt-8">
-        <h2 className="mb-4 font-bold">{t('privacy:privacy-statement-title')}</h2>
-        <p dangerouslySetInnerHTML={{ __html: t('privacy:privacy-statement-content') }} />
+        <h2 className="mb-4 text-lg font-bold">{t('privacy:privacy-statement.title')}</h2>
+        <p className="mb-4">{t('privacy:privacy-statement.content1')}</p>
+        <p className="mb-4">{t('privacy:privacy-statement.content2')}</p>
+        <p>{t('privacy:privacy-statement.content3')}</p>
+        <a className="text-[#0535D2]" href={t('privacy:privacy-statement.privacy-policy-link')}>
+          {t('privacy:privacy-statement.privacy-policy-link-text')}
+        </a>
+        <p className="mt-4">{t('privacy:privacy-statement.content4')}</p>
+        <a className="text-[#0535D2]" href={t('privacy:privacy-statement.report-a-concern')}>
+          {t('privacy:privacy-statement.report-a-concern')}.
+        </a>
       </section>
       <section className="mt-8">
-        <h2 className="mb-4 font-bold">{t('privacy:identity-info-title')}</h2>
-        <p dangerouslySetInnerHTML={{ __html: t('privacy:identity-info-content') }} />
+        <h2 className="mb-4 text-lg font-bold">{t('privacy:identity-information.title')}</h2>
+        <p>{t('privacy:identity-information.content1')}</p>
+        <ul className="list-disc pl-8">
+          <li>{t('privacy:identity-information.list-item1')}</li>
+          <li>{t('privacy:identity-information.list-item2')}</li>
+        </ul>
       </section>
       <section className="mt-8">
-        <h2 className="mb-4 font-bold">{t('privacy:terms-title')}</h2>
-        <p dangerouslySetInnerHTML={{ __html: t('privacy:terms-content') }} />
+        <h2 className="mb-4 text-lg font-bold">{t('privacy:terms-and-conditions.title')}</h2>
+        <p>{t('privacy:terms-and-conditions.content')}</p>
       </section>
       <section className="my-8">
-        <h2 className="mb-4 font-bold">{t('privacy:apply-title')}</h2>
-        <p dangerouslySetInnerHTML={{ __html: t('privacy:apply-content') }} />
+        <h2 className="mb-4 text-lg font-bold">{t('privacy:apply.title')}</h2>
+        <p>{t('privacy:apply.content')}</p>
       </section>
-      <Link className="rounded-sm bg-[#318000] px-4 py-2 text-white" to="/application-form">
+      <ButtonLink size="base" variant="green" className="rounded-sm" to="/application">
         {t('privacy:button.start-app')}
-      </Link>
+      </ButtonLink>
     </>
   );
 }
