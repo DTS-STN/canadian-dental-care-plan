547db4578503a7801b543b5f95bfe51a376c66be
frank basham <frankabasham@gmail.com>
2024-02-22 09:11:38 -0500

    add initial beefed up e2e tests

--- frontend/e2e/_gcweb-app.$.spec.ts
+++ frontend/e2e/_gcweb-app.$.spec.ts
@@ -3,16 +3,13 @@ import { expect, test } from '@playwright/test';
 
 test.describe('not found page', () => {
   test('should navigate to not found page', async ({ page }) => {
-    await page.goto('/personal-info');
-
-    await expect(page.locator('h1')).toHaveText("We couldn't find that web page (Error 404)");
+    await page.goto('/page-does-not-exist');
+    await expect(page).toHaveURL('/page-does-not-exist');
   });
 
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto('/personal-info');
-
+    await page.goto('/page-does-not-exist');
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
--- frontend/e2e/_gcweb-app.personal-information.preferred-language.confirm.spec.ts
+++ frontend/e2e/_gcweb-app.personal-information.preferred-language.confirm.spec.ts
@@ -4,13 +4,13 @@ import { expect, test } from '@playwright/test';
 test.describe('preferred language confirm page', async () => {
   test('should navigate to page and render', async ({ page }) => {
     await page.goto('/personal-information/preferred-language/edit');
-    await page.locator('button', { hasText: /change/i }).click();
+    await page.locator('button#change-button').click();
     await expect(page).toHaveURL(/.*personal-information\/preferred-language\/confirm/);
   });
 
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     await page.goto('/personal-information/preferred-language/edit');
-    await page.locator('button', { hasText: /change/i }).click();
+    await page.locator('button#change-button').click();
     await expect(page).toHaveURL(/.*personal-information\/preferred-language\/confirm/);
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
