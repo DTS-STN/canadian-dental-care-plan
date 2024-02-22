4554c231d34101ff75747b640dbd5150d57b4f57
olivierroyESDC <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-02-22 08:57:07 -0500

    save before rebase with main

--- frontend/app/routes/_gcweb-app.apply.date-of-birth.tsx
+++ frontend/app/routes/_gcweb-app.apply.date-of-birth.tsx
@@ -1,17 +1,26 @@
+import { useEffect, useState } from 'react';
+
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { Form } from '@remix-run/react';
+import { json, redirect } from '@remix-run/node';
+import { Form, useActionData } from '@remix-run/react';
 
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
 import { Button, ButtonLink } from '~/components/buttons';
-import { InputRadios } from '~/components/input-radios';
+import { ErrorSummary, createErrorSummaryItems, hasErrors, scrollAndFocusToErrorSummary } from '~/components/error-summary';
+import { InputField } from '~/components/input-field';
 import { getRaoidcService } from '~/services/raoidc-service.server';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import type { RouteHandleData } from '~/utils/route-utils';
 
 const i18nNamespaces = getTypedI18nNamespaces('intake-forms');
-
+export const handle = {
+  breadcrumbs: [{ labelI18nKey: 'intake-forms:date-of-birth.page-title' }],
+  i18nNamespaces,
+  pageIdentifier: 'CDCP-1122',
+  pageTitleI18nKey: 'intake-forms:date-of-birth.page-title',
+} as const satisfies RouteHandleData;
 export async function loader({ request }: LoaderFunctionArgs) {
   const raoidcService = await getRaoidcService();
   await raoidcService.handleSessionValidation(request);
@@ -23,19 +32,88 @@ export async function action({ request }: ActionFunctionArgs) {
   const raoidcService = await getRaoidcService();
   await raoidcService.handleSessionValidation(request);
 
+  const dateOfBirthFormSchema = z.object({
+    year: z
+      .string()
+      .min(1, { message: 'empty-field' })
+      .transform((val) => val.trim()),
+    month: z
+      .string()
+      .min(1, { message: 'empty-field' })
+      .transform((val) => val.trim()),
+    day: z.string().trim().optional(),
+  });
+  const formData = Object.fromEntries(await request.formData());
+
+  const dateOfBirth = formData['year'] + '-' + formData['month'] + '-' + formData['day']; //2024-11-14
+  const dateSchema = z.coerce.date();
+  type DateSchema = z.infer<typeof dateSchema>;
+  console.debug('date scheeeemmm ' + JSON.stringify(dateSchema.safeParse(dateOfBirth))); // false
+  const dateParsedResult = dateSchema.safeParse(dateOfBirth);
+
+  //const myDateSchema = z.coerce.date();
+  if (!dateParsedResult.success) {
+    return json({
+      errors: dateParsedResult.error.flatten(),
+      formData: formData as Partial<z.infer<typeof dateOfBirthFormSchema>>,
+    });
+  }
+
+  //if (!parsedDataResult.success) {
+  // return json({
+  //   errors: parsedDataResult.error.flatten(),
+  //   formData: formData as Partial<z.infer<typeof dateOfBirthFormSchema>>,
+  // });
+  // }
   //TODO
   //COMPLETE ONCE THE NEXT PAGE IS ADDED
 
-  return json({ ok: true });
+  return redirect('/apply/date-of-birth', {});
 }
 
 export default function EnterDoB() {
+  const actionData = useActionData<typeof action>();
+  const errorSummaryId = 'error-summary';
   const { t } = useTranslation(i18nNamespaces);
+  /**
+   * Gets an error message based on the provided internationalization (i18n) key.
+   *
+   * @param errorI18nKey - The i18n key for the error message.
+   * @returns The corresponding error message, or undefined if no key is provided.
+   */
+  function getErrorMessage(errorI18nKey?: string): string | undefined {
+    if (!errorI18nKey) return undefined;
+
+    /**
+     * The 'as any' is employed to circumvent typechecking, as the type of
+     * 'errorI18nKey' is a string, and the string literal cannot undergo validation.
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return t(`intake-forms:date-of-birth.error-message.${errorI18nKey}` as any);
+  }
+  const errorMessages = {
+    //year: getErrorMessage(actionData?.errors.fieldErrors.fieldErrors.year?.[0]),
+    // month: getErrorMessage(actionData?.errors.fieldErrors.month?.[0]),
+    // day: getErrorMessage(actionData?.errors.fieldErrors.day?.[0]),
+  };
+  const errorSummaryItems = createErrorSummaryItems(errorMessages);
+
+  useEffect(() => {
+    if (actionData?.formData && hasErrors(actionData.formData)) {
+      scrollAndFocusToErrorSummary(errorSummaryId);
+    }
+  }, [actionData]);
 
   return (
     <>
+      {errorSummaryItems.length > 0 && <ErrorSummary id={errorSummaryId} errors={errorSummaryItems} />}
       <Form method="post">
-        <input name="email" placeholder="Email Address" />
+        <div>
+          <p>{t('intake-forms:date-of-birth.enter-dob-message')}</p>
+          <InputField id="year" label={t('intake-forms:date-of-birth.field.year')} placeholder="YYYY" name="year" required errorMessage={errorMessages.year} maxLength={4} />
+          <InputField id="month" label={t('intake-forms:date-of-birth.field.month')} placeholder="MM" name="month" required errorMessage={errorMessages.month} maxLength={2} />
+          <InputField id="day" label={t('intake-forms:date-of-birth.field.day')} placeholder="DD" name="day" required errorMessage={errorMessages.day} maxLength={2} />
+        </div>
         <div>
           <ButtonLink id="back-button" to="/apply/application-type">
             {t('intake-forms:date-of-birth.button-back')}
