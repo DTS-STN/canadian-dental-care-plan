51c55fab69fdacaf48cfe19f5fcfa5ba20763362
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-02-22 09:29:12 -0330

    Add k8s readiness endpoint (#466)

---
+++ frontend/app/routes/api.readyz.ts
@@ -0,0 +1,11 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+
+/**
+ * A basic readiness endpoint to be used by kubernetes container probes
+ *
+ * @see https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/#define-readiness-probes
+ */
+export function loader({ request }: LoaderFunctionArgs) {
+  return json({ ready: true });
+}
