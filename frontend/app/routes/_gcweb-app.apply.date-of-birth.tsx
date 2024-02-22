8dcc86eabdbe4d5d7aa6ed1db426b782e6823ab2
olivierroyESDC <78967586+olivierroyESDC@users.noreply.github.com>
2024-02-22 14:09:41 -0500

    Intake form dob (#474)
    * AB#2705 -- Refactoring wsaddress-service to correctly consume (mocked) API (#450)
    
    * Fix app title link style (#453)
    
    * AB#2717 consume letter api (#454)
    
    * Remove JAVASCRIPT_ENABLED feature flag (#456)
    
    * Add unit to SESSION_COOKIE_MAX_AGE (#457)
    
    * AB#2673 -- Session timeout extension (#458)
    
    * AB#2935 -- Call /api/refresh-session to extend session (#459)
    
    * Configure eslint @typescript-eslint/no-unused-vars (#461)
    
    * Configure vitest coverage report (#462)
    
    * add session timeout variables into env (#455)
    
    * Fix input error a11y issue (#463)
    
    * Fix session timeout dialog (#465)
    
    * Add k8s readiness endpoint (#466)
    
    * [gitops] Bump staging version (#467)
    
    * Add Playwright Test for VSCode as recommended extension (#468)
    
    * Add k8s livez/readyz checks (#469)
    
    * add initial beefed up e2e tests (#470)
    
    * Changing staging environment auth configs to use ECAS' SYS2 url (#472)
    
    * add additional beefed up e2e tests (#471)
    
    * Temporary set SESSION_TIMEOUT configs for DEV (#473)
    
    * AB#2705 -- Removing outstanding TODOs for suggested address flow (#464)
    
    * AB#2931 Intake start page (#445)
    
    * [gitops] AB#2659 -- Add RASCL logout endpoint (#444)
    
    * AB#2659 -- Add RASCL logout endpoint (#446)
    
    * add first iteration of start page for intake form
    
    * applied changes from PR feedback
    
    * applied changes from PR feedback
    
    ---------
    
    Co-authored-by: Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
    
    * AB#2931 Type of application (#449)
    
    * add the application type page
    
    * remove unsused import
    
    * Add file for date of birth form
    
    * save before rebase with main
    
    * fixed typo on route name
    
    * save before rebase with main
    
    * fixed error message for invalid date
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Nicholas Ly <55502055+nicholas-ly@users.noreply.github.com>
    Co-authored-by: Sébastien Comeau <114004123+sebastien-comeau@users.noreply.github.com>
    Co-authored-by: Frank <48450599+Fbasham@users.noreply.github.com>
    Co-authored-by: Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
    Co-authored-by: Lei Ye <lei.ye@hrsdc-rhdcc.gc.ca>

---
+++ frontend/app/routes/_gcweb-app.apply.date-of-birth.tsx
@@ -0,0 +1,120 @@
+import { useEffect } from 'react';
+
+import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
+import { Form, useActionData } from '@remix-run/react';
+
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+
+import { Button, ButtonLink } from '~/components/buttons';
+import { ErrorSummary, createErrorSummaryItems, hasErrors, scrollAndFocusToErrorSummary } from '~/components/error-summary';
+import { InputField } from '~/components/input-field';
+import { getRaoidcService } from '~/services/raoidc-service.server';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+
+const i18nNamespaces = getTypedI18nNamespaces('intake-forms');
+export const handle = {
+  breadcrumbs: [{ labelI18nKey: 'intake-forms:date-of-birth.page-title' }],
+  i18nNamespaces,
+  pageIdentifier: 'CDCP-1122',
+  pageTitleI18nKey: 'intake-forms:date-of-birth.page-title',
+} as const satisfies RouteHandleData;
+export async function loader({ request }: LoaderFunctionArgs) {
+  const raoidcService = await getRaoidcService();
+  await raoidcService.handleSessionValidation(request);
+
+  return json({ ok: true });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const raoidcService = await getRaoidcService();
+  await raoidcService.handleSessionValidation(request);
+
+  const dateOfBirthFormSchema = z.object({
+    year: z
+      .string()
+      .min(1, { message: 'empty-field' })
+      .transform((val) => val.trim()),
+    month: z
+      .string()
+      .min(1, { message: 'empty-field' })
+      .transform((val) => val.trim()),
+    day: z
+      .string()
+      .min(1, { message: 'empty-field' })
+      .transform((val) => val.trim()),
+  });
+  const formData = Object.fromEntries(await request.formData());
+
+  const dateOfBirth = formData['year'] + '-' + formData['month'] + '-' + formData['day']; //2024-11-14
+  const dateSchema = z.coerce.date();
+  const dateParsedResult = dateSchema.safeParse(dateOfBirth);
+
+  if (!dateParsedResult.success) {
+    return json({
+      errors: 'invalid-date',
+      formData: formData as Partial<z.infer<typeof dateOfBirthFormSchema>>,
+    });
+  }
+
+  //TODO
+  //COMPLETE ONCE THE NEXT PAGE IS ADDED
+
+  return redirect('/apply/date-of-birth', {});
+}
+
+export default function EnterDoB() {
+  const actionData = useActionData<typeof action>();
+  const errorSummaryId = 'error-summary';
+  const { t } = useTranslation(i18nNamespaces);
+  /**
+   * Gets an error message based on the provided internationalization (i18n) key.
+   *
+   * @param errorI18nKey - The i18n key for the error message.
+   * @returns The corresponding error message, or undefined if no key is provided.
+   */
+  function getErrorMessage(errorI18nKey?: string): string | undefined {
+    if (!errorI18nKey) return undefined;
+
+    /**
+     * The 'as any' is employed to circumvent typechecking, as the type of
+     * 'errorI18nKey' is a string, and the string literal cannot undergo validation.
+     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return t(`intake-forms:date-of-birth.error-message.${errorI18nKey}` as any);
+  }
+  const errorMessages = {
+    dateOfBirthFieldSet: getErrorMessage(actionData?.errors),
+  };
+  const errorSummaryItems = createErrorSummaryItems(errorMessages);
+
+  useEffect(() => {
+    if (actionData?.formData && hasErrors(actionData.formData)) {
+      scrollAndFocusToErrorSummary(errorSummaryId);
+    }
+  }, [actionData]);
+
+  return (
+    <>
+      {errorSummaryItems.length > 0 && <ErrorSummary id={errorSummaryId} errors={errorSummaryItems} />}
+      <Form method="post">
+        <fieldset id="dateOfBirthFieldSet">
+          <legend>{t('intake-forms:date-of-birth.enter-dob-message')}</legend>
+          <InputField id="year" label={t('intake-forms:date-of-birth.field.year')} placeholder="YYYY" name="year" required errorMessage={errorMessages.year} maxLength={4} />
+          <InputField id="month" label={t('intake-forms:date-of-birth.field.month')} placeholder="MM" name="month" required errorMessage={errorMessages.month} maxLength={2} />
+          <InputField id="day" label={t('intake-forms:date-of-birth.field.day')} placeholder="DD" name="day" required errorMessage={errorMessages.day} maxLength={2} />
+        </fieldset>
+        <div>
+          <ButtonLink id="back-button" to="/apply/application-type">
+            {t('intake-forms:date-of-birth.button-back')}
+          </ButtonLink>
+          <Button id="confirm-button" variant="primary">
+            {t('intake-forms:date-of-birth.button-continue')}
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
+}
