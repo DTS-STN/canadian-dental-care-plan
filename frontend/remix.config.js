02cf36c26ee08c41a5015436b480163b869d4970
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-02-23 08:26:02 -0330

    Add remix-flat-routes

--- frontend/remix.config.js
+++ frontend/remix.config.js
@@ -1,8 +1,11 @@
+import { flatRoutes } from 'remix-flat-routes';
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 export default {
   cacheDirectory: './node_modules/.cache/remix',
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ['**/.*'], // let remix-flat-routes handle routing
+  routes: async (defineRoutes) => flatRoutes('routes', defineRoutes),
   serverDependenciesToBundle: ['react-idle-timer'],
 };
