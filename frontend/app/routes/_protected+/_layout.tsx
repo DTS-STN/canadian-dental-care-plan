9d79ae5450fd4975c10dd4057d6f0364bc221e14
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-02-28 19:23:01 -0330

    Move 404 page and fix layout handle's `satisfies`

--- frontend/app/routes/_protected+/_layout.tsx
+++ frontend/app/routes/_protected+/_layout.tsx
@@ -3,9 +3,9 @@ import { Outlet, isRouteErrorResponse, useRouteError } from '@remix-run/react';
 import ApplicationLayout, { ServerError, i18nNamespaces as layoutI18nNamespaces } from '~/components/layouts/application-layout';
 import type { RouteHandleData } from '~/utils/route-utils';
 
-export const handle: RouteHandleData = {
+export const handle = {
   i18nNamespaces: [...layoutI18nNamespaces],
-} as const;
+} as const satisfies RouteHandleData;
 
 export function ErrorBoundary() {
   const error = useRouteError();
