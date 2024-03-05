32645e7e99a39fe94aa427c656a443f97bb3e394
Sébastien Comeau <114004123+sebastien-comeau@users.noreply.github.com>
2024-03-05 13:24:40 -0400

    Fix collapsible style (#597)

--- frontend/app/components/collapsible.tsx
+++ frontend/app/components/collapsible.tsx
@@ -11,25 +11,19 @@ export interface CollapsibleSummaryProps extends ComponentProps<'summary'> {
   id: string;
 }
 
-export function CollapsibleSummary({ id, children, className, ...props }: CollapsibleSummaryProps) {
-  const headerId = `${id}-header`;
-
+export function CollapsibleSummary({ children, className, ...props }: CollapsibleSummaryProps) {
   return (
-    <summary {...props}>
-      <span id={headerId} className={cn('inline text-blue-900 underline', className)}>
-        {children}
-      </span>
+    <summary className={cn('cursor-pointer text-blue-900 underline', className)} {...props}>
+      <span className="ml-4">{children}</span>
     </summary>
   );
 }
 
 export function CollapsibleDetails({ children, id, className, summary, ...props }: CollapsibleDetailsProps) {
-  const detailsId = `${id}-details`;
   const summaryId = `${id}-summary`;
   const contentId = `${id}-content`;
-
   return (
-    <details id={detailsId} {...props}>
+    <details {...props}>
       <CollapsibleSummary id={summaryId}>{summary}</CollapsibleSummary>
       <div id={contentId} className={cn('mt-2 border-l-[6px] border-gray-400 px-6 py-4', className)}>
         {children}
