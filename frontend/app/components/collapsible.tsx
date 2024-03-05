8bd37c17a27887c22a107aaf79fdf0732465b54e
Caroline George <156207329+carolinegeorge-gov@users.noreply.github.com>
2024-03-04 20:43:10 -0500

    user-story-2999: created collapsible component (#588)
    * user-story-2999: created collapsible component

---
+++ frontend/app/components/collapsible.tsx
@@ -0,0 +1,39 @@
+import type { ComponentProps, ReactNode } from 'react';
+
+import { cn } from '~/utils/tw-utils';
+
+export interface CollapsibleDetailsProps extends ComponentProps<'details'> {
+  id: string;
+  summary: ReactNode;
+}
+
+export interface CollapsibleSummaryProps extends ComponentProps<'summary'> {
+  id: string;
+}
+
+export function CollapsibleSummary({ id, children, className, ...props }: CollapsibleSummaryProps) {
+  const headerId = `${id}-header`;
+
+  return (
+    <summary {...props}>
+      <span id={headerId} className={cn('inline text-blue-900 underline', className)}>
+        {children}
+      </span>
+    </summary>
+  );
+}
+
+export function CollapsibleDetails({ children, id, className, summary, ...props }: CollapsibleDetailsProps) {
+  const detailsId = `${id}-details`;
+  const summaryId = `${id}-summary`;
+  const contentId = `${id}-content`;
+
+  return (
+    <details id={detailsId} {...props}>
+      <CollapsibleSummary id={summaryId}>{summary}</CollapsibleSummary>
+      <div id={contentId} className={cn('mt-2 border-l-[6px] border-gray-400 px-6 py-4', className)}>
+        {children}
+      </div>
+    </details>
+  );
+}
