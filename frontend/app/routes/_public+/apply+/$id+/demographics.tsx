902ac20c9bc70c252d791f7bdab068f4696e5357
olivierroyESDC <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-03-06 14:58:11 -0500

    applied code changes after code review

--- frontend/app/routes/_public+/apply+/$id+/demographics.tsx
+++ frontend/app/routes/_public+/apply+/$id+/demographics.tsx
@@ -2,9 +2,10 @@ import { json, redirect } from '@remix-run/node';
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { Form } from '@remix-run/react';
 
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Trans, useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
 import { Button } from '~/components/buttons';
 import { getApplyFlow } from '~/routes-flow/apply-flow';
@@ -26,18 +27,27 @@ export async function action({ request, params }: ActionFunctionArgs) {
   const applyFlow = getApplyFlow();
   const { id, state } = await applyFlow.loadState({ request, params });
 
-  const body = new URLSearchParams(await request.text());
-  const buttonPressed = body.get('button');
-  console.debug('BUTTON PRESSED ::: ' + JSON.stringify(buttonPressed, null, 2));
-  if (buttonPressed === 'answerQuestions') {
+  const formDataSchema = z.object({
+    demographics: z.enum(['answer', 'skip']),
+  });
+
+  const formData = Object.fromEntries(await request.formData());
+  const parsedDataResult = await formDataSchema.safeParseAsync(formData);
+  if (!parsedDataResult.success) {
+    return json({
+      errors: parsedDataResult.error.format(),
+      formData: formData as Partial<z.infer<typeof formDataSchema>>,
+    });
+  }
+
+  if (parsedDataResult.data.demographics === 'answer') {
     const sessionResponseInit = await applyFlow.saveState({
       request,
       params,
       state,
     });
     return redirect(`/apply/${id}/demographics-part1`, sessionResponseInit);
-  }
-  if (buttonPressed === 'continueToReview') {
+  } else if (parsedDataResult.data.demographics === 'skip') {
     const sessionResponseInit = await applyFlow.saveState({
       request,
       params,
@@ -45,7 +55,6 @@ export async function action({ request, params }: ActionFunctionArgs) {
     });
     return redirect(`/apply/${id}/review-information`, sessionResponseInit);
   }
-
   throw new Response('Not Found', { status: 404 });
 }
 
