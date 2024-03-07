b44d1ea75fa663ef252ca6b22f5d8b179e0e0c2f
olivierroyESDC <78967586+olivierroyESDC@users.noreply.github.com>
2024-03-07 08:27:40 -0500

    AB#3000 Demographics welcome page (#603)
    * added the welcome page for the demographics questionnaire
    
    * changed redirect URL

--- frontend/app/routes/_public+/apply+/$id+/demographics-part1.tsx
+++ frontend/app/routes/_public+/apply+/$id+/demographics-part1.tsx
@@ -2,6 +2,8 @@ import { json } from '@remix-run/node';
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { Form, useLoaderData } from '@remix-run/react';
 
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
 
 import { Button, ButtonLink } from '~/components/buttons';
@@ -71,8 +73,10 @@ export default function DemographicsPart1() {
         <div className="flex flex-wrap items-center gap-3">
           <ButtonLink id="cancel-button" to="/personal-information">
             {t('demographics-oral-health-questions:part1.button-back')}
+            <FontAwesomeIcon icon={faChevronLeft} className="pl-2" />
           </ButtonLink>
           <Button id="change-button" variant="primary">
+            <FontAwesomeIcon icon={faChevronRight} className="pl-2" />
             {t('demographics-oral-health-questions:part1.button-continue')}
           </Button>
         </div>
---
+++ frontend/app/routes/_public+/apply+/$id+/demographics.tsx
@@ -0,0 +1,61 @@
+import { json, redirect } from '@remix-run/node';
+import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useTranslation } from 'react-i18next';
+
+import { Button, ButtonLink } from '~/components/buttons';
+import { getApplyFlow } from '~/routes-flow/apply-flow';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+
+const i18nNamespaces = getTypedI18nNamespaces('demographics-oral-health-questions');
+export const handle = {
+  i18nNamespaces,
+  pageIdentifier: 'CDCP-1110',
+  pageTitleI18nKey: 'demographics-oral-health-questions:optional-demographic-oral-health-questions.page-title',
+};
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  const applyFlow = getApplyFlow();
+  const { id, state } = await applyFlow.loadState({ request, params });
+
+  return json({ id, state });
+}
+export async function action({ request, params }: ActionFunctionArgs) {
+  const applyFlow = getApplyFlow();
+  const { id, state } = await applyFlow.loadState({ request, params });
+
+  const sessionResponseInit = await applyFlow.saveState({
+    request,
+    params,
+    state,
+  });
+  return redirect(`/apply/${id}/demographics-part1`, sessionResponseInit);
+}
+
+export default function Demographics() {
+  const { t } = useTranslation(i18nNamespaces);
+  const { id } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <Form method="post" className="space-y-6">
+        <p className="mb-6">{t('optional-demographic-oral-health-questions.responses-will-be-confidential')}</p>
+        <p className="mb-6">{t('optional-demographic-oral-health-questions.questions-are-voluntary')}</p>
+        <p className="mb-6">{t('optional-demographic-oral-health-questions.anwsers-will-not-affect-eligibility')}</p>
+
+        <div className="mt-6 flex flex-wrap items-center gap-3">
+          <ButtonLink id="back-button" variant="alternative" to={`/apply/${id}/federal-provincial-territorial-benefits`}>
+            {t('optional-demographic-oral-health-questions.back-button')}
+            <FontAwesomeIcon icon={faChevronLeft} className="pl-2" />
+          </ButtonLink>
+          <Button id="answer-button" variant="primary">
+            {t('optional-demographic-oral-health-questions.answer-button')}
+            <FontAwesomeIcon icon={faChevronRight} className="pl-2" />
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
+}
