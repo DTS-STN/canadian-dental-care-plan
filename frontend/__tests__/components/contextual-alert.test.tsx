671566b09f179237723a86381e0c32d26ef41268
Benoit Cloutier <77116011+nibivi77@users.noreply.github.com>
2024-03-07 15:37:16 -0500

    Adding contextual alert. (#611)
    * Adding contextual alert.
    
    * Adding translations.
    
    * Requested changes.
    
    * Semantic header.
    
    * Fixing test.
    
    * Small test improvement.

---
+++ frontend/__tests__/components/contextual-alert.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import { describe, expect, it } from 'vitest';
+
+import { ContextualAlert } from '~/components/contextual-alert';
+
+describe('ContextualAlert', () => {
+  it('renders ContextualAlert Component with correct Icon', () => {
+    const mockChildren = 'Text test';
+    render(<ContextualAlert type="success">{mockChildren}</ContextualAlert>);
+
+    expect(screen.getByText('Text test')).toBeInTheDocument();
+    expect(screen.getByTestId('success')).toBeInTheDocument();
+  });
+});
---
+++ frontend/app/components/contextual-alert.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from 'react';
+
+import { faCheckCircle, faCircleInfo, faExclamationCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export interface ContextualAlertProps {
+  children: ReactNode;
+  type: 'warning' | 'success' | 'danger' | 'info';
+}
+
+export function ContextualAlert(props: ContextualAlertProps) {
+  const { children, type } = props;
+
+  const alertColor = type === 'warning' ? 'border-l-amber-700' : type === 'danger' ? 'border-l-red-700' : type === 'info' ? 'border-l-cyan-700' : 'border-l-green-700';
+
+  return (
+    <div className={`relative min-w-72 pl-4 sm:pl-6`}>
+      <div className="absolute left-1.5 top-3 bg-white pt-1 sm:left-3.5">
+        <Icon type={type} />
+      </div>
+      <div className={`overflow-auto border-l-4 ${alertColor} py-2.5 pl-6 leading-8`}>{children}</div>
+    </div>
+  );
+}
+
+function Icon({ type }: { type: string }) {
+  switch (type) {
+    case 'warning':
+      return <FontAwesomeIcon icon={faExclamationTriangle} className="h-6 w-6 text-amber-700" data-testid="warning" />;
+    case 'success':
+      return <FontAwesomeIcon icon={faCheckCircle} className="h-6 w-6 text-green-700" data-testid="success" />;
+    case 'danger':
+      return <FontAwesomeIcon icon={faExclamationCircle} className="h-6 w-6 text-red-700" data-testid="danger" />;
+    case 'info':
+      return <FontAwesomeIcon icon={faCircleInfo} className="h-6 w-6 text-cyan-700" data-testid="info" />;
+    default:
+      break;
+  }
+}
