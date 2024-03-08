2cd489c633c53f901c0cfbe9027e2f65f7a62359
Lei Ye <lei.ye@hrsdc-rhdcc.gc.ca>
2024-03-07 20:30:55 -0500

    add unit tests for personal information and communication preference

---
+++ frontend/__tests__/routes/_public+/communication-preference.test.tsx
@@ -0,0 +1,187 @@
+import { redirect } from '@remix-run/node';
+
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+import { action, loader } from '~/routes/_public+/apply+/$id+/communication-preference';
+
+vi.mock('~/routes-flow/apply-flow', () => ({
+  getApplyFlow: vi.fn().mockReturnValue({
+    loadState: vi.fn().mockReturnValue({
+      id: '123',
+      state: {},
+    }),
+    saveState: vi.fn().mockReturnValue({
+      headers: {
+        'Set-Cookie': 'some-set-cookie-header',
+      },
+    }),
+  }),
+}));
+
+vi.mock('~/services/lookup-service.server', () => ({
+  getLookupService: vi.fn().mockReturnValue({
+    getAllPreferredLanguages: vi.fn().mockReturnValue([
+      {
+        id: 'en',
+        nameEn: 'English',
+        nameFr: 'Anglais',
+      },
+      {
+        id: 'fr',
+        nameEn: 'French',
+        nameFr: 'Français',
+      },
+    ]),
+    getAllPreferredCommunicationMethods: vi.fn().mockReturnValue([
+      {
+        id: 'email',
+        nameEn: 'Email',
+        nameFr: 'Adresse courriel',
+      },
+      {
+        id: 'mail',
+        nameEn: 'Postal mail',
+        nameFr: 'Courrier postal',
+      },
+    ]),
+  }),
+}));
+
+vi.mock('~/utils/env.server', () => ({
+  getEnv: vi.fn().mockReturnValue({
+    COMMUNICATION_METHOD_EMAIL_ID: 'email',
+  }),
+}));
+
+describe('_public.apply.id.communication-preference', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe('loader()', () => {
+    it('should id, state, country list and region list', async () => {
+      const response = await loader({
+        request: new Request('http://localhost:3000/apply/123/communication-preference'),
+        context: {},
+        params: {},
+      });
+
+      const data = await response.json();
+
+      expect(data).toEqual({
+        communicationMethodEmail: {
+          id: 'email',
+          nameEn: 'Email',
+          nameFr: 'Adresse courriel',
+        },
+        preferredLanguages: [
+          {
+            id: 'en',
+            nameEn: 'English',
+            nameFr: 'Anglais',
+          },
+          {
+            id: 'fr',
+            nameEn: 'French',
+            nameFr: 'Français',
+          },
+        ],
+        preferredCommunicationMethods: [
+          {
+            id: 'email',
+            nameEn: 'Email',
+            nameFr: 'Adresse courriel',
+          },
+          {
+            id: 'mail',
+            nameEn: 'Postal mail',
+            nameFr: 'Courrier postal',
+          },
+        ],
+        id: '123',
+      });
+    });
+  });
+
+  describe('action()', () => {
+    it('should redirect to dental-insurance', async () => {
+      afterEach(() => {
+        vi.clearAllMocks();
+      });
+
+      const formData = new FormData();
+      formData.append('preferredMethod', 'mail');
+      formData.append('preferredLanguage', 'fr');
+
+      const response = await action({
+        request: new Request('http://localhost:3000/apply/123/communication-preference', { method: 'POST', body: formData }),
+        context: {},
+        params: {},
+      });
+
+      expect(response).toEqual(redirect('/apply/123/dental-insurance', { headers: { 'Set-Cookie': 'some-set-cookie-header' } }));
+    });
+
+    it('should validate required preferred method', async () => {
+      afterEach(() => {
+        vi.clearAllMocks();
+      });
+
+      const formData = new FormData();
+      formData.append('preferredLanguage', 'fr');
+
+      const response = await action({
+        request: new Request('http://localhost:3000/apply/123/communication-preference', { method: 'POST', body: formData }),
+        context: {},
+        params: {},
+      });
+
+      const data = await response.json();
+      expect(response.status).toBe(200);
+      expect(data.errors).toHaveProperty('preferredMethod');
+    });
+
+    it('should validate required email field', async () => {
+      afterEach(() => {
+        vi.clearAllMocks();
+      });
+
+      const formData = new FormData();
+      formData.append('preferredMethod', 'email');
+      formData.append('email', '');
+      formData.append('preferredLanguage', 'fr');
+
+      const response = await action({
+        request: new Request('http://localhost:3000/apply/123/communication-preference', { method: 'POST', body: formData }),
+        context: {},
+        params: {},
+      });
+
+      const data = await response.json();
+      expect(response.status).toBe(200);
+      expect(data.errors).toHaveProperty('email');
+    });
+
+    it('should validate required mismatched email addresses', async () => {
+      afterEach(() => {
+        vi.clearAllMocks();
+      });
+
+      const formData = new FormData();
+      formData.append('preferredMethod', 'email');
+      formData.append('email', 'john@gmail.com');
+      formData.append('confirmEmail', 'johndoe@gmail.com');
+      formData.append('preferredLanguage', 'fr');
+
+      const response = await action({
+        request: new Request('http://localhost:3000/apply/123/communication-preference', { method: 'POST', body: formData }),
+        context: {},
+        params: {},
+      });
+
+      const data = await response.json();
+      expect(response.status).toBe(200);
+      expect(data.errors).toHaveProperty('confirmEmail');
+    });
+  });
+});
