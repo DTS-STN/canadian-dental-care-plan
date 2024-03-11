e96451724a976124e55b25d7ee032736f52a030a
Shawn <azuneko1110@gmail.com>
2024-03-11 13:45:11 -0400

    refactor dental insurance route

--- frontend/app/routes/_public+/apply+/$id+/dental-insurance.tsx
+++ frontend/app/routes/_public+/apply+/$id+/dental-insurance.tsx
@@ -1,8 +1,8 @@
-import { ReactNode } from 'react';
+import { useEffect } from 'react';
 
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
-import { Form, MetaFunction, useLoaderData } from '@remix-run/react';
+import { Form, MetaFunction, useActionData, useLoaderData } from '@remix-run/react';
 
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,21 +10,23 @@ import { Trans, useTranslation } from 'react-i18next';
 import { z } from 'zod';
 
 import { Button, ButtonLink } from '~/components/buttons';
+import { CollapsibleDetails } from '~/components/collapsible';
+import { ErrorSummary, createErrorSummaryItems, hasErrors, scrollAndFocusToErrorSummary } from '~/components/error-summary';
 import { InputRadios } from '~/components/input-radios';
 import { getApplyFlow } from '~/routes-flow/apply-flow';
 import { getLookupService } from '~/services/lookup-service.server';
-import { getNameByLanguage, getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { mergeMeta } from '~/utils/meta-utils';
 
 export const handle = {
   i18nNamespaces: getTypedI18nNamespaces('apply', 'gcweb'),
   pageIdentifier: 'CDCP-1115',
-  pageTitleI18nKey: 'apply:dental-insurance-question.title',
+  pageTitleI18nKey: 'apply:dental-insurance.title',
 };
 
 export const meta: MetaFunction<typeof loader> = mergeMeta((args) => {
   const { t } = useTranslation(handle.i18nNamespaces);
-  return [{ title: t('gcweb:meta.title.template', { title: t('apply:dental-insurance-question.title') }) }];
+  return [{ title: t('gcweb:meta.title.template', { title: t('apply:dental-insurance.title') }) }];
 });
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
@@ -54,94 +56,93 @@ export async function action({ request, params }: ActionFunctionArgs) {
     state: { dentalInsurance: parsedDataResult.data },
   });
 
-  return redirect(`/apply/${id}/confirm`, sessionResponseInit);
+  return redirect(`/apply/${id}/federal-provincial-territorial-benefits`, sessionResponseInit);
 }
 
 export default function AccessToDentalInsuranceQuestion() {
-  const { options, state } = useLoaderData<typeof loader>();
-  const { i18n, t } = useTranslation(handle.i18nNamespaces);
+  const { options, state, id } = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+  const { t } = useTranslation(handle.i18nNamespaces);
+  const errorSummaryId = 'error-summary';
+
+  useEffect(() => {
+    if (actionData?.formData && hasErrors(actionData.formData)) {
+      scrollAndFocusToErrorSummary(errorSummaryId);
+    }
+  }, [actionData]);
+
+  function getErrorMessage(errorI18nKey?: string): string | undefined {
+    if (!errorI18nKey) return undefined;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return t(`dental-insurance.error-message.${errorI18nKey}` as any);
+  }
+
+  const errorMessages = {
+    dentalInsurance: getErrorMessage(actionData?.errors.dentalInsurance?._errors[0]),
+  };
+
+  console.log('error: ', errorMessages);
+
+  const errorSummaryItems = createErrorSummaryItems(errorMessages);
 
   const helpMessage = (
     <>
       <ul className="mb-4 list-disc">
-        <li>
-          <Trans ns={handle.i18nNamespaces} i18nKey="apply:dental-insurance-question.list.employer" />
-        </li>
-        <li>
-          <Trans ns={handle.i18nNamespaces} i18nKey="apply:dental-insurance-question.list.pension" />
-        </li>
-        <li>
-          <Trans ns={handle.i18nNamespaces} i18nKey="apply:dental-insurance-question.list.dental-coverage" />
-        </li>
+        <li>{t('dental-insurance.list.employment')}</li>
+        <li>{t('dental-insurance.list.pension')}</li>
+        <li>{t('dental-insurance.list.purchased')}</li>
+        <li>{t('dental-insurance.list.professional')}</li>
         <li className="list-none">
-          <Details id={t('apply:dental-insurance-question.detail.additional-info.title')} title={t('apply:dental-insurance-question.detail.additional-info.title')}>
+          <CollapsibleDetails id={t('dental-insurance.detail.additional-info.title')} summary={t('dental-insurance.detail.additional-info.title')}>
             <div>
-              <p>{t('apply:dental-insurance-question.detail.additional-info.eligible')}</p>
+              <Trans ns={handle.i18nNamespaces} i18nKey="dental-insurance.detail.additional-info.not-eligible" />
+              <p className="mt-4">{t('dental-insurance.detail.additional-info.eligible')}</p>
               <ul className="mb-4 list-disc pl-6">
-                <li>{t('apply:dental-insurance-question.detail.additional-info.list.opted')}</li>
-                <li>{t('apply:dental-insurance-question.detail.additional-info.list.cannot-opt')}</li>
+                <li>{t('dental-insurance.detail.additional-info.list.opted')}</li>
+                <li>{t('dental-insurance.detail.additional-info.list.cannot-opt')}</li>
               </ul>
-              <Trans ns={handle.i18nNamespaces} i18nKey="apply:dental-insurance-question.detail.additional-info.not-eligible" />
             </div>
-          </Details>
+          </CollapsibleDetails>
         </li>
       </ul>
     </>
   );
 
   return (
-    <Form method="post">
-      {options.length > 0 && (
-        <div className="my-6">
-          {options.length > 0 && (
-            <InputRadios
-              id="dental-insurance"
-              name="dentalInsurance"
-              legend={t('apply:dental-insurance-question.legend')}
-              options={options.map((option) => ({
-                children: (
-                  <div>
-                    <strong>{getNameByLanguage(i18n.language, option)}</strong>
-                    {`, ${option.id === 'yes' ? t('apply:dental-insurance-question.option-yes') : t('apply:dental-insurance-question.option-no')}`}
-                  </div>
-                ),
-                value: option.id,
-                defaultChecked: state.dentalInsurance?.dentalInsurance === option.id,
-              }))}
-              helpMessagePrimary={helpMessage}
-              helpMessagePrimaryClassName="pl-8 text-black"
-            />
-          )}
+    <>
+      {errorSummaryItems.length > 0 && <ErrorSummary id={errorSummaryId} errors={errorSummaryItems} />}
+      <Form method="post">
+        {options.length > 0 && (
+          <div className="my-6">
+            {options.length > 0 && (
+              <InputRadios
+                id="dental-insurance"
+                name="dentalInsurance"
+                legend={t('dental-insurance.legend')}
+                options={options.map((option) => ({
+                  children: <Trans ns={handle.i18nNamespaces}>{`dental-insurance.option-${option.id}`}</Trans>,
+                  value: option.id,
+                  defaultChecked: state.dentalInsurance?.dentalInsurance === option.id,
+                }))}
+                helpMessagePrimary={helpMessage}
+                helpMessagePrimaryClassName="pl-8 text-black"
+                required={errorSummaryItems.length > 0}
+                errorMessage={errorMessages.dentalInsurance}
+              />
+            )}
+          </div>
+        )}
+        <div className="flex flex-wrap items-center gap-3">
+          <ButtonLink type="button" variant="alternative" to={`/apply/${id}/communication-preference`}>
+            <FontAwesomeIcon icon={faChevronLeft} className="me-3 block size-4" />
+            {t('dental-insurance.button.back')}
+          </ButtonLink>
+          <Button type="submit" variant="primary">
+            {t('dental-insurance.button.continue')}
+            <FontAwesomeIcon icon={faChevronRight} className="me-3 block size-4" />
+          </Button>
         </div>
-      )}
-      <div className="flex flex-wrap items-center gap-3">
-        <ButtonLink id="back-button" to="/apply">
-          <FontAwesomeIcon icon={faChevronLeft} className="me-3 block size-4" />
-          {t('apply:dental-insurance-question.button.back')}
-        </ButtonLink>
-        <Button variant="primary" id="continue-button">
-          {t('apply:dental-insurance-question.button.continue')}
-          <FontAwesomeIcon icon={faChevronRight} className="ms-3 block size-4" />
-        </Button>
-      </div>
-    </Form>
-  );
-}
-
-interface DetailsProps {
-  id: string;
-  title: string;
-  children: ReactNode;
-}
-
-// TODO: Temporary, will be replaced by reusable component
-function Details({ id, title, children }: DetailsProps) {
-  return (
-    <details id={id} className="mt-6">
-      <summary className="mb-2 text-blue-700">
-        <p className="-mt-6 ml-6 underline">{title}</p>
-      </summary>
-      <div className="border-l-4 border-gray-400 py-4 pl-4">{children}</div>
-    </details>
+      </Form>
+    </>
   );
 }
