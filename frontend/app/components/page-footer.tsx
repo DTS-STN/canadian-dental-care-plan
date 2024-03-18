8cfce61554f39826b62e7b6d7e883e3e087fe548
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-03-18 20:16:38 -0300

    Update footer content

--- frontend/app/components/page-footer.tsx
+++ frontend/app/components/page-footer.tsx
@@ -8,16 +8,10 @@ export function PageFooter() {
     <footer id="wb-info" className="bg-stone-50 print:hidden">
       <div className="bg-gray-700 text-white">
         <section className="container py-6">
-          <h2 className="mb-4">My Service Canada Account</h2>
+          <h2 className="mb-4">{t('gcweb:footer.main-band.header')}</h2>
           <div className="grid gap-x-4 gap-y-2 text-sm sm:grid-cols-3">
-            <Link className="hover:underline" to="https://example.com/contact-us">
-              Contact Us
-            </Link>
-            <Link className="hover:underline" to="https://example.com/link-1">
-              [Link1]
-            </Link>
-            <Link className="hover:underline" to="https://example.com/link-2">
-              [Link2]
+            <Link className="hover:underline" to={t('gcweb:footer.main-band.links.contact-us.href')}>
+              {t('gcweb:footer.main-band.links.contact-us.content')}
             </Link>
           </div>
         </section>
