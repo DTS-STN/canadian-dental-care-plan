296c49d5403bd2fdcb1750147810e83d2abe4148
Lei Ye <lei.ye@hrsdc-rhdcc.gc.ca>
2024-03-19 14:17:36 -0400

    update content of Data unavailable

--- frontend/app/routes/$lang+/_protected+/data-unavailable.tsx
+++ frontend/app/routes/$lang+/_protected+/data-unavailable.tsx
@@ -1,12 +1,11 @@
 import { json } from '@remix-run/node';
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
 
 import { Trans, useTranslation } from 'react-i18next';
 
+import { ButtonLink } from '~/components/buttons';
 import { InlineLink } from '~/components/inline-link';
 import { getRaoidcService } from '~/services/raoidc-service.server';
-import { getClientEnv } from '~/utils/env-utils';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { getFixedT } from '~/utils/locale-utils.server';
 import { mergeMeta } from '~/utils/meta-utils';
@@ -30,36 +29,41 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const raoidcService = await getRaoidcService();
   await raoidcService.handleSessionValidation(request);
 
-  const { SCCH_BASE_URI } = getClientEnv();
-
   const t = await getFixedT(request, handle.i18nNamespaces);
   const meta = { title: t('gcweb:meta.title.template', { title: t('data-unavailable:page-title') }) };
 
-  return json({ meta, SCCH_BASE_URI });
+  return json({ meta });
 }
 
 export default function DataUnavailable() {
   const { t } = useTranslation(handle.i18nNamespaces);
-  const { SCCH_BASE_URI } = useLoaderData<typeof loader>();
   const userOrigin = useUserOrigin();
 
   const doyouqualify = <InlineLink to={t('do-you-qualify.href')} />;
   const howtoapply = <InlineLink to={t('how-to-apply.href')} />;
-  const contactus = <InlineLink to={t('contact-us.href', { baseUri: SCCH_BASE_URI })} />;
+  const contactus = <InlineLink to={t('contact-us.href')} />;
+  const statuschecker = <InlineLink to={t('status-checker.href')} />;
 
   return (
     <>
       <p className="mb-6">
         <Trans ns={handle.i18nNamespaces} i18nKey="service-eligible" components={{ doyouqualify }} />
-      </p>
-      <p className="mb-6">
         <Trans ns={handle.i18nNamespaces} i18nKey="more-information" components={{ howtoapply }} />
       </p>
       <p className="mb-6">
         <Trans ns={handle.i18nNamespaces} i18nKey="other-enquiry" components={{ contactus }} />
       </p>
-      <p className="mb-8">{t('service-delay')}</p>
-      {userOrigin && <InlineLink to={userOrigin.to}>{userOrigin.text}</InlineLink>}
+      <p className="mb-6">
+        <Trans ns={handle.i18nNamespaces} i18nKey="service-delay" components={{ statuschecker }} />
+      </p>
+
+      {userOrigin && (
+        <div className="flex flex-wrap items-center gap-3">
+          <ButtonLink id="back-button" to={userOrigin.to}>
+            {t('back-button')}
+          </ButtonLink>
+        </div>
+      )}
     </>
   );
 }
