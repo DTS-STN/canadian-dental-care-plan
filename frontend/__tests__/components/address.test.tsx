2a7e21872cc0872283801a935d0f42d85e7e323a
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2024-03-20 13:52:38 -0400

    Fixed test for spacing

--- frontend/__tests__/components/address.test.tsx
+++ frontend/__tests__/components/address.test.tsx
@@ -11,13 +11,13 @@ describe('Address', () => {
   it('should render country when not a Canadian address', async () => {
     render(<Address address="123 Fake Street" city="Beverly Hills" provinceState="CA" postalZipCode="90210" country="USA" />);
     const actual = screen.getByTestId('address-id');
-    expect(actual.textContent).toBe('123 Fake Street\nBeverly Hills CA 90210\nUSA');
+    expect(actual.textContent).toBe('123 Fake Street\nBeverly Hills CA  90210\nUSA');
   });
 
   it('should not render country when address is Canadian', async () => {
     render(<Address address="123 Fake Street" city="Ottawa" provinceState="ON" postalZipCode="K2K 2K2" country="Canada" />);
     const actual = screen.getByTestId('address-id');
-    expect(actual.textContent).toBe('123 Fake Street\nOttawa ON K2K 2K2');
+    expect(actual.textContent).toBe('123 Fake Street\nOttawa ON  K2K 2K2');
   });
 
   it('should not have any a11y issues', async () => {
