499f18a8667297e3053cb96be44b7cce3b3ed1fa
Sébastien Comeau <114004123+sebastien-comeau@users.noreply.github.com>
2024-03-20 13:13:37 -0300

    Replace apply index with terms and conditions (#738)

--- frontend/app/routes/$lang+/_public+/apply+/$id+/_route.tsx
+++ frontend/app/routes/$lang+/_public+/apply+/$id+/_route.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 
-import { Outlet, useNavigate } from '@remix-run/react';
+import { LoaderFunctionArgs, json } from '@remix-run/node';
+import { Outlet, useLoaderData, useNavigate } from '@remix-run/react';
 
 import { handle as layoutHandle } from '~/routes/$lang+/_public+/apply+/_route';
+import { getLocale } from '~/utils/locale-utils.server';
 import { RouteHandleData } from '~/utils/route-utils';
 
 export const handle = {
@@ -10,18 +12,24 @@ export const handle = {
   i18nNamespaces: [...layoutHandle.i18nNamespaces],
 } as const satisfies RouteHandleData;
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const locale = await getLocale(request);
+  return json({ locale });
+}
+
 /**
  * The parent route of all /apply/{id}/* routes, used to
  * redirect to /apply if the flow has not yet been initialized.
  */
 export default function Route() {
+  const { locale } = useLoaderData<typeof loader>();
   const navigate = useNavigate();
 
   useEffect(() => {
     // redirect to start if the flow has not yet been initialized
     const flowState = sessionStorage.getItem('flow.state');
-    if (flowState !== 'active') navigate('/apply');
-  }, [navigate]);
+    if (flowState !== 'active') navigate(`/${locale}/apply`, { replace: true });
+  }, [locale, navigate]);
 
   return <Outlet />;
 }
