bc28993ff9a324921972cf66bdb316d2062f199b
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-03-22 13:35:49 -0230

    AB#3066 -- custom audit transport (#779)

--- frontend/app/utils/logging.server.ts
+++ frontend/app/utils/logging.server.ts
@@ -1,16 +1,18 @@
+import os from 'node:os';
 import { LeveledLogMethod, Logger, createLogger, format, transports } from 'winston';
+import 'winston-daily-rotate-file';
 import { z } from 'zod';
 
 // see https://github.com/winstonjs/winston?tab=readme-ov-file#using-custom-logging-levels
-const logLevels = { audit: 0, error: 1, warn: 2, info: 3, debug: 4, trace: 5 };
+const logLevels = { audit: 0, error: 1, warn: 2, info: 3, debug: 4, trace: 5 } as const;
 
-const isValidLogLevel = (val: string) => Object.keys(logLevels).includes(val);
-
-const logLevel = z
-  .string()
-  .refine(isValidLogLevel, { message: 'Invalid log level', path: ['process.env.LOG_LEVEL'] })
-  .default('info')
-  .parse(process.env.LOG_LEVEL);
+// prettier-ignore
+const env = {
+  logLevel: z.string().refine(((val: string) => Object.keys(logLevels).includes(val)), { message: 'Invalid log level' }).default('info').parse(process.env.LOG_LEVEL),
+  auditLogEnabled: z.string().transform((val: string) => val === 'true').default('true').parse(process.env.AUDIT_LOG_ENABLED),
+  auditLogDirname: z.string().trim().min(1, { message: 'Invalid audit log directory name' }).default('logs').parse(process.env.AUDIT_LOG_DIRNAME),
+  auditLogFilename: z.string().trim().min(1, { message: 'Invalid audit log file name' }).default('audit').parse(process.env.AUDIT_LOG_FILENAME),
+} as const;
 
 /**
  * Formats a log category string to be a fixed length. When the category string
@@ -26,11 +28,8 @@ function formatCategory(category: string, size: number) {
  * Returns a logger for the specified logging category.
  */
 export const getLogger = (category: string) => {
-  // required so typescript knows about log.audit(..), log.trace(..), etc
-  type CustomLogger = Logger & { [level in keyof typeof logLevels]: LeveledLogMethod };
-
-  return createLogger({
-    level: logLevel,
+  const logger = createLogger({
+    level: env.logLevel,
     levels: logLevels,
     format: format.combine(
       format.timestamp(),
@@ -38,5 +37,27 @@ export const getLogger = (category: string) => {
       format.printf((info) => `${info.timestamp} ${info.level.toUpperCase().padStart(7)} --- [${formatCategory(category, 25)}]: ${info.stack ?? info.message}`),
     ),
     transports: [new transports.Console()],
-  }) as CustomLogger;
+  });
+
+  //
+  // Audit logs are persisted to disk to ensure that we
+  // can retain a history record of important system events
+  //
+  if (env.auditLogEnabled) {
+    logger.add(
+      new transports.DailyRotateFile({
+        level: 'audit',
+        dirname: env.auditLogDirname,
+        filename: env.auditLogFilename,
+        format: format.printf((info) => info.message),
+        extension: `_${os.hostname()}.log`,
+        utc: true,
+      }),
+    );
+  }
+
+  // required so typescript knows about log.audit(..), log.trace(..), etc
+  type LeveledLogMethods = { [level in keyof typeof logLevels]: LeveledLogMethod };
+
+  return logger as Logger & LeveledLogMethods;
 };
