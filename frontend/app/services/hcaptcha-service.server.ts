eabbf1751d3d2c422ee4be93ae089a1dd25c778c
Nicholas Ly <55502055+nicholas-ly@users.noreply.github.com>
2024-03-23 14:47:18 -0400

    AB#2977 -- Refactor hCaptchaResponse validation, allow execution on failure; add instrumentation (#783)

--- frontend/app/services/hcaptcha-service.server.ts
+++ frontend/app/services/hcaptcha-service.server.ts
@@ -1,5 +1,8 @@
 import moize from 'moize';
+import { z } from 'zod';
 
+import { getAuditService } from '~/services/audit-service.server';
+import { getInstrumentationService } from '~/services/instrumentation-service.server';
 import { getEnv } from '~/utils/env.server';
 import { getLogger } from '~/utils/logging.server';
 
@@ -19,6 +22,10 @@ function createHCaptchaService() {
    * @see https://docs.hcaptcha.com/#verify-the-user-response-server-side
    */
   async function verifyHCaptchaResponse(hCaptchaResponse: string) {
+    const instrumentationService = getInstrumentationService();
+
+    getAuditService().audit('hcaptcha.verify', { userId: 'anonymous' });
+
     const url = new URL(HCAPTCHA_VERIFY_URL);
     url.searchParams.set('response', hCaptchaResponse);
     url.searchParams.set('secret', HCAPTCHA_SECRET_KEY);
@@ -26,6 +33,7 @@ function createHCaptchaService() {
     const response = await fetch(url, { method: 'POST' });
 
     if (!response.ok) {
+      instrumentationService.countHttpStatus('http.client.hcaptcha.posts', response.status);
       log.error('%j', {
         message: 'Failed to verify hCaptcha',
         status: response.status,
@@ -37,7 +45,22 @@ function createHCaptchaService() {
       throw new Error(`Failed to verify hCaptcha: ${response.status}, Status Text: ${response.statusText}`);
     }
 
-    return response.json();
+    const verifyResultSchema = z.object({
+      success: z.boolean(),
+      challenge_ts: z.string().datetime(),
+      hostname: z.string(),
+      'error-codes': z.array(z.string()).optional(),
+      score: z.number().optional(),
+    });
+
+    const verifyResult = verifyResultSchema.parse(await response.json());
+    if (verifyResult.success) {
+      instrumentationService.countHttpStatus('http.client.hcaptcha.posts.success', 200);
+      log.info(`hCaptcha verification successful: [${JSON.stringify(verifyResult)}]`);
+    } else {
+      instrumentationService.countHttpStatus('http.client.hcaptcha.posts.failed', 200);
+      log.warn(`hCaptcha verification failed: [${JSON.stringify(verifyResult)}]`);
+    }
   }
 
   return { verifyHCaptchaResponse };
