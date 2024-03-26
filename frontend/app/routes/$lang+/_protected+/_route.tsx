7fa1a04338113d49a3ee0d97e23a70749984239c
Frank <48450599+Fbasham@users.noreply.github.com>
2024-03-26 11:32:11 -0400

    3035 modify timeouts (#811)

--- frontend/app/routes/$lang+/_protected+/_route.tsx
+++ frontend/app/routes/$lang+/_protected+/_route.tsx
@@ -1,11 +1,19 @@
-import { Outlet, isRouteErrorResponse, useRouteError } from '@remix-run/react';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { Outlet, isRouteErrorResponse, json, useLoaderData, useRouteError } from '@remix-run/react';
 
 import { NotFoundError, ProtectedLayout, ServerError, i18nNamespaces as layoutI18nNamespaces } from '~/components/layouts/protected-layout';
+import SessionTimeout from '~/components/session-timeout';
+import { getPublicEnv } from '~/utils/env.server';
 import type { RouteHandleData } from '~/utils/route-utils';
 
 export const handle = {
   i18nNamespaces: [...layoutI18nNamespaces],
 } as const satisfies RouteHandleData;
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const env = getPublicEnv();
+  return json({ env });
+}
 
 export function ErrorBoundary() {
   const error = useRouteError();
@@ -22,8 +30,10 @@ export function ErrorBoundary() {
 }
 
 export default function Layout() {
+  const { env } = useLoaderData<typeof loader>();
   return (
     <ProtectedLayout>
+      <SessionTimeout promptBeforeIdle={env.SESSION_TIMEOUT_PROMPT_SECONDS * 1000} timeout={env.SESSION_TIMEOUT_SECONDS * 1000} navigateTo="/auth/logout" />
       <Outlet />
     </ProtectedLayout>
   );
