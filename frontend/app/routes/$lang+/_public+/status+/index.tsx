43b33ead12a73f08c84081b28dd883957d0a4dd5
shawn320 <azuneko1110@gmail.com>
2024-03-26 21:24:53 -0400

    add content

--- frontend/app/routes/$lang+/_public+/status+/index.tsx
+++ frontend/app/routes/$lang+/_public+/status+/index.tsx
@@ -2,10 +2,13 @@ import type { ActionFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Form, useActionData } from '@remix-run/react';
 
+import { Trans, useTranslation } from 'react-i18next';
 import { z } from 'zod';
 
 import pageIds from '../../page-ids.json';
 import { Button } from '~/components/buttons';
+import { Collapsible } from '~/components/collapsible';
+import { InlineLink } from '~/components/inline-link';
 import { InputField } from '~/components/input-field';
 import { PublicLayout } from '~/components/layouts/public-layout';
 import { getApplicationStatusService } from '~/services/application-status-service.server';
@@ -49,10 +52,60 @@ export async function action({ context: { session }, params, request }: ActionFu
 
 export default function StatusChecker() {
   const actionData = useActionData<typeof action>();
+  const { t } = useTranslation(handle.i18nNamespaces);
+
+  const hcaptchaTermsOfService = <InlineLink to={t('status:links.hcaptcha')} />;
+  const microsoftDataPrivacyPolicy = <InlineLink to={t('status:links.microsoft-data-privacy-policy')} />;
+  const microsoftServiceAgreement = <InlineLink to={t('status:links.microsoft-service-agreement')} />;
+  const fileacomplaint = <InlineLink to={t('status:links.file-complaint')} />;
 
   // TODO use <PublicLayout> for now
   return (
     <PublicLayout>
+      <h2 className="font-bold">{t('status:status-checker-heading')}</h2>
+      <p>{t('status:status-checker-content')}</p>
+      <h2 className="mt-4 font-bold">{t('status:online-status-heading')}</h2>
+      <p>{t('status:online-status-content')}</p>
+      <Collapsible summary={t('status:terms-of-use.summary')} className="mt-8">
+        <h2 className="font-bold">{t('status:terms-of-use.heading')}</h2>
+        <Trans ns={handle.i18nNamespaces} i18nKey="status:terms-of-use.legal-terms" />
+        <p className="my-4">{t('status:terms-of-use.access-terms')}</p>
+        <p>{t('status:terms-of-use.usage-terms')}</p>
+        <p className="my-4">{t('status:terms-of-use.terms-rejection-policy')}</p>
+        <p>{t('status:terms-of-use.esdc-definition-clarification')}</p>
+        <p className="mt-4 font-bold">{t('status:terms-of-use.status-checker.heading')}</p>
+        <ul className="list-disc pl-8">
+          <li>{t('status:terms-of-use.status-checker.self-agreement')}</li>
+          <li>{t('status:terms-of-use.status-checker.on-behalf-of-someone-else')}</li>
+          <li>{t('status:terms-of-use.status-checker.at-your-own-risk')}</li>
+          <li>{t('status:terms-of-use.status-checker.only-use')}</li>
+          <li>{t('status:terms-of-use.status-checker.maintenance')}</li>
+          <li>{t('status:terms-of-use.status-checker.inactive')}</li>
+          <li>
+            <Trans ns={handle.i18nNamespaces} i18nKey="status:terms-of-use.status-checker.msdc" components={{ microsoftServiceAgreement }} />
+          </li>
+          <li>
+            <Trans ns={handle.i18nNamespaces} i18nKey="status:terms-of-use.status-checker.antibot" components={{ hcaptchaTermsOfService }} />
+          </li>
+        </ul>
+        <h2 className="mt-4 font-bold">{t('status:terms-of-use.disclaimers.heading')}</h2>
+        <p>{t('status:terms-of-use.disclaimers.disclaimers')}</p>
+        <ol className="list-decimal pl-8">
+          <li>{t('status:terms-of-use.disclaimers.external-factors-disclaimer')}</li>
+          <li>{t('status:terms-of-use.disclaimers.non-acceptance')}</li>
+          <li>{t('status:terms-of-use.disclaimers.non-compliance')}</li>
+        </ol>
+        <p>{t('status:terms-of-use.disclaimers.esdc-liability-indemnification')}</p>
+        <h2 className="mt-4 font-bold">{t('status:terms-of-use.changes-to-these-terms-of-use.heading')}</h2>
+        <p>{t('status:terms-of-use.changes-to-these-terms-of-use.esdc-terms-amendment-policy')}</p>
+      </Collapsible>
+      <Collapsible summary={t('status:privacy-notice-statement.summary')} className="my-8">
+        <p>{t('status:privacy-notice-statement.collection-of-use')}</p>
+        <p className="my-4">{t('status:privacy-notice-statement.provided-information')}</p>
+        <Trans ns={handle.i18nNamespaces} i18nKey="status:privacy-notice-statement.third-party-provider" components={{ microsoftDataPrivacyPolicy }} />
+        <p className="my-4">{t('status:privacy-notice-statement.personal-information')}</p>
+        <Trans ns={handle.i18nNamespaces} i18nKey="status:privacy-notice-statement.report-a-concern" components={{ fileacomplaint }} />
+      </Collapsible>
       <Form method="post" noValidate>
         <div className="space-y-6">
           <InputField id="sin" name="sin" label="Please enter your SIN" required />
