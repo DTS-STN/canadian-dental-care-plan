9b636eed59864fd41fcb7ea216bc97db030e8fb7
olivierroyESDC <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-03-28 09:53:57 -0400

    Work in progress to refine the phone page

--- frontend/app/routes/$lang+/_protected+/personal-information+/phone-number+/edit.tsx
+++ frontend/app/routes/$lang+/_protected+/personal-information+/phone-number+/edit.tsx
@@ -13,11 +13,13 @@ import { Button, ButtonLink } from '~/components/buttons';
 import { ErrorSummary, createErrorSummaryItems, hasErrors, scrollAndFocusToErrorSummary } from '~/components/error-summary';
 import { InputField } from '~/components/input-field';
 import { getInstrumentationService } from '~/services/instrumentation-service.server';
+import { getPersonalInformationService } from '~/services/personal-information-service.server';
 import { getRaoidcService } from '~/services/raoidc-service.server';
 import { getUserService } from '~/services/user-service.server';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { getFixedT, redirectWithLocale } from '~/utils/locale-utils.server';
 import { mergeMeta } from '~/utils/meta-utils';
+import type { UserinfoToken } from '~/utils/raoidc-utils.server';
 import type { RouteHandleData } from '~/utils/route-utils';
 import { getTitleMetaTags } from '~/utils/seo-utils';
 
@@ -40,7 +42,7 @@ export async function loader({ context: { session }, request }: LoaderFunctionAr
   const instrumentationService = getInstrumentationService();
   const raoidcService = await getRaoidcService();
   const userService = getUserService();
-
+  const personalInformationService = await getPersonalInformationService();
   await raoidcService.handleSessionValidation(request, session);
 
   const userId = await userService.getUserId();
@@ -50,12 +52,15 @@ export async function loader({ context: { session }, request }: LoaderFunctionAr
     instrumentationService.countHttpStatus('phone-number.edit', 404);
     throw new Response(null, { status: 404 });
   }
-
+  const userInfoToken: UserinfoToken = session.get('userInfoToken');
+  const personailInformation = await personalInformationService.getPersonalInformationIntoSession(session, request, '/data-unavailable', userInfoToken.sin);
+  session.set('personailInformation', personailInformation);
   const t = await getFixedT(request, handle.i18nNamespaces);
   const meta = { title: t('gcweb:meta.title.template', { title: t('personal-information:phone-number.edit.page-title') }) };
 
   instrumentationService.countHttpStatus('phone-number.edit', 200);
-  return json({ meta, userInfo });
+
+  return json({ meta, userInfo, personailInformation });
 }
 
 export async function action({ context: { session }, request }: ActionFunctionArgs) {
@@ -69,6 +74,20 @@ export async function action({ context: { session }, request }: ActionFunctionAr
       .string()
       .min(1, { message: 'empty-phone-number' })
       .refine((val) => isValidPhoneNumber(val, 'CA'), { message: 'invalid-phone-format' }),
+
+    alternatePhoneNumber: z
+      .string()
+      .refine(
+        (val) => {
+          if (val.length === 0) {
+            return true;
+          } else {
+            return isValidPhoneNumber(val, 'CA');
+          }
+        },
+        { message: 'invalid-phone-format' },
+      )
+      .optional(),
   });
 
   const formData = Object.fromEntries(await request.formData());
@@ -83,9 +102,15 @@ export async function action({ context: { session }, request }: ActionFunctionAr
   }
 
   session.set('newPhoneNumber', parsedDataResult.data.phoneNumber);
+  session.set('newAlternatePhoneNumber', parsedDataResult.data.alternatePhoneNumber);
 
   instrumentationService.countHttpStatus('phone-number.confirm', 302);
+
+  //TODO Call the method to SAVE the new DATA.
+
   return redirectWithLocale(request, '/personal-information/phone-number/confirm');
+  //TODO use this redirect below once we can save here the new data. We will stop using the confirm page.
+  //return redirectWithSuccess(`/${locale}/personal-information`, 'personal-information:phone-number.confirm.updated-notification');
 }
 
 export default function PhoneNumberEdit() {
@@ -97,7 +122,9 @@ export default function PhoneNumberEdit() {
 
   const defaultValues = {
     phoneNumber: actionData?.formData.phoneNumber ?? userInfo.phoneNumber ?? '',
+    alternatePhoneNumber: actionData?.formData.alternatePhoneNumber ?? userInfo.alternatePhoneNumber ?? '',
   };
+  t;
 
   /**
    * Gets an error message based on the provided internationalization (i18n) key.
@@ -118,6 +145,7 @@ export default function PhoneNumberEdit() {
 
   const errorMessages = {
     phoneNumber: getErrorMessage(actionData?.errors.phoneNumber?._errors[0]),
+    alternatePhoneNumber: getErrorMessage(actionData?.errors.alternatePhoneNumber?._errors[0]),
   };
 
   const errorSummaryItems = createErrorSummaryItems(errorMessages);
@@ -130,20 +158,26 @@ export default function PhoneNumberEdit() {
 
   return (
     <>
-      <p className="mb-8 border-b border-gray-200 pb-8 text-lg text-gray-500">{t('personal-information:phone-number.edit.subtitle')}</p>
       {errorSummaryItems.length > 0 && <ErrorSummary id={errorSummaryId} errors={errorSummaryItems} />}
       <Form method="post" noValidate>
         <div className="my-6">
-          <p className="mb-4 text-red-600">{t('gcweb:asterisk-indicates-required-field')}</p>
-          <InputField id="phoneNumber" name="phoneNumber" type="tel" label={t('personal-information:phone-number.edit.component.phone')} required defaultValue={defaultValues.phoneNumber} errorMessage={errorMessages.phoneNumber} />
+          <InputField id="phoneNumber" name="phoneNumber" type="tel" label={t('personal-information:phone-number.edit.component.phone')} defaultValue={defaultValues.phoneNumber} errorMessage={errorMessages.phoneNumber} />
+          <InputField
+            id="alternatePhoneNumber"
+            name="alternatePhoneNumber"
+            type="tel"
+            label={t('personal-information:phone-number.edit.component.alternate-phone')}
+            defaultValue={defaultValues.alternatePhoneNumber}
+            errorMessage={errorMessages.alternatePhoneNumber}
+          />
         </div>
         <div className="flex flex-wrap items-center gap-3">
-          <Button id="submit" variant="primary">
-            {t('personal-information:phone-number.edit.button.save')}
-          </Button>
           <ButtonLink id="cancel" to="/personal-information">
             {t('personal-information:phone-number.edit.button.cancel')}
           </ButtonLink>
+          <Button id="submit" variant="primary">
+            {t('personal-information:phone-number.edit.button.save')}
+          </Button>
         </div>
       </Form>
     </>
