57812eb86c03fcdb44a77ad26cb8e56c81645161
Benoit Cloutier <77116011+nibivi77@users.noreply.github.com>
2024-03-28 13:56:31 -0400

    Rebase (#839)

---
+++ frontend/app/components/new-tab-indicator.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import { cn } from '~/utils/tw-utils';
+
+export function NewTabIndicator({ className, ...props }: Omit<ComponentProps<'span'>, 'children'>) {
+  const { t } = useTranslation('gcweb');
+  return (
+    <span className={cn('sr-only', className)} {...props}>
+      {/*Following whitespace is important to ensure the content's text is seperated for the screen-reader text*/}&#32;({t('screen-reader.new-tab')})
+    </span>
+  );
+}
