64ff954a74a9776af734cb73ebe5dd9470143846
Nicholas Ly <55502055+nicholas-ly@users.noreply.github.com>
2024-04-04 20:34:03 -0400

    AB#2977 -- Including 'remoteip' fied in /siteverify request to obtain the score (#904)

---
+++ frontend/app/utils/ip-address-utils.server.ts
@@ -0,0 +1,22 @@
+import { getEnv } from './env.server';
+
+/**
+ * Attempts to retrieve the client's IP address from the "X-Forwarded-For" header of the HTTP request.
+ *
+ * If the "X-Forwarded-For" header exists, the first element of the comma-separated header is extracted.
+ * This is because the header may contain a chain of IP addresses, representing intermediate proxies.
+ *
+ * In a development environment, this function will return "127.0.0.1" (localhost IP).
+ *
+ * @param request the incoming HTTP request object.
+ * @returns the trimmed IP address or null if the "X-Forwarded-For" header is missing.
+ */
+export function getClientIpAddress(request: Request): string | null {
+  const { NODE_ENV } = getEnv();
+  if (NODE_ENV === 'development') {
+    return '127.0.0.1';
+  }
+
+  const xForwardedForHeader = request.headers.get('X-Forwarded-For');
+  return xForwardedForHeader?.split(',')[0].trim() ?? null;
+}
