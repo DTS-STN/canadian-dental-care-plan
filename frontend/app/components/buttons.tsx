60ba309eca6e1e6752710e6d949036302f3ecb83
Sébastien Comeau <114004123+sebastien-comeau@users.noreply.github.com>
2024-04-09 12:20:29 -0300

    Fix button style (#966)

--- frontend/app/components/buttons.tsx
+++ frontend/app/components/buttons.tsx
@@ -13,15 +13,15 @@ const sizes = {
 };
 
 const variants = {
-  alternative: 'border border-gray-200 bg-white text-gray-900 hover:bg-gray-100 hover:text-blue-700',
-  default: 'border border-gray-300 bg-gray-200 text-slate-700 hover:bg-neutral-300',
-  dark: 'bg-gray-800 text-white hover:bg-gray-900',
-  green: 'bg-green-700 text-white hover:bg-green-800',
-  primary: 'bg-slate-700 text-white hover:bg-sky-800',
-  red: 'bg-red-700 text-white hover:bg-red-800',
+  alternative: 'border-gray-200 bg-white text-gray-900 hover:bg-gray-100 hover:text-blue-700',
+  default: 'border-gray-300 bg-gray-200 text-slate-700 hover:bg-neutral-300',
+  dark: 'border-gray-800 bg-gray-800 text-white hover:bg-gray-900 hover:bg-gray-900',
+  green: 'border-green-700 bg-green-700 text-white hover:bg-green-800 hover:bg-green-800',
+  primary: 'border-slate-700 bg-slate-700 text-white hover:bg-sky-800 hover:bg-sky-800',
+  red: 'border-red-700 bg-red-700 text-white hover:bg-red-800 hover:bg-red-800',
 };
 
-const baseClassName = 'inline-flex items-center justify-center rounded align-middle font-lato outline-offset-2';
+const baseClassName = 'inline-flex items-center justify-center rounded border align-middle font-lato outline-offset-2';
 
 export interface ButtonProps extends ComponentProps<'button'> {
   size?: keyof typeof sizes;
