1cf8cee4b4fbcf8da5a012ecbaf186e312e3f18a
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2024-04-10 14:45:09 -0400

    AB#2977 -- Refactoring to add and use custom useHCaptcha hook and route helper

---
+++ frontend/app/utils/hcaptcha-utils.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from 'react';
+
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+
+export function useHCaptcha() {
+  const captchaRef = useRef<HCaptcha>(null);
+
+  useEffect(() => {
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    if (captchaRef.current?.isReady()) {
+      captchaRef.current.execute();
+    } else {
+      timeoutId = setTimeout(() => {
+        captchaRef.current?.execute();
+      }, 500);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return { captchaRef };
+}
