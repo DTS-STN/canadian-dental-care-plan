a882d7dd7a698c860367076e58f8e461283f7bfd
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-04-10 17:04:34 -0300

    Push AA pageLoad event with with a transformed url

---
+++ frontend/app/utils/url-utils.ts
@@ -0,0 +1,12 @@
+import { validate } from 'uuid';
+
+export function removeUUIDSegmentsFromURL(url: string) {
+  const urlObj = new URL(url);
+  const pathname = urlObj.pathname;
+  const segments = pathname.split('/').filter((segment) => segment !== '');
+
+  const filteredSegments = segments.filter((segment) => !validate(segment));
+  urlObj.pathname = '/' + filteredSegments.join('/');
+
+  return urlObj.toString();
+}
