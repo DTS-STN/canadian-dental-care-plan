ba33eeda1548994f4c10f1f4264b985f1aa351e1
CanadianPhilipBarkow <154452024+CanadianPhilipBarkow@users.noreply.github.com>
2024-04-11 07:10:41 -0300

    Personal Information Service now called on personal information page (#995)
    * Personal Information Service now called on personal information page
    
    AB#3216
    
    Relevant test updates made.
    
    * Updated based on comments on PR 995
    
    Superfluous property check removed.

--- frontend/app/routes/$lang+/_protected+/personal-information+/index.tsx
+++ frontend/app/routes/$lang+/_protected+/personal-information+/index.tsx
@@ -9,16 +9,15 @@ import { useTranslation } from 'react-i18next';
 import pageIds from '../../page-ids.json';
 import { Address } from '~/components/address';
 import { InlineLink } from '~/components/inline-link';
-import { getAddressService } from '~/services/address-service.server';
+import { getPersonalInformationRouteHelpers } from '~/route-helpers/personal-information-route-helpers.server';
 import { getAuditService } from '~/services/audit-service.server';
 import { getLookupService } from '~/services/lookup-service.server';
 import { getRaoidcService } from '~/services/raoidc-service.server';
-import { getUserService } from '~/services/user-service.server';
 import { featureEnabled } from '~/utils/env.server';
 import { getNameByLanguage, getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { getFixedT } from '~/utils/locale-utils.server';
 import { mergeMeta } from '~/utils/meta-utils';
-import { IdToken } from '~/utils/raoidc-utils.server';
+import { IdToken, UserinfoToken } from '~/utils/raoidc-utils.server';
 import type { RouteHandleData } from '~/utils/route-utils';
 import { getTitleMetaTags } from '~/utils/seo-utils';
 
@@ -34,7 +33,7 @@ export const meta: MetaFunction<typeof loader> = mergeMeta(({ data }) => {
   return getTitleMetaTags(data.meta.title);
 });
 
-export async function loader({ context: { session }, request }: LoaderFunctionArgs) {
+export async function loader({ context: { session }, params, request }: LoaderFunctionArgs) {
   featureEnabled('update-personal-info');
 
   const raoidcService = await getRaoidcService();
@@ -43,28 +42,21 @@ export async function loader({ context: { session }, request }: LoaderFunctionAr
   const idToken: IdToken = session.get('idToken');
   getAuditService().audit('page-view.personal-information', { userId: idToken.sub });
 
-  const userService = getUserService();
-  const userId = await userService.getUserId();
-  const userInfo = await userService.getUserInfo(userId);
+  const userInfoToken: UserinfoToken = session.get('userInfoToken');
+  const personalInformationRouteHelpers = getPersonalInformationRouteHelpers();
+  const personalInformation = await personalInformationRouteHelpers.getPersonalInformation(userInfoToken, params, request, session);
   const countryList = await getLookupService().getAllCountries();
   const regionList = await getLookupService().getAllRegions();
-
-  if (!userInfo) {
-    throw new Response(null, { status: 404 });
-  }
-
-  const preferredLanguage = userInfo.preferredLanguage && (await getLookupService().getPreferredLanguage(userInfo.preferredLanguage));
-  const homeAddressInfo = userInfo.homeAddress && (await getAddressService().getAddressInfo(userId, userInfo.homeAddress));
-  const mailingAddressInfo = userInfo.mailingAddress && (await getAddressService().getAddressInfo(userId, userInfo.mailingAddress));
+  const preferredLanguage = personalInformation.preferredLanguageId ? await getLookupService().getPreferredLanguage(personalInformation.preferredLanguageId) : undefined;
 
   const t = await getFixedT(request, handle.i18nNamespaces);
   const meta = { title: t('gcweb:meta.title.template', { title: t('personal-information:index.page-title') }) };
 
-  return json({ countryList, homeAddressInfo, mailingAddressInfo, meta, preferredLanguage, regionList, user: userInfo });
+  return json({ preferredLanguage, countryList, personalInformation, meta, regionList });
 }
 
 export default function PersonalInformationIndex() {
-  const { user, preferredLanguage, homeAddressInfo, mailingAddressInfo, countryList, regionList } = useLoaderData<typeof loader>();
+  const { personalInformation, preferredLanguage, countryList, regionList } = useLoaderData<typeof loader>();
   const { i18n, t } = useTranslation(handle.i18nNamespaces);
   const params = useParams();
 
@@ -72,7 +64,7 @@ export default function PersonalInformationIndex() {
     <>
       <p className="mb-8 text-lg text-gray-500">{t('personal-information:index.on-file')}</p>
       <dl className="mt-6 divide-y border-y">
-        <DescriptionListItem term={t('personal-information:index.full-name')}>{`${user.firstName} ${user.lastName}`}</DescriptionListItem>
+        <DescriptionListItem term={t('personal-information:index.full-name')}>{`${personalInformation.firstName} ${personalInformation.lastName}`}</DescriptionListItem>
         <DescriptionListItem term={t('personal-information:index.preferred-language')}>
           <p>{preferredLanguage ? getNameByLanguage(i18n.language, preferredLanguage) : t('personal-information:index.no-preferred-language-on-file')}</p>
           <p>
@@ -82,13 +74,13 @@ export default function PersonalInformationIndex() {
           </p>
         </DescriptionListItem>
         <DescriptionListItem term={t('personal-information:index.home-address')}>
-          {homeAddressInfo ? (
+          {personalInformation.homeAddress ? (
             <Address
-              address={homeAddressInfo.address}
-              city={homeAddressInfo.city}
-              provinceState={regionList.find((region) => region.provinceTerritoryStateId === homeAddressInfo.province)?.abbr}
-              postalZipCode={homeAddressInfo.postalCode}
-              country={countryList.find((country) => country.countryId === homeAddressInfo.country)?.[i18n.language === 'fr' ? 'nameFr' : 'nameEn'] ?? ' '}
+              address={personalInformation.homeAddress.streetName ?? ''}
+              city={personalInformation.homeAddress.cityName ?? ''}
+              provinceState={regionList.find((region) => region.provinceTerritoryStateId === personalInformation.homeAddress!.provinceTerritoryStateId)?.abbr}
+              postalZipCode={personalInformation.homeAddress.postalCode}
+              country={countryList.find((country) => country.countryId === personalInformation.homeAddress!.countryId)?.[i18n.language === 'fr' ? 'nameFr' : 'nameEn'] ?? ' '}
             />
           ) : (
             <p>{t('personal-information:index.no-address-on-file')}</p>
@@ -100,13 +92,13 @@ export default function PersonalInformationIndex() {
           </p>
         </DescriptionListItem>
         <DescriptionListItem term={t('personal-information:index.mailing-address')}>
-          {mailingAddressInfo ? (
+          {personalInformation.mailingAddress ? (
             <Address
-              address={mailingAddressInfo.address}
-              city={mailingAddressInfo.city}
-              provinceState={regionList.find((region) => region.provinceTerritoryStateId === mailingAddressInfo.province)?.abbr}
-              postalZipCode={mailingAddressInfo.postalCode}
-              country={countryList.find((country) => country.countryId === mailingAddressInfo.country)?.[i18n.language === 'fr' ? 'nameFr' : 'nameEn'] ?? ''}
+              address={personalInformation.mailingAddress.streetName ?? ''}
+              city={personalInformation.mailingAddress.cityName ?? ''}
+              provinceState={regionList.find((region) => region.provinceTerritoryStateId === personalInformation.mailingAddress!.provinceTerritoryStateId)?.abbr}
+              postalZipCode={personalInformation.mailingAddress.postalCode}
+              country={countryList.find((country) => country.countryId === personalInformation.mailingAddress!.countryId)?.[i18n.language === 'fr' ? 'nameFr' : 'nameEn'] ?? ''}
             />
           ) : (
             <p>{t('personal-information:index.no-address-on-file')}</p>
@@ -118,7 +110,7 @@ export default function PersonalInformationIndex() {
           </p>
         </DescriptionListItem>
         <DescriptionListItem term={t('personal-information:index.phone-number')}>
-          <p>{user.phoneNumber}</p>
+          <p>{personalInformation.primaryTelephoneNumber}</p>
           <p>
             <InlineLink id="change-phone-number-button" routeId="$lang+/_protected+/personal-information+/phone-number+/edit" params={params}>
               {t('personal-information:index.change-phone-number')}
