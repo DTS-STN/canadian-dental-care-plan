f0d649c8553eae7869892dadfc7c130e2cd1cd39
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-04-15 08:01:02 -0230

    Add image tag to /api/buildinfo

--- frontend/app/routes/api+/buildinfo.ts
+++ frontend/app/routes/api+/buildinfo.ts
@@ -6,5 +6,8 @@ import { getBuildInfoService } from '~/services/build-info-service.server';
  * An API endpoint that returns the build info.
  */
 export function loader({ context, params, request }: LoaderFunctionArgs) {
-  return json(getBuildInfoService().getBuildInfo());
+  const buildInfo = getBuildInfoService().getBuildInfo();
+  const imageTag = `${buildInfo.buildVersion}-${buildInfo.buildRevision}-${buildInfo.buildId}`;
+
+  return json({ ...buildInfo, imageTag });
 }
