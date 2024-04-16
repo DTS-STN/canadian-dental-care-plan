52ff15272fe5a13fa3007d5ee05919d32ed2a0e5
shawn320 <azuneko1110@gmail.com>
2024-04-16 11:34:27 -0400

    status feature flag

--- frontend/app/routes/$lang+/_public+/status+/index.tsx
+++ frontend/app/routes/$lang+/_public+/status+/index.tsx
@@ -22,7 +22,7 @@ import { getHCaptchaRouteHelpers } from '~/route-helpers/h-captcha-route-helpers
 import { getApplicationStatusService } from '~/services/application-status-service.server';
 import { getLookupService } from '~/services/lookup-service.server';
 import { isValidApplicationCode } from '~/utils/application-code-utils';
-import { getEnv } from '~/utils/env.server';
+import { featureEnabled, getEnv } from '~/utils/env.server';
 import { useHCaptcha } from '~/utils/hcaptcha-utils';
 import { getNameByLanguage, getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { getFixedT } from '~/utils/locale-utils.server';
@@ -38,6 +38,7 @@ export const handle = {
 } as const satisfies RouteHandleData;
 
 export async function loader({ context: { session }, params, request }: LoaderFunctionArgs) {
+  featureEnabled('status');
   const { ENABLED_FEATURES, HCAPTCHA_SITE_KEY } = getEnv();
 
   const csrfToken = String(session.get('csrfToken'));
