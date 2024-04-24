6d12b7ea49e42ab5da34d031c5c249f4e8e0f6cd
Lei Ye <lei.ye@hrsdc-rhdcc.gc.ca>
2024-04-24 16:17:55 -0400

    create alerts index (#1174)

---
+++ frontend/app/routes/$lang+/_protected+/alerts+/index.tsx
@@ -0,0 +1,27 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+
+import { getRaoidcService } from '~/services/raoidc-service.server';
+import { getSubscriptionService } from '~/services/subscription-service.server';
+import { featureEnabled } from '~/utils/env.server';
+import { UserinfoToken } from '~/utils/raoidc-utils.server';
+import { getPathById } from '~/utils/route-utils';
+
+export async function loader({ context: { session }, params, request }: LoaderFunctionArgs) {
+  featureEnabled('email-alerts');
+
+  const raoidcService = await getRaoidcService();
+
+  await raoidcService.handleSessionValidation(request, session);
+
+  const userInfoToken: UserinfoToken = session.get('userInfoToken');
+  const alertSubscription = await getSubscriptionService().getSubscription(userInfoToken.sin ?? '');
+
+  if (!alertSubscription) {
+    return redirect(getPathById('$lang+/_protected+/alerts+/subscribe+/index', params));
+  } else if (alertSubscription.subscribed === true) {
+    return redirect(getPathById('$lang+/_protected+/alerts+/manage+/index', params));
+  } else {
+    return redirect(getPathById('$lang+/_protected+/alerts+/subscribe+/confirm', params));
+  }
+}
