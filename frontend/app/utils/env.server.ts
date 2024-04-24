8e346fa8d062e986647c0dd81874bb7d29404185
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2024-04-24 12:00:44 -0400

    rebase

--- frontend/app/utils/env.server.ts
+++ frontend/app/utils/env.server.ts
@@ -22,7 +22,7 @@ function tryOrElseFalse(fn: () => unknown) {
 const validMockNames = ['cct', 'lookup', 'power-platform', 'raoidc', 'status-check', 'wsaddress', 'subscription'] as const;
 export type MockName = (typeof validMockNames)[number];
 
-const validFeatureNames = ['doc-upload', 'email-alerts', 'hcaptcha', 'view-personal-info', 'view-applications', 'view-letters', 'view-messages', 'edit-personal-info', 'status', 'authenticated-status-check'] as const;
+const validFeatureNames = ['doc-upload', 'email-alerts', 'hcaptcha', 'view-personal-info', 'view-applications', 'view-letters', 'view-messages', 'edit-personal-info', 'status', 'authenticated-status-check', 'show-prototype-banner'] as const;
 export type FeatureName = (typeof validFeatureNames)[number];
 
 // refiners
