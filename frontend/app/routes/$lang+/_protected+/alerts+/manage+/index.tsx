e298470a0f537225473ec7bb3a409b51c46e1987
lei ye <lei.ye@hrsdc-rhdcc.gc.ca>
2024-04-24 15:54:10 -0400

    create alerts index

--- frontend/app/routes/$lang+/_protected+/alerts+/manage+/index.tsx
+++ frontend/app/routes/$lang+/_protected+/alerts+/manage+/index.tsx
@@ -2,14 +2,14 @@ import { useEffect, useMemo } from 'react';
 
 import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
-import { useFetcher, useLoaderData, useParams } from '@remix-run/react';
+import { useFetcher, useLoaderData } from '@remix-run/react';
 
 import { useTranslation } from 'react-i18next';
 import validator from 'validator';
 import { z } from 'zod';
 
 import pageIds from '../../../page-ids.json';
-import { Button, ButtonLink } from '~/components/buttons';
+import { Button } from '~/components/buttons';
 import { ErrorSummary, createErrorSummaryItems, hasErrors, scrollAndFocusToErrorSummary } from '~/components/error-summary';
 import { InputField } from '~/components/input-field';
 import { InputRadios } from '~/components/input-radios';
@@ -27,13 +27,12 @@ import { IdToken, UserinfoToken } from '~/utils/raoidc-utils.server';
 import { getPathById } from '~/utils/route-utils';
 import type { RouteHandleData } from '~/utils/route-utils';
 import { getTitleMetaTags } from '~/utils/seo-utils';
-import { useUserOrigin } from '~/utils/user-origin-utils';
 
 export const handle = {
-  breadcrumbs: [{ labelI18nKey: 'alerts:subscribe.page-title' }],
+  breadcrumbs: [{ labelI18nKey: 'alerts:manage.page-title' }],
   i18nNamespaces: getTypedI18nNamespaces('alerts', 'gcweb'),
-  pageIdentifier: pageIds.protected.alerts.subscribe,
-  pageTitleI18nKey: 'alerts:subscribe.page-title',
+  pageIdentifier: pageIds.protected.alerts.manage,
+  pageTitleI18nKey: 'alerts:manage.page-title',
 } as const satisfies RouteHandleData;
 
 export const meta: MetaFunction<typeof loader> = mergeMeta(({ data }) => {
@@ -52,15 +51,18 @@ export async function loader({ context: { session }, params, request }: LoaderFu
   const t = await getFixedT(request, handle.i18nNamespaces);
   const preferredLanguages = await lookupService.getAllPreferredLanguages();
 
-  const csrfToken = String(session.get('csrfToken'));
-  const meta = { title: t('gcweb:meta.title.template', { title: t('alerts:subscribe.page-title') }) };
+  const userInfoToken: UserinfoToken = session.get('userInfoToken');
+  const alertSubscription = await getSubscriptionService().getSubscription(userInfoToken.sin ?? '');
 
-  instrumentationService.countHttpStatus('alerts.subscibe', 302);
-  return json({ csrfToken, meta, preferredLanguages });
+  const csrfToken = String(session.get('csrfToken'));
+  const meta = { title: t('gcweb:meta.title.template', { title: t('alerts:manage.page-title') }) };
+
+  instrumentationService.countHttpStatus('alerts.manage', 302);
+  return json({ csrfToken, meta, preferredLanguages, alertSubscription });
 }
 
 export async function action({ context: { session }, params, request }: ActionFunctionArgs) {
-  const log = getLogger('alerts/subscribe');
+  const log = getLogger('alerts/manage');
 
   const instrumentationService = getInstrumentationService();
   const auditService = getAuditService();
@@ -68,13 +70,13 @@ export async function action({ context: { session }, params, request }: ActionFu
 
   const formSchema = z
     .object({
-      email: z.string().trim().min(1, t('alerts:subscribe.error-message.email-required')).max(100).email(t('alerts:subscribe.error-message.email-valid')),
-      confirmEmail: z.string().trim().min(1, t('alerts:subscribe.error-message.confirm-email-required')).max(100).email(t('alerts:subscribe.error-message.email-valid')),
-      preferredLanguage: z.string().trim().min(1, t('alerts:subscribe.error-message.preferred-language-required')),
+      email: z.string().trim().min(1, t('alerts:manage.error-message.email-required')).max(100).email(t('alerts:manage.error-message.email-valid')),
+      confirmEmail: z.string().trim().min(1, t('alerts:manage.error-message.confirm-email-required')).max(100).email(t('alerts:manage.error-message.email-valid')),
+      preferredLanguage: z.string().trim().min(1, t('alerts:manage.error-message.preferred-language-required')),
     })
     .superRefine((val, ctx) => {
       if (!validator.isEmpty(val.email) && !validator.isEmpty(val.confirmEmail) && val.email !== val.confirmEmail) {
-        ctx.addIssue({ code: z.ZodIssueCode.custom, message: t('alerts:subscribe.error-message.email-match'), path: ['confirmEmail'] });
+        ctx.addIssue({ code: z.ZodIssueCode.custom, message: t('alerts:manage.error-message.email-match'), path: ['confirmEmail'] });
       }
     });
 
@@ -99,7 +101,7 @@ export async function action({ context: { session }, params, request }: ActionFu
   }
 
   const idToken: IdToken = session.get('idToken');
-  auditService.audit('update-date.subscribe-alerts', { userId: idToken.sub });
+  auditService.audit('update-date.manage-alerts', { userId: idToken.sub });
 
   const userInfoToken: UserinfoToken = session.get('userInfoToken');
   const alertSubscription = await getSubscriptionService().getSubscription(userInfoToken.sin ?? '');
@@ -113,16 +115,14 @@ export async function action({ context: { session }, params, request }: ActionFu
 
   await getSubscriptionService().updateSubscription(userInfoToken.sin ?? '', newAlertSubscription);
 
-  instrumentationService.countHttpStatus('alerts.subscibe', 302);
+  instrumentationService.countHttpStatus('alerts.manage', 302);
   return redirect(getPathById('$lang+/_protected+/alerts+/subscribe+/confirm', params));
 }
 
-export default function AlertsSubscribe() {
+export default function AlertsSubscribeEdit() {
   const { i18n, t } = useTranslation(handle.i18nNamespaces);
-  const { csrfToken, preferredLanguages } = useLoaderData<typeof loader>();
-  const params = useParams();
+  const { csrfToken, preferredLanguages, alertSubscription } = useLoaderData<typeof loader>();
   const fetcher = useFetcher<typeof action>();
-  const userOrigin = useUserOrigin();
   const errorSummaryId = 'error-summary';
 
   // Keys order should match the input IDs order.
@@ -149,16 +149,28 @@ export default function AlertsSubscribe() {
       <fetcher.Form className="max-w-prose" method="post" noValidate>
         <input type="hidden" name="_csrf" value={csrfToken} />
         <div className="mb-6 grid gap-6 md:grid-cols-2">
-          <InputField id="email" type="email" className="w-full" label={t('alerts:subscribe.email')} maxLength={100} name="email" errorMessage={errorMessages.email} autoComplete="email" required />
-          <InputField id="confirm-email" type="email" className="w-full" label={t('alerts:subscribe.confirm-email')} maxLength={100} name="confirmEmail" errorMessage={errorMessages['confirm-email']} autoComplete="email" required />
+          <InputField id="email" type="email" className="w-full" label={t('alerts:manage.email')} maxLength={100} name="email" errorMessage={errorMessages.email} autoComplete="email" defaultValue={alertSubscription?.email} required />
+          <InputField
+            id="confirm-email"
+            type="email"
+            className="w-full"
+            label={t('alerts:manage.confirm-email')}
+            maxLength={100}
+            name="confirmEmail"
+            errorMessage={errorMessages['confirm-email']}
+            autoComplete="email"
+            defaultValue={alertSubscription?.email}
+            required
+          />
         </div>
         <div className="mb-8 space-y-6">
           {preferredLanguages.length > 0 && (
             <InputRadios
               id="preferred-language"
               name="preferredLanguage"
-              legend={t('alerts:subscribe.preferred-language')}
+              legend={t('alerts:manage.preferred-language')}
               options={preferredLanguages.map((language) => ({
+                defaultChecked: alertSubscription?.preferredLanguage === language.id,
                 children: getNameByLanguage(i18n.language, language),
                 value: language.id,
               }))}
@@ -169,11 +181,8 @@ export default function AlertsSubscribe() {
         </div>
 
         <div className="flex flex-wrap items-center gap-3">
-          <ButtonLink id="back-button" to={userOrigin?.to} params={params}>
-            {t('alerts:subscribe.button.back')}
-          </ButtonLink>
           <Button id="save-button" variant="primary">
-            {t('alerts:subscribe.button.subscribe')}
+            {t('alerts:manage.button.save')}
           </Button>
         </div>
       </fetcher.Form>
