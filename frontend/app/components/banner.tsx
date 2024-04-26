a701a74f4dd42fcff148cac9037a1467c567b29b
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2024-04-26 16:13:00 -0400

    banner change for release

--- frontend/app/components/banner.tsx
+++ frontend/app/components/banner.tsx
@@ -7,7 +7,7 @@ interface BannerProps {
 
 export function Banner({ alert, description }: BannerProps) {
   return (
-    <div className="font-body bg-sky-800 text-white" data-testid="banner-id">
+    <div className="font-body bg-slate-700 text-white" data-testid="banner-id">
       <div className="container mx-auto flex flex-col space-y-2 p-4 lg:flex-row lg:items-center lg:space-x-4 lg:space-y-0">
         <div className="w-max whitespace-nowrap border-2 border-current px-4 py-1">{alert}</div>
         <div>{description}</div>
