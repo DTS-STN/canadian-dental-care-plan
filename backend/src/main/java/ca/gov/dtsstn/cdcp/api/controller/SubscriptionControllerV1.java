900fcefa6f3f5ca6e4fec3103e3cbc9cc1fe40e0
lei ye <lei.ye@hrsdc-rhdcc.gc.ca>
2024-05-06 12:01:48 -0400

    backend/SubscriptionController to get mock subscription data

---
+++ backend/src/main/java/ca/gov/dtsstn/cdcp/api/controller/SubscriptionControllerV1.java
@@ -0,0 +1,41 @@
+package ca.gov.dtsstn.cdcp.api.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.gov.dtsstn.cdcp.api.dto.ImmutableSubscription;
+
+@RestController
+@RequestMapping(path={"/api/v1"})
+public class SubscriptionControllerV1 {
+
+	@GetMapping("/users/{userId}/subscriptions")
+	public List<ImmutableSubscription> getSubscriptions(@PathVariable String userId) {
+
+		return List.of(
+			ImmutableSubscription.builder()
+				.id(10001L)
+				.sin("800011819")
+				.email("user@example.com")
+				.registered(true)
+				.subscribed(true)
+				.preferredLanguage(1033L)
+				.alertType("cdcp")
+				.build(),			
+			ImmutableSubscription.builder()
+				.id(10002L)
+				.sin("800011819")
+				.email("user@example.com")
+				.registered(true)
+				.subscribed(false)
+				.preferredLanguage(1033L)
+				.alertType("ei")
+				.build()
+		);
+	}
+}
