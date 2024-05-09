28da13582c46c797b25484e5c0bf7c48146b1251
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-05-09 07:40:08 -0230

    [backend] Minor code fixups

---
+++ backend/src/test/java/ca/gov/dtsstn/cdcp/api/ApplicationIT.java
@@ -0,0 +1,24 @@
+package ca.gov.dtsstn.cdcp.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
+ */
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationIT {
+
+	@Autowired ApplicationContext applicationContext;
+
+	@Test void contextLoads() {
+		assertThat(applicationContext).isNotNull();
+	}
+
+}
