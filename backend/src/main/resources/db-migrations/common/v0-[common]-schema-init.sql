26baa687eded7ab659de3f9541e184603b2c6ce8
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-05-09 08:33:32 -0230

    [backend] Minor code fixups (#1316)

--- backend/src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
+++ backend/src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
@@ -1,17 +1,17 @@
 CREATE TABLE subscription (
-    id VARCHAR(64) NOT NULL,
-    user_id VARCHAR(9) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    registered BOOLEAN, 
-    subscribed BOOLEAN,
-    preferred_language BIGINT,
-    alert_type VARCHAR(10) NOT NULL,
+	id VARCHAR(64) NOT NULL,
+	user_id VARCHAR(9) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	registered BOOLEAN,
+	subscribed BOOLEAN,
+	preferred_language BIGINT,
+	alert_type VARCHAR(10) NOT NULL,
 
-    -- audit fields
-    created_by VARCHAR(64) NOT NULL,
-    created_date TIMESTAMP WITH TIME ZONE NOT NULL,
-    last_modified_by VARCHAR(64),
-    last_modified_date TIMESTAMP WITH TIME ZONE,
+	-- audit fields
+	created_by VARCHAR(64) NOT NULL,
+	created_date TIMESTAMP WITH TIME ZONE NOT NULL,
+	last_modified_by VARCHAR(64),
+	last_modified_date TIMESTAMP WITH TIME ZONE,
 
-    CONSTRAINT pk_subscription PRIMARY KEY (id)
+	CONSTRAINT pk_subscription PRIMARY KEY (id)
 );
