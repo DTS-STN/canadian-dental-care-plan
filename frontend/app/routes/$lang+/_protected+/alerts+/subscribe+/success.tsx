5377645e59be277399407be2f5f496af1f3e7197
Benoit Cloutier <77116011+nibivi77@users.noreply.github.com>
2024-05-15 05:29:26 -0400

    Adding route for successful alert subscription. (#1330)
    * Adding route for successful alert subscription.
    
    * Lint

--- frontend/app/routes/$lang+/_protected+/alerts+/subscribe+/success.tsx
+++ frontend/app/routes/$lang+/_protected+/alerts+/subscribe+/success.tsx
@@ -1,26 +1,25 @@
 import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useParams } from '@remix-run/react';
+import { useLoaderData, useParams } from '@remix-run/react';
 
-import { Trans, useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
 
 import pageIds from '../../../page-ids.json';
+import { ContextualAlert } from '~/components/contextual-alert';
 import { InlineLink } from '~/components/inline-link';
 import { getRaoidcService } from '~/services/raoidc-service.server';
-import { getSubscriptionService } from '~/services/subscription-service.server';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { getFixedT } from '~/utils/locale-utils.server';
 import { getLogger } from '~/utils/logging.server';
 import { mergeMeta } from '~/utils/meta-utils';
-import { UserinfoToken } from '~/utils/raoidc-utils.server';
 import type { RouteHandleData } from '~/utils/route-utils';
 import { getTitleMetaTags } from '~/utils/seo-utils';
 
 export const handle = {
-  breadcrumbs: [{ labelI18nKey: 'alerts:success.page-title' }],
+  breadcrumbs: [{ labelI18nKey: 'alerts:subscribe.success.page-title' }],
   i18nNamespaces: getTypedI18nNamespaces('alerts', 'gcweb'),
   pageIdentifier: pageIds.protected.alerts.success,
-  pageTitleI18nKey: 'alerts:success.page-title',
+  pageTitleI18nKey: 'alerts:subscribe.success.page-title',
 } as const satisfies RouteHandleData;
 
 export const meta: MetaFunction<typeof loader> = mergeMeta(({ data }) => {
@@ -32,40 +31,38 @@ export async function loader({ context: { session }, params, request }: LoaderFu
 
   await raoidcService.handleSessionValidation(request, session);
 
-  const log = getLogger('alerts.unsubscribe.success');
+  const log = getLogger('alerts.subscription.success');
   const t = await getFixedT(request, handle.i18nNamespaces);
 
-  const meta = { title: t('gcweb:meta.title.template', { title: t('alerts:success.page-title') }) };
+  const meta = { title: t('gcweb:meta.title.template', { title: t('alerts:subscribe.success.page-title') }) };
 
-  const userInfoToken: UserinfoToken = session.get('userInfoToken');
-  if (!userInfoToken.sin) {
-    log.warn('SIN was not found in session, returning a 500 status');
-    throw new Response(null, { status: 500 });
-  }
-  const alertSubscription = await getSubscriptionService().getSubscription(userInfoToken.sin);
-
-  if (alertSubscription?.subscribed) {
-    log.warn('User is already subscribed, returning a 400 status');
-    throw new Response(null, { status: 400 });
+  const alertSubscription = session.get('alertSubscription');
+  if (!alertSubscription.email) {
+    log.warn('alert subscription email not found, throwing 400');
+    throw new Response('alert subscription email not found', { status: 400 });
   }
 
-  return json({ meta });
+  return json({ meta, alertSubscription });
 }
 
 export default function SuccessSubscription() {
-  const { t } = useTranslation(handle.i18nNamespaces);
+  const { alertSubscription } = useLoaderData<typeof loader>();
   const params = useParams();
 
-  const subscribelink = <InlineLink routeId="$lang+/_protected+/alerts+/subscribe+/index" params={params} />;
+  const personalInformationLink = <InlineLink routeId="$lang+/_protected+/personal-information+/index" params={params} />;
   const homeLink = <InlineLink routeId="$lang+/_protected+/home" params={params} />;
   return (
     <>
-      <p className="mb-4">{t('alerts:success.unsubscribe-successful')}</p>
+      <ContextualAlert type="success">
+        <p>
+          <Trans ns={handle.i18nNamespaces} i18nKey="alerts:subscribe.success.subscribe-successful" values={{ userEmailAddress: alertSubscription.email }} />
+        </p>
+      </ContextualAlert>
       <p className="mb-4">
-        <Trans ns={handle.i18nNamespaces} i18nKey="alerts:success.subscribe-again" components={{ subscribelink }} />
+        <Trans ns={handle.i18nNamespaces} i18nKey="alerts:subscribe.success.change-email" components={{ personalInformationLink }} />
       </p>
       <p className="mb-4">
-        <Trans ns={handle.i18nNamespaces} i18nKey="alerts:success.return-home" components={{ homeLink }} />
+        <Trans ns={handle.i18nNamespaces} i18nKey="alerts:subscribe.success.return-home" components={{ homeLink }} />
       </p>
     </>
   );
