9c75b6d3ee8f42a56780c8e3cce15b82d4f86cef
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-05-16 13:00:12 -0230

    Add `user` and `user_attribute` tables (#1389)

--- backend/src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
+++ backend/src/main/resources/db-migrations/common/v0-[common]-schema-init.sql
@@ -1,33 +1,83 @@
-CREATE TABLE alert_type (
-	id VARCHAR(64) NOT NULL,
+CREATE TABLE `user` (
+	`id` VARCHAR(64) NOT NULL,
 
-	code VARCHAR(64) NOT NULL,
-	description VARCHAR(256),
-
-	-- audit fields
-	created_by VARCHAR(64) NOT NULL,
-	created_date TIMESTAMP NOT NULL,
-	last_modified_by VARCHAR(64),
-	last_modified_date TIMESTAMP,
-
-	CONSTRAINT pk_status_code PRIMARY KEY (id)
-);
-
-CREATE TABLE subscription (
-	id VARCHAR(64) NOT NULL,
-	user_id VARCHAR(9) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	registered BOOLEAN,
-	subscribed BOOLEAN,
-	preferred_language BIGINT,
-	alert_type_id VARCHAR(64) NOT NULL,
+	`email` VARCHAR(256),
+	`email_verified` BOOLEAN,
 
 	-- audit fields
-	created_by VARCHAR(64) NOT NULL,
-	created_date TIMESTAMP WITH TIME ZONE NOT NULL,
-	last_modified_by VARCHAR(64),
-	last_modified_date TIMESTAMP WITH TIME ZONE,
+	`created_by` VARCHAR(64) NOT NULL,
+	`created_date` TIMESTAMP WITH TIME ZONE NOT NULL,
+	`last_modified_by` VARCHAR(64),
+	`last_modified_date` TIMESTAMP WITH TIME ZONE,
 
-	CONSTRAINT pk_subscription PRIMARY KEY (id),
-	CONSTRAINT fk_subscription_alert_type FOREIGN KEY (alert_type_id) REFERENCES alert_type(id)
+	CONSTRAINT `pk_user` PRIMARY KEY (`id`)
 );
+
+CREATE INDEX `ix_user_email` ON `user` (`email`);
+
+
+create TABLE `user_attribute` (
+	`id` VARCHAR(64) NOT NULL,
+
+	`name` VARCHAR(256) NOT NULL,
+	`value` VARCHAR(2048),
+	`user_id` VARCHAR(64) NOT NULL,
+
+	-- audit fields
+	`created_by` VARCHAR(64) NOT NULL,
+	`created_date` TIMESTAMP WITH TIME ZONE NOT NULL,
+	`last_modified_by` VARCHAR(64),
+	`last_modified_date` TIMESTAMP WITH TIME ZONE,
+
+	CONSTRAINT `pk_user_attribute` PRIMARY KEY (`id`),
+	CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+	CONSTRAINT `uq_user_attribute` UNIQUE (`name`, `user_id`)
+);
+
+CREATE INDEX `ix_user_attribute_user_id` on `user_attribute` (`user_id`);
+CREATE INDEX `ix_user_attribute_name_value` ON `user_attribute` (`name`, `value`);
+
+
+
+CREATE TABLE `alert_type` (
+	`id` VARCHAR(64) NOT NULL,
+
+	`code` VARCHAR(64) NOT NULL,
+	`description` VARCHAR(256),
+
+	-- audit fields
+	`created_by` VARCHAR(64) NOT NULL,
+	`created_date` TIMESTAMP WITH TIME ZONE NOT NULL,
+	`last_modified_by` VARCHAR(64),
+	`last_modified_date` TIMESTAMP WITH TIME ZONE,
+
+	CONSTRAINT `pk_alert_type` PRIMARY KEY (`id`)
+);
+
+CREATE INDEX `ix_alert_type_code` on `alert_type` (`code`);
+
+
+
+CREATE TABLE `subscription` (
+	`id` VARCHAR(64) NOT NULL,
+
+	`user_id` VARCHAR(9) NOT NULL,
+	`email` VARCHAR(50) NOT NULL,
+	`registered` BOOLEAN,
+	`subscribed` BOOLEAN,
+	`preferred_language` BIGINT,
+	`alert_type_id` VARCHAR(64) NOT NULL,
+
+	-- audit fields
+	`created_by` VARCHAR(64) NOT NULL,
+	`created_date` TIMESTAMP WITH TIME ZONE NOT NULL,
+	`last_modified_by` VARCHAR(64),
+	`last_modified_date` TIMESTAMP WITH TIME ZONE,
+
+	CONSTRAINT `pk_subscription` PRIMARY KEY (`id`),
+	CONSTRAINT `fk_subscription_alert_type` FOREIGN KEY (`alert_type_id`) REFERENCES `alert_type` (`id`)
+);
+
+CREATE INDEX `ix_subscription_alert_type_id` on `subscription` (`alert_type_id`);
+CREATE INDEX `ix_subscription_email` on `subscription` (`email`);
+CREATE INDEX `ix_subscription_user_id` on `subscription` (`user_id`);
--- backend/src/main/resources/db-migrations/h2/v0.1-[vendor]-data-init.sql
+++ backend/src/main/resources/db-migrations/h2/v0.1-[vendor]-data-init.sql
@@ -1,11 +1,22 @@
-INSERT INTO alert_type 
-	(id, code, description, created_by, created_date, last_modified_by, last_modified_date)
+INSERT INTO `user` (`id`, `email`, `email_verified`, `created_by`, `created_date`, `last_modified_by`, `last_modified_date`)
 VALUES
-	('cf185099-8a17-4086-a890-c456250822a3', 'cdcp', 'cdcp email alerts', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP),
-	('daf8b8d9-95f4-4f38-9ee3-17ac7826c1e7', 'ei', 'ei email alerts', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
+	('76c48130-e1d4-4c2f-8dd0-1c17f9bbb4f6', 'unverified@example.com', false, 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP),
+	('f9f33652-0ebd-46bc-8d93-04cef538a689', 'verified@example.com', true, 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
 
-INSERT INTO subscription 
-	(id, user_id, email, registered, subscribed, preferred_language, alert_type_id, created_by, created_date, last_modified_by, last_modified_date)
+
+INSERT INTO `user_attribute` (`id`, `name`, `value`, `user_id`, `created_by`, `created_date`, `last_modified_by`, `last_modified_date`)
 VALUES
-	('a6ea4925-f813-493e-80ec-a5b90ca28b6c', '800011819', 'example@gmail.com', true, true, 1033, 'cf185099-8a17-4086-a890-c456250822a3', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP),
-	('d3419bcb-5e46-4678-831b-c9211d479429', '800011819', 'example@gmail.com', true, false, 1033, 'daf8b8d9-95f4-4f38-9ee3-17ac7826c1e7', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
+	('291e1ebb-8298-4c47-8903-87b2a57ccfd1', 'RAOIDC_USER_ID', 'b5336580-d93d-4da9-9e19-c2a5e098bd08', '76c48130-e1d4-4c2f-8dd0-1c17f9bbb4f6', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP),
+	('7da43bfb-cb58-4ed2-8fea-436fda24020e', 'RAOIDC_USER_ID', 'd827416b-f808-4035-9ccc-7572f3297015', 'f9f33652-0ebd-46bc-8d93-04cef538a689', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
+
+
+INSERT INTO `alert_type` (`id`, `code`, `description`, `created_by`, `created_date`, `last_modified_by`, `last_modified_date`)
+VALUES
+	('cf185099-8a17-4086-a890-c456250822a3', 'cdcp', 'CDCP email alerts', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP),
+	('daf8b8d9-95f4-4f38-9ee3-17ac7826c1e7', 'ei', 'EI email alerts', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
+
+
+INSERT INTO `subscription` (`id`, `user_id`, `email`, `registered`, `subscribed`, `preferred_language`, `alert_type_id`, `created_by`, `created_date`, `last_modified_by`, `last_modified_date`)
+VALUES
+	('a6ea4925-f813-493e-80ec-a5b90ca28b6c', '800011819', 'user0001@example.com', true, true, 1033, 'cf185099-8a17-4086-a890-c456250822a3', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP),
+	('d3419bcb-5e46-4678-831b-c9211d479429', '800011819', 'user0002@example.com', true, false, 1033, 'daf8b8d9-95f4-4f38-9ee3-17ac7826c1e7', 'flyway-community-edition', CURRENT_TIMESTAMP, 'flyway-community-edition', CURRENT_TIMESTAMP);
