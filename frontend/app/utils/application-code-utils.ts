e7d1affef29fab5e8ad7bd52d9d9510fedb4bc3b
frank <frankabasham@gmail.com>
2024-05-23 09:19:51 -0400

    update confirmation pages

--- frontend/app/utils/application-code-utils.ts
+++ frontend/app/utils/application-code-utils.ts
@@ -10,3 +10,14 @@ export function isValidApplicationCode(applicationCode: string): boolean {
   const applicationCodeRegex = /^(?:\d{13}|\d{11}|\d{6})$/;
   return applicationCodeRegex.test(applicationCode);
 }
+
+/**
+ *
+ * @param applicationCode the 13 digit application code returned from PP after an applicant submits their application
+ * @returns submission code in the format of XXX XXX XXX XXXX if it is valid otherwise the input
+ */
+export function formatSubmissionApplicationCode(applicationCode: string): string {
+  const strippedCode = applicationCode.replace(/ /g, '');
+  if (!/^\d{13}$/.test(strippedCode)) return applicationCode;
+  return (strippedCode.match(/....$|.../g) ?? []).join(' ');
+}
