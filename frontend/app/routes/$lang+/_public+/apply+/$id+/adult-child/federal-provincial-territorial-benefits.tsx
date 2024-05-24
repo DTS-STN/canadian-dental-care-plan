5b6ae5e366157891661e6160425e8e57a785b2f4
shawn320 <azuneko1110@gmail.com>
2024-05-24 11:28:48 -0400

    update link

--- frontend/app/routes/$lang+/_public+/apply+/$id+/adult-child/federal-provincial-territorial-benefits.tsx
+++ frontend/app/routes/$lang+/_public+/apply+/$id+/adult-child/federal-provincial-territorial-benefits.tsx
@@ -384,7 +384,7 @@ export default function AccessToDentalInsuranceQuestion() {
               </Button>
               <ButtonLink
                 id="back-button"
-                routeId="$lang+/_public+/apply+/$id+/adult/dental-insurance" //TODO: Change over to adult-child when available
+                routeId="$lang+/_public+/apply+/$id+/adult-child/dental-insurance"
                 params={params}
                 disabled={isSubmitting}
                 data-gc-analytics-customclick="ESDC-EDSC:CDCP Online Application Form:Back - Access to other federal, provincial or territorial dental benefits click"
