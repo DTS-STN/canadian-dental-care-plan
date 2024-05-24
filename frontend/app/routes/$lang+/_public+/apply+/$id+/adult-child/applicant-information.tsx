d7e42991519fae0ae0ab1162fa897ceb1268c79e
Frank <48450599+Fbasham@users.noreply.github.com>
2024-05-24 11:43:35 -0400

    3730 review applicant info and spouse info screens (#1466)

--- frontend/app/routes/$lang+/_public+/apply+/$id+/adult-child/applicant-information.tsx
+++ frontend/app/routes/$lang+/_public+/apply+/$id+/adult-child/applicant-information.tsx
@@ -197,7 +197,7 @@ export default function ApplyFlowApplicationInformation() {
       <div className="max-w-prose">
         <p className="mb-4">{t('applicant-information.form-instructions-sin')}</p>
         <p className="mb-6">{t('applicant-information.form-instructions-info')}</p>
-        <p className="italic">{t('apply:required-label')}</p>
+        <p className="mb-4 italic">{t('apply:required-label')}</p>
         {errorSummaryItems.length > 0 && <ErrorSummary id={errorSummaryId} errors={errorSummaryItems} />}
         <fetcher.Form method="post" noValidate>
           <input type="hidden" name="_csrf" value={csrfToken} />
