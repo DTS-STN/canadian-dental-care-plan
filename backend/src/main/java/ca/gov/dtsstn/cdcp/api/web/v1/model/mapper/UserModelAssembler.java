c5f4a945da15485fb8855515ff8889034188b4b0
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-05-24 09:42:09 -0230

    Add user subscriptions hateoas links (#1459)

--- backend/src/main/java/ca/gov/dtsstn/cdcp/api/web/v1/model/mapper/UserModelAssembler.java
+++ backend/src/main/java/ca/gov/dtsstn/cdcp/api/web/v1/model/mapper/UserModelAssembler.java
@@ -1,5 +1,8 @@
 package ca.gov.dtsstn.cdcp.api.web.v1.model.mapper;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.mapstruct.factory.Mappers;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.stereotype.Component;
@@ -7,6 +10,7 @@ import org.springframework.util.Assert;
 
 import ca.gov.dtsstn.cdcp.api.service.domain.User;
 import ca.gov.dtsstn.cdcp.api.web.model.AbstractModelAssembler;
+import ca.gov.dtsstn.cdcp.api.web.v1.controller.SubscriptionsController;
 import ca.gov.dtsstn.cdcp.api.web.v1.controller.UsersController;
 import ca.gov.dtsstn.cdcp.api.web.v1.model.UserModel;
 
@@ -22,7 +26,8 @@ public class UserModelAssembler extends AbstractModelAssembler<User, UserModel> 
 	@Override
 	protected UserModel instantiateModel(User user) {
 		Assert.notNull(user, "user is required; it must not be null");
-		return userModelMapper.map(user);
+		return userModelMapper.map(user)
+			.add(linkTo(methodOn(SubscriptionsController.class).getSubscriptionsByUserId(user.getId())).withRel("subscriptions"));
 	}
 
 }
