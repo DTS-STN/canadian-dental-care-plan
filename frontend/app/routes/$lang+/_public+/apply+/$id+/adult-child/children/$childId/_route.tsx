203caf3bd72ed364bc01c4e13326da2ff89d5e15
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-05-27 08:58:58 -0300

    Add transform AA url for child routes

--- frontend/app/routes/$lang+/_public+/apply+/$id+/adult-child/children/$childId/_route.tsx
+++ frontend/app/routes/$lang+/_public+/apply+/$id+/adult-child/children/$childId/_route.tsx
@@ -1,5 +1,12 @@
 import { Outlet } from '@remix-run/react';
 
+import { transformChildrenRouteAdobeAnalyticsUrl } from '~/route-helpers/apply-adult-child-route-helpers';
+import { RouteHandleData } from '~/utils/route-utils';
+
+export const handle = {
+  transformAdobeAnalyticsUrl: transformChildrenRouteAdobeAnalyticsUrl,
+} as const satisfies RouteHandleData;
+
 /**
  * Do-nothing parent route.
  * (placeholder for future code)
