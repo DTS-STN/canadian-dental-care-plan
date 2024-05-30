e7c2f70047f72d7574a5bf6c6e9997eb02c30e80
ESDC-Olivier-Roy <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-05-30 11:28:00 -0400

    refactor code for the index route of the alert section

--- frontend/app/routes/$lang/_protected/alerts/index.tsx
+++ frontend/app/routes/$lang/_protected/alerts/index.tsx
@@ -4,6 +4,8 @@ import { redirect } from '@remix-run/node';
 import invariant from 'tiny-invariant';
 
 import { getRaoidcService } from '~/services/raoidc-service.server';
+import { getSubscriptionService } from '~/services/subscription-service.server';
+import { getUserService } from '~/services/user-service.server';
 import { featureEnabled } from '~/utils/env.server';
 import { UserinfoToken } from '~/utils/raoidc-utils.server';
 import { getPathById } from '~/utils/route-utils';
@@ -12,22 +14,24 @@ export async function loader({ context: { session }, params, request }: LoaderFu
   featureEnabled('email-alerts');
 
   const raoidcService = await getRaoidcService();
-
   await raoidcService.handleSessionValidation(request, session);
-
   const userInfoToken: UserinfoToken = session.get('userInfoToken');
 
+  const userService = getUserService();
+  const subscriptionService = getSubscriptionService();
   invariant(userInfoToken.sin, 'Expected userInfoToken.sin to be defined');
 
-  return redirect(getPathById('$lang/_protected/alerts/subscribe/index', params));
+  const userInfo = userService.getUser(userInfoToken.sub);
+  const subscriptions = await subscriptionService.getSubscription(userInfoToken.sub);
+  const emailVerified = (await userInfo).emailVerified;
 
-  //TODO fix the redirection once we can retrieve the status of the subscription
+  if (emailVerified === false && !subscriptions) {
+    return redirect(getPathById('$lang/_protected/alerts+/subscribe+/index', params));
+  }
 
-  /*if (!alertSubscription || alertSubscription.registered === false) {
-    return redirect(getPathById('$lang/_protected/alerts/subscribe/index', params));
+  if (emailVerified) {
+    return redirect(getPathById('$lang/_protected/alerts+/manage+/index', params));
   }
-  if (alertSubscription.subscribed === true) {
-    return redirect(getPathById('$lang/_protected/alerts/manage/index', params));
-  }
-  return redirect(getPathById('$lang/_protected/alerts/subscribe/confirm', params));*/
+
+  return redirect(getPathById('$lang/_protected/alerts+/subscribe+/confirm', params));
 }
