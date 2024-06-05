df5a1257ebdd99bc4a8b643165cc5dc9933e026f
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-06-05 13:13:43 -0300

    Use phone number input without country "<select/>"

--- frontend/__tests__/components/input-phone-field.test.tsx
+++ frontend/__tests__/components/input-phone-field.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 
-import { parsePhoneNumber } from 'libphonenumber-js';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
 import { InputPhoneField } from '~/components/input-phone-field';
@@ -18,7 +17,7 @@ describe('InputPhoneField', () => {
   });
 
   it('should render', () => {
-    const phoneNumber = parsePhoneNumber('+15146667777').number;
+    const phoneNumber = '+15146667777';
     render(<InputPhoneField id="test-id" name="test" label="label test" defaultValue={phoneNumber} />);
 
     const actual: HTMLInputElement = screen.getByTestId('input-phone-field');
@@ -26,13 +25,27 @@ describe('InputPhoneField', () => {
     expect(actual).toBeInTheDocument();
     expect(actual).toHaveAccessibleName('label test');
     expect(actual).toHaveAttribute('id', 'test-id');
-    expect(actual).toHaveValue('+1 514 666 7777');
+    expect(actual).toHaveValue('(514) 666-7777');
+    expect(actual).not.toBeRequired();
+    expect(actual).not.toHaveAccessibleDescription();
+  });
+
+  it('should render international phone number', () => {
+    const phoneNumber = '+50644444444';
+    render(<InputPhoneField id="test-id" name="test" label="label test" defaultValue={phoneNumber} />);
+
+    const actual: HTMLInputElement = screen.getByTestId('input-phone-field');
+
+    expect(actual).toBeInTheDocument();
+    expect(actual).toHaveAccessibleName('label test');
+    expect(actual).toHaveAttribute('id', 'test-id');
+    expect(actual).toHaveValue('+506 4444 4444');
     expect(actual).not.toBeRequired();
     expect(actual).not.toHaveAccessibleDescription();
   });
 
   it('should render with help message', () => {
-    const phoneNumber = parsePhoneNumber('+15146667777').number;
+    const phoneNumber = '+15146667777';
     render(<InputPhoneField id="test-id" name="test" label="label test" defaultValue={phoneNumber} helpMessageSecondary="help message" />);
 
     const actual = screen.getByTestId('input-phone-field');
@@ -41,25 +54,25 @@ describe('InputPhoneField', () => {
     expect(actual).toHaveAccessibleDescription('help message');
     expect(actual).toHaveAccessibleName('label test');
     expect(actual).toHaveAttribute('id', 'test-id');
-    expect(actual).toHaveValue('+1 514 666 7777');
+    expect(actual).toHaveValue('(514) 666-7777');
     expect(actual).not.toBeRequired();
   });
 
   it('should render with required', () => {
-    const phoneNumber = parsePhoneNumber('+15146667777').number;
+    const phoneNumber = '+15146667777';
     render(<InputPhoneField id="test-id" name="test" label="label test" defaultValue={phoneNumber} required />);
 
     const actual = screen.getByTestId('input-phone-field');
 
     expect(actual).toBeInTheDocument();
     expect(actual).toHaveAttribute('id', 'test-id');
-    expect(actual).toHaveValue('+1 514 666 7777');
+    expect(actual).toHaveValue('(514) 666-7777');
     expect(actual).toBeRequired();
     expect(actual).not.toHaveAccessibleDescription();
   });
 
   it('should render with error message', () => {
-    const phoneNumber = parsePhoneNumber('+15146667777').number;
+    const phoneNumber = '+15146667777';
     render(<InputPhoneField id="test-id" name="test" label="label test" defaultValue={phoneNumber} errorMessage="error message" />);
 
     const actual = screen.getByTestId('input-phone-field');
