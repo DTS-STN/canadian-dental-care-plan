350c785efef2dc7ee75dc9138a9069d36abb4588
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-06-04 21:18:55 -0300

    Add loadApplyAdultStateForReview for application "adult" flow

--- frontend/app/routes/$lang/_public/apply/$id/adult/review-information.tsx
+++ frontend/app/routes/$lang/_public/apply/$id/adult/review-information.tsx
@@ -20,7 +20,7 @@ import { DescriptionListItem } from '~/components/description-list-item';
 import { InlineLink } from '~/components/inline-link';
 import { Progress } from '~/components/progress';
 import { toBenefitApplicationRequestFromApplyAdultState } from '~/mappers/benefit-application-service-mappers.server';
-import { loadApplyAdultState, validateApplyAdultStateForReview } from '~/route-helpers/apply-adult-route-helpers.server';
+import { loadApplyAdultStateForReview } from '~/route-helpers/apply-adult-route-helpers.server';
 import { clearApplyState, saveApplyState } from '~/route-helpers/apply-route-helpers.server';
 import { getHCaptchaRouteHelpers } from '~/route-helpers/h-captcha-route-helpers.server';
 import { getBenefitApplicationService } from '~/services/benefit-application-service.server';
@@ -52,7 +52,7 @@ export const meta: MetaFunction<typeof loader> = mergeMeta(({ data }) => {
 });
 
 export async function loader({ context: { session }, params, request }: LoaderFunctionArgs) {
-  const state = validateApplyAdultStateForReview({ params, state: loadApplyAdultState({ params, request, session }) });
+  const state = loadApplyAdultStateForReview({ params, request, session });
 
   // apply state is valid then edit mode can be set to true
   saveApplyState({ params, session, state: { editMode: true } });
@@ -171,7 +171,7 @@ export async function loader({ context: { session }, params, request }: LoaderFu
 export async function action({ context: { session }, params, request }: ActionFunctionArgs) {
   const log = getLogger('apply/review-information');
 
-  const state = validateApplyAdultStateForReview({ params, state: loadApplyAdultState({ params, request, session }) });
+  const state = loadApplyAdultStateForReview({ params, request, session });
 
   const { ENABLED_FEATURES } = getEnv();
   const benefitApplicationService = getBenefitApplicationService();
