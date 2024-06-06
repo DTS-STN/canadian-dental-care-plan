8d21b40f8b5ae2182ea7e9cc7ae536f0bc029acc
ESDC-Olivier-Roy <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-06-06 05:42:58 -0400

    Fix api SubscriptionModelMapper (#1604)
    * Fixed the api to return the same object structure when there are no subscriptions for a user
    
    * refactor the code
    
    * WIP to make the class abstract
    
    * Added AbstractModelMapper and refactor subscriptionModelMapper
    
    * code refactor
    
    * fixed indentation
    
    * fixed indentation
    
    * Formatting and clean up imports
    
    ---------
    
    Co-authored-by: Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>

--- backend/src/main/java/ca/gov/dtsstn/cdcp/api/web/v1/model/mapper/SubscriptionModelMapper.java
+++ backend/src/main/java/ca/gov/dtsstn/cdcp/api/web/v1/model/mapper/SubscriptionModelMapper.java
@@ -20,43 +20,45 @@ import ca.gov.dtsstn.cdcp.api.web.v1.model.SubscriptionModel;
 import jakarta.annotation.Nullable;
 
 @Mapper
-public interface SubscriptionModelMapper {
+public abstract class SubscriptionModelMapper extends AbstractModelMapper {
 
 	@Nullable
-	default CollectionModel<SubscriptionModel> toModel(String userId, @Nullable Iterable<Subscription> subscriptions) {
+	public CollectionModel<SubscriptionModel> toModel(String userId, @Nullable Iterable<Subscription> subscriptions) {
 		final var subscriptionModels = StreamSupport.stream(subscriptions.spliterator(), false)
 			.map(subscription -> toModel(userId, subscription)).toList();
 
-		return CollectionModel.of(subscriptionModels)
+		final var collection = CollectionModel.of(subscriptionModels)
 			.add(linkTo(methodOn(SubscriptionsController.class).getSubscriptionsByUserId(userId)).withSelfRel());
+
+		return wrapCollection(collection, SubscriptionModel.class);
 	}
 
 	@Nullable
 	@Mapping(target = "alertTypeCode", source = "subscription.alertType.code")
 	@Mapping(target = "msLanguageCode", source = "subscription.language.msLocaleCode")
 	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-	SubscriptionModel toModel(String userId, @Nullable Subscription subscription);
+	public abstract SubscriptionModel toModel(String userId, @Nullable Subscription subscription);
 
 	@AfterMapping
-	default SubscriptionModel afterMappingToModel(String userId, @MappingTarget SubscriptionModel subscription) {
+	public SubscriptionModel afterMappingToModel(String userId, @MappingTarget SubscriptionModel subscription) {
 		return subscription.add(linkTo(methodOn(SubscriptionsController.class).getSubscriptionById(userId, subscription.getId())).withSelfRel());
 	}
 
-	@Mapping(target= "id", ignore = true)
-	@Mapping(target= "createdBy", ignore = true)
-	@Mapping(target= "createdDate", ignore = true)
-	@Mapping(target= "lastModifiedBy", ignore = true)
-	@Mapping(target= "lastModifiedDate", ignore = true)
-	@Mapping(target= "alertType.code", source = "alertTypeCode")
-	@Mapping(target= "language.msLocaleCode", source = "msLanguageCode")
-	Subscription toDomainObject(@Nullable SubscriptionCreateModel subscriptionModel);
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "createdBy", ignore = true)
+	@Mapping(target = "createdDate", ignore = true)
+	@Mapping(target = "lastModifiedBy", ignore = true)
+	@Mapping(target = "lastModifiedDate", ignore = true)
+	@Mapping(target = "alertType.code", source = "alertTypeCode")
+	@Mapping(target = "language.msLocaleCode", source = "msLanguageCode")
+	public abstract Subscription toDomainObject(@Nullable SubscriptionCreateModel subscriptionModel);
 
-	@Mapping(target= "createdBy", ignore = true)
-	@Mapping(target= "createdDate", ignore = true)
-	@Mapping(target= "lastModifiedBy", ignore = true)
-	@Mapping(target= "lastModifiedDate", ignore = true)
-	@Mapping(target= "alertType.code", source = "alertTypeCode")
-	@Mapping(target= "language.msLocaleCode", source = "msLanguageCode")
-	Subscription toDomain(@Nullable SubscriptionModel subscriptionModel);
+	@Mapping(target = "createdBy", ignore = true)
+	@Mapping(target = "createdDate", ignore = true)
+	@Mapping(target = "lastModifiedBy", ignore = true)
+	@Mapping(target = "lastModifiedDate", ignore = true)
+	@Mapping(target = "alertType.code", source = "alertTypeCode")
+	@Mapping(target = "language.msLocaleCode", source = "msLanguageCode")
+	public abstract Subscription toDomain(@Nullable SubscriptionModel subscriptionModel);
 
 }
