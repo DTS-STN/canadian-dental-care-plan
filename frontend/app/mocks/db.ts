67ebe04095a36bb8b2fee1dacff0c72bbb3ce2af
ESDC-Olivier-Roy <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-06-07 13:54:31 -0400

    fix mock data that were set incorrectly

--- frontend/app/mocks/db.ts
+++ frontend/app/mocks/db.ts
@@ -134,7 +134,7 @@ const unverifiedUserAttributes = [
 ];
 
 const unverifiedUser = db.user.create({
-  id: '76c48130-e1d4-4c2f-8dd0-1c17f9bbb4f6',
+  id: 'uuidddtestOlivierRoy',
   email: 'unverified@example.com',
   emailVerified: false,
   userAttributes: unverifiedUserAttributes,
@@ -150,7 +150,7 @@ db.subscription.create({
 const verifiedUserAttributes = [
   db.userAttributes.create({
     name: 'RAOIDC_USER_ID',
-    value: '8769695a4bcb78e132e06f96c246ea8e',
+    value: '76c48130-e1d4-4c2f-8dd0-1c17f9bbb4f6',
   }),
 ];
 
