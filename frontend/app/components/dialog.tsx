5a885b2f104dd69fc13eecbdc7b90cff69e6995d
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-06-11 14:39:05 -0300

    Fix dialog footer button layout

--- frontend/app/components/dialog.tsx
+++ frontend/app/components/dialog.tsx
@@ -44,10 +44,10 @@ const DialogContent = React.forwardRef<React.ElementRef<typeof DialogPrimitive.C
 });
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
-const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn('flex flex-col space-y-1.5 text-center sm:text-left', className)} {...props} />;
+const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn('flex flex-col space-y-1.5 sm:text-left', className)} {...props} />;
 DialogHeader.displayName = 'DialogHeader';
 
-const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />;
+const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => <div className={cn('flex flex-wrap-reverse justify-end gap-2', className)} {...props} />;
 DialogFooter.displayName = 'DialogFooter';
 
 const DialogTitle = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Title>, React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>>(({ className, ...props }, ref) => (
