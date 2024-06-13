ecba45e425e156df779043ed894343bd972f84be
ESDC-Olivier-Roy <olivier.roy@hrsdc-rhdcc.gc.ca>
2024-06-13 15:12:31 -0400

    refactor model class to interface

--- backend/src/main/java/ca/gov/dtsstn/cdcp/api/web/v1/model/UserAttributeCreateModel.java
+++ backend/src/main/java/ca/gov/dtsstn/cdcp/api/web/v1/model/UserAttributeCreateModel.java
@@ -1,47 +1,18 @@
 package ca.gov.dtsstn.cdcp.api.web.v1.model;
 
-import org.springframework.core.style.ToStringCreator;
-import org.springframework.hateoas.server.core.Relation;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.immutables.value.Value.Immutable;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-
+@Immutable
 @Schema(name = "userAttribute")
-@Relation(collectionRelation = "userAttributes", itemRelation = "userAttribute")
-@JsonPropertyOrder({ "id", "name", "value", "createdBy", "createdDate", "lastModifiedBy", "lastModifiedDate" })
-public class UserAttributeCreateModel {
-	
-	private String name;
-
-	private String value;
+@JsonDeserialize(as = ImmutableUserAttributeCreateModel.class)
+public interface UserAttributeCreateModel {
 
 	@NotBlank
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
+	public String getName(); 
 
 	@NotBlank
-	public String getValue() {
-		return value;
-	}
-
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
-
-	@Override
-	public String toString() {
-		return new ToStringCreator(this)
-			.append("super", super.toString())
-			.append("name", name)
-			.append("value", value)
-			.toString();
-	}
+	public String getValue();
 }
