f0866d9b8fa79c65e6fa35e681611ae1cfe7d710
Frank <48450599+Fbasham@users.noreply.github.com>
2024-06-19 10:49:29 -0400

    add getUserOrigin() unit test from user-origin-utils.server.ts (#1789)

---
+++ frontend/__tests__/utils/user-origin-utils.server.test.ts
@@ -0,0 +1,68 @@
+import { createMemorySessionStorage } from '@remix-run/node';
+
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+import { getUserOrigin } from '~/utils/user-origin-utils.server';
+
+vi.mock('@remix-run/node', () => ({
+  createMemorySessionStorage: vi.fn().mockReturnValue({
+    getSession: vi.fn().mockReturnValue({
+      get: vi.fn(),
+      set: vi.fn(),
+    }),
+  }),
+}));
+
+describe('getUserOrigin', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetModules();
+  });
+
+  it('should return msca if msca is set in the search params', async () => {
+    const url = new URL('http://www.example.com');
+    url.searchParams.append('origin', 'msca');
+    const req = new Request(url);
+    const session = await createMemorySessionStorage({ cookie: { secrets: [''] } }).getSession();
+    expect(getUserOrigin(req, session)).toEqual('msca');
+  });
+
+  it('should return msca-d if msca-d is set in the search params', async () => {
+    const url = new URL('http://www.example.com');
+    url.searchParams.append('origin', 'msca-d');
+    const req = new Request(url);
+    const session = await createMemorySessionStorage({ cookie: { secrets: [''] } }).getSession();
+    expect(getUserOrigin(req, session)).toEqual('msca-d');
+  });
+
+  it('should return msca if msca is set in the session', async () => {
+    vi.mock('@remix-run/node', () => ({
+      createMemorySessionStorage: vi.fn().mockReturnValue({
+        getSession: vi.fn().mockReturnValue({
+          get: vi.fn().mockReturnValueOnce('msca'),
+          set: vi.fn(),
+        }),
+      }),
+    }));
+    const url = new URL('http://www.example.com');
+    const req = new Request(url);
+    const session = await createMemorySessionStorage({ cookie: { secrets: [''] } }).getSession();
+    session.set('userOrigin', 'msca');
+    expect(getUserOrigin(req, session)).toEqual('msca');
+  });
+
+  it('should return msca-d if msca-d is set in the session', async () => {
+    const url = new URL('http://www.example.com');
+    const req = new Request(url);
+    const session = await createMemorySessionStorage({ cookie: { secrets: [''] } }).getSession();
+    session.set('userOrigin', 'msca-d');
+    expect(getUserOrigin(req, session)).toEqual('msca-d');
+  });
+
+  it("should return msca-d as a default if the origin isn't in the search params or session", async () => {
+    const url = new URL('http://www.example.com');
+    const req = new Request(url);
+    const session = await createMemorySessionStorage({ cookie: { secrets: [''] } }).getSession();
+    expect(getUserOrigin(req, session)).toEqual('msca-d');
+  });
+});
