763b4181396480576c46c4ba99a43d2594f30a2a
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-06-27 10:46:03 -0230

    AB#4224 -- Spring Data auditor should use the current user

--- backend/src/main/java/ca/gov/dtsstn/cdcp/api/config/DataSourceConfig.java
+++ backend/src/main/java/ca/gov/dtsstn/cdcp/api/config/DataSourceConfig.java
@@ -1,12 +1,6 @@
 package ca.gov.dtsstn.cdcp.api.config;
 
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -14,17 +8,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @Configuration
 @EnableJpaAuditing
-public class DataSourceConfig {
-
-	static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
-
-	/**
-	 * Creates an {@link AuditorAware} bean that provides the name of
-	 * the currentauthentication context for auditing purposes.
-	 */
-	@Bean AuditorAware<String> auditor() {
-		log.info("Creating 'auditor' bean");
-		return () -> Optional.of("Canadian Dental Care Plan API");
-	}
-
-}
+public class DataSourceConfig {}
