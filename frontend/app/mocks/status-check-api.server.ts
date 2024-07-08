2b8a2895a73330f4ba13506356752a392ba85bd9
Frank <48450599+Fbasham@users.noreply.github.com>
2024-07-08 10:45:20 -0400

    add mock mappings for application codes to application status codes for status checker (#1911)

--- frontend/app/mocks/status-check-api.server.ts
+++ frontend/app/mocks/status-check-api.server.ts
@@ -1,9 +1,31 @@
 import { HttpResponse, http } from 'msw';
 import { z } from 'zod';
 
+import { getLookupService } from '~/services/lookup-service.server';
 import { getLogger } from '~/utils/logging.server';
 
 const log = getLogger('status-check-api.server');
+
+/**
+ * Mock mapping for application codes to application status codes
+ * which can then be used to render on the frontend
+ *
+ * Example mapping:
+ *  {
+ *    '111111': '873ac4c6-77c0-ee11-9079-000d3a09d132',
+ *    '222222': 'f752c665-c4e6-ee11-a204-000d3a09d1b8',
+ *    '333333': 'e882086c-c4e6-ee11-a204-000d3a09d1b8',
+ *    '444444': '4d335896-c4e6-ee11-a204-000d3a09d1b8',
+ *    '555555': '504fba6e-604e-ee11-be6f-000d3a09d640',
+ *    '666666': '1e158ec8-604e-ee11-be6f-000d3a09d640',
+ *    '777777': 'c23252fe-604e-ee11-be6f-000d3a09d640',
+ *    '888888': '9687572e-614e-ee11-be6f-000d3a09d640',
+ *    '999999': '51af5170-614e-ee11-be6f-000d3a09d640'
+ *  }
+ */
+const MOCK_APPLICATION_CODES_TO_STATUS_CODES_MAP: Record<string, string> = getLookupService()
+  .getAllClientFriendlyStatuses()
+  .reduce((acc, { id }, i) => ({ ...acc, [(i + 1).toString().repeat(6)]: id }), {});
 
 /**
  * Server-side MSW mocks for the Status Check API
@@ -39,11 +61,13 @@ export function getStatusCheckApiMockHandlers() {
         return new HttpResponse(null, { status: 401 });
       }
 
+      const statusCode = MOCK_APPLICATION_CODES_TO_STATUS_CODES_MAP[statusRequest.data.BenefitApplication.Applicant.ClientIdentification[0].IdentificationID];
+
       return HttpResponse.json({
         BenefitApplication: {
           BenefitApplicationStatus: [
             {
-              ReferenceDataID: 'c23252fe-604e-ee11-be6f-000d3a09d640',
+              ReferenceDataID: statusCode || 'c23252fe-604e-ee11-be6f-000d3a09d640',
               ReferenceDataName: 'Dental Status Code',
             },
           ],
@@ -83,11 +107,13 @@ export function getStatusCheckApiMockHandlers() {
         return new HttpResponse(null, { status: 401 });
       }
 
+      const statusCode = MOCK_APPLICATION_CODES_TO_STATUS_CODES_MAP[statusRequest.data.BenefitApplication.Applicant.ClientIdentification[0].IdentificationID];
+
       return HttpResponse.json({
         BenefitApplication: {
           BenefitApplicationStatus: [
             {
-              ReferenceDataID: 'c23252fe-604e-ee11-be6f-000d3a09d640',
+              ReferenceDataID: statusCode || 'c23252fe-604e-ee11-be6f-000d3a09d640',
               ReferenceDataName: 'Dental Status Code',
             },
           ],
