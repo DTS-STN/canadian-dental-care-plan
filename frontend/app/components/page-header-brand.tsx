f2ba0603ff41f453a39b1894039e9efc24f5ba62
Frank <48450599+Fbasham@users.noreply.github.com>
2024-07-11 09:55:40 -0400

    update government of canada logo alt text (#1927)

--- frontend/app/components/page-header-brand.tsx
+++ frontend/app/components/page-header-brand.tsx
@@ -3,9 +3,14 @@ import { Link } from '@remix-run/react';
 import { useTranslation } from 'react-i18next';
 
 import { LanguageSwitcher } from '~/components/language-switcher';
+import { getAltLanguage } from '~/utils/locale-utils';
 
 export function PageHeaderBrand() {
   const { i18n, t } = useTranslation(['gcweb']);
+
+  const altLanguage = getAltLanguage(i18n.language);
+  const altLogoContent = <span lang={altLanguage}>{i18n.getFixedT(altLanguage)('gcweb:header.govt-of-canada.text')}</span>;
+
   return (
     <div id="wb-bnr">
       <div className="container flex items-center justify-between gap-6 py-2.5 sm:py-3.5">
@@ -13,6 +18,7 @@ export function PageHeaderBrand() {
           <Link to={t('gcweb:header.govt-of-canada.href')} property="url">
             <img className="h-8 w-auto" src={`/assets/sig-blk-${i18n.language}.svg`} alt={t('gcweb:header.govt-of-canada.text')} property="logo" width="300" height="28" decoding="async" />
           </Link>
+          <span className="sr-only">/ {altLogoContent}</span>
           <meta property="name" content={t('gcweb:header.govt-of-canada.text')} />
           <meta property="areaServed" typeof="Country" content="Canada" />
           <link property="logo" href="/assets/wmms-blk.svg" />
