88c2d702ee815698163ad3900e38f44d3d98e8a3
Benoit Cloutier <77116011+nibivi77@users.noreply.github.com>
2024-07-15 09:11:14 -0400

    Adding Timeoutsession to status-checker (#1937)

--- frontend/app/routes/$lang/_public/status/_route.tsx
+++ frontend/app/routes/$lang/_public/status/_route.tsx
@@ -1,20 +1,41 @@
-import { Outlet } from '@remix-run/react';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { Outlet, isRouteErrorResponse, useLoaderData, useRouteError } from '@remix-run/react';
 
-import { PublicLayout, i18nNamespaces as layoutI18nNamespaces } from '~/components/layouts/public-layout';
+import { NotFoundError, PublicLayout, ServerError, i18nNamespaces as layoutI18nNamespaces } from '~/components/layouts/public-layout';
+import SessionTimeout from '~/components/session-timeout';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { getLocale } from '~/utils/locale-utils.server';
 import type { RouteHandleData } from '~/utils/route-utils';
 
 export const handle = {
   i18nNamespaces: getTypedI18nNamespaces(...layoutI18nNamespaces),
 } as const satisfies RouteHandleData;
 
-/**
- * Do-nothing parent route.
- * (placeholder for future code)
- */
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function loader({ context: { session }, request }: LoaderFunctionArgs) {
+  const lang = getLocale(request);
+  return { lang };
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    switch (error.status) {
+      case 404: {
+        return <NotFoundError error={error} />;
+      }
+    }
+  }
+
+  return <ServerError error={error} />;
+}
+
 export default function Route() {
+  const { lang } = useLoaderData<typeof loader>();
   return (
     <PublicLayout>
+      <SessionTimeout navigateTo={`/${lang}/status`} promptBeforeIdle={5 * 60 * 1000} timeout={15 * 60 * 1000} />
       <Outlet />
     </PublicLayout>
   );
