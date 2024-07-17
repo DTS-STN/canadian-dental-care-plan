c382be75220c2e689213e6e69f0f199019409310
shawn320 <azuneko1110@gmail.com>
2024-07-17 11:14:39 -0400

    refine e2e test

--- frontend/e2e/$lang+/_public/apply/adult/_adult-flow.spec.ts
+++ frontend/e2e/$lang+/_public/apply/adult/_adult-flow.spec.ts
@@ -1,90 +1,226 @@
 import type { Page } from '@playwright/test';
 import { expect, test } from '@playwright/test';
 
+// Reusable funtion to check empty input
+async function hasError(page: Page, error: string) {
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await expect(page.getByRole('link', { name: error })).toBeVisible();
+}
+
 // Reusable function to fill out date of birth
 async function fillOutDOB(page: Page, year: string) {
   await page.getByRole('button', { name: 'Continue' }).click();
-  await expect(page.getByTestId('date-picker-error-month')).toBeVisible();
-  await expect(page.getByTestId('date-picker-error-year')).toBeVisible();
-  await expect(page.getByTestId('date-picker-error-day')).toBeVisible();
   await page.getByTestId('date-picker-month-select').selectOption('01');
   await page.getByTestId('date-picker-day-input').fill('1');
   await page.getByTestId('date-picker-year-input').fill(year);
-  await page.getByRole('button', { name: 'Continue' }).click();
+}
+
+// Reusable function to fill out address
+async function fillOutAddress(page: Page, group: string, address: string, unit: string, country: string, province: string, city: string, postalCode: string) {
+  await page.getByRole('group', { name: group }).getByRole('textbox', { name: 'Address', exact: true }).fill(address);
+  await page.getByRole('group', { name: group }).getByRole('textbox', { name: 'Apartment, suite, etc. (optional)', exact: true }).fill(unit);
+  await page.getByRole('group', { name: group }).getByRole('combobox', { name: 'Country', exact: true }).selectOption(country);
+  await page.getByRole('group', { name: group }).getByRole('combobox', { name: 'Province, territory, state, or region', exact: true }).selectOption(province);
+  await page.getByRole('group', { name: group }).getByRole('textbox', { name: 'City or town', exact: true }).fill(city);
+
+  //invalid postal code
+  await page.getByRole('group', { name: group }).getByRole('textbox', { name: 'Postal code or ZIP code', exact: true }).fill('12345678');
+  await hasError(page, 'Enter mailing address postal code in the correct format, such as A1A 1A1');
+
+  await page.getByRole('group', { name: group }).getByRole('textbox', { name: 'Postal code or ZIP code', exact: true }).fill(postalCode);
 }
 
 test.describe('Adult flow', () => {
-  test('applicant age category is adult', async ({ page }) => {
-    await page.goto('/en/apply');
-    await page.getByRole('button', { name: 'Agree and continue' }).click();
-    await page.getByText('myself', { exact: true }).click();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByLabel('Yes').check();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await fillOutDOB(page, '1990');
-    await page.getByLabel('No').check();
-    await page.getByRole('button', { name: 'Continue' }).click();
+  test('should complete application as adult applicant', async ({ page }) => {
+    await test.step('Terms and condition page', async () => {
+      await page.goto('/en/apply');
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/terms-and-conditions/);
+      await page.getByRole('button', { name: 'Agree and continue' }).click();
+    });
 
-    // should navigate to dob-eligibilty page when applicant's age category is adult and no disability tax credit
-    await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/dob-eligibility/);
+    await test.step('Type of application page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/type-application/);
 
-    await page.getByRole('link', { name: 'Back' }).click();
+      //check for empty fields
+      await hasError(page, 'Select who this application is for');
 
-    await page.getByText('Yes').click();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByTestId('input-sanitize-field-first-name').getByTestId('input-sanitize-field').fill('John');
-    await page.getByTestId('input-sanitize-field-last-name').getByTestId('input-sanitize-field').fill('Smith');
-    await page.getByTestId('input-pattern-field').fill('900000001');
-    await page.getByText('Married').click();
-    await page.getByRole('button', { name: 'Continue' }).click();
+      await page.getByRole('radio', { name: 'I am applying for myself', exact: true }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
 
-    // should navigate to partner information page
-    await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/partner-information/);
+    await test.step('Tax filing page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/tax-filing/);
 
-    await page.getByTestId('input-sanitize-field-first-name').getByTestId('input-sanitize-field').fill('Karen');
-    await page.getByTestId('input-sanitize-field-last-name').getByTestId('input-sanitize-field').fill('Smith');
-    await fillOutDOB(page, '1995');
-    await page.getByTestId('input-pattern-field').fill('968042036');
-    await page.getByTestId('input-checkbox').check();
-    await page.getByRole('button', { name: 'Continue' }).click();
+      //check for empty fields
+      await hasError(page, 'Select whether or not you have filed your taxes');
 
-    // should navigate to contact info page
-    await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/contact-information/);
-    await page.getByTestId('input-email').getByTestId('input-field').fill('example@mail.com');
-    await page.getByTestId('input-confirm-email').getByTestId('input-field').fill('example@mail.com');
-    await page.getByTestId('input-sanitize-field-mailing-address').getByTestId('input-sanitize-field').fill('123 abc st');
-    await page.getByTestId('input-mailing-country-test').selectOption('0cf5389e-97ae-eb11-8236-000d3af4bfc3');
-    await page.getByTestId('input-mailing-province-test').selectOption('daf4d05b-37b3-eb11-8236-0022486d8d5f');
-    await page.getByTestId('input-sanitize-field-mailing-city').getByTestId('input-sanitize-field').fill('Ottawa');
-    await page.getByTestId('input-sanitize-field-mailing-postal-code').getByTestId('input-sanitize-field').fill('H0H0H0');
-    await page.getByText('My home address is the same').click();
+      await page.getByRole('radio', { name: 'No', exact: true }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('File taxes page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/file-taxes/);
+      await page.getByRole('link', { name: 'Back' }).click();
+    });
+
+    await page.getByRole('radio', { name: 'Yes', exact: true }).check();
     await page.getByRole('button', { name: 'Continue' }).click();
 
-    // should navigate to communication preference page
-    await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/communication-preference/);
-    await page.getByLabel('English').check();
-    await page.getByLabel('Email').check();
+    await test.step('Date of birth page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/date-of-birth/);
+
+      //check for empty fields
+      await hasError(page, 'Date of birth must include a year');
+      await hasError(page, 'Date of birth must include a month');
+      await hasError(page, 'Date of birth must include a day');
+
+      await fillOutDOB(page, '1990');
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Disability tax credit page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/disability-tax-credit/);
+
+      //check for empty fields
+      await hasError(page, 'Select whether or not you have a valid disability tax credit');
+
+      await page.getByRole('radio', { name: 'No' }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Find out when you can apply page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/dob-eligibility/);
+      await page.getByRole('link', { name: 'Back' }).click();
+    });
+
+    await page.getByRole('radio', { name: 'Yes' }).check();
     await page.getByRole('button', { name: 'Continue' }).click();
 
-    // should navigate to dental insurance page
-    await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/dental-insurance/);
-    await page.getByLabel('Yes, I have access to dental').check();
-    await page.getByRole('button', { name: 'Continue' }).click();
+    await test.step('Applicant information page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/applicant-information/);
 
-    // should navigate to government programs page
-    await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/federal-provincial-territorial-benefits/);
-    await page.getByLabel('No, I do not have federal dental benefits').check();
-    await page.getByLabel('Yes, I have provincial or territorial dental benefits').check();
-    await page.getByTestId('input-province-test').selectOption('3b17d494-35b3-eb11-8236-0022486d8d5f');
-    await page.getByLabel('Alberta Adult Health Benefits').check();
-    await page.getByLabel('Alberta Adult Health Benefits').click();
-    await page.getByRole('button', { name: 'Continue' }).click();
+      //check for empty fields
+      await hasError(page, 'Enter first name');
+      await hasError(page, 'Enter last name');
+      await hasError(page, 'Enter 9-digit SIN, for example 123 456 789');
+      await hasError(page, 'Select marital status');
 
-    // should navigate to review information page
+      await page.getByRole('textbox', { name: 'First name' }).fill('John');
+      await page.getByRole('textbox', { name: 'Last name' }).fill('Smith');
+      await page.getByRole('textbox', { name: 'Social Insurance Number (SIN)' }).fill('900000001');
+      await page.getByRole('radio', { name: 'Married' }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Partner information page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/partner-information/);
+
+      //check for empty fields
+      await hasError(page, 'Enter first name');
+      await hasError(page, 'Enter last name');
+      await hasError(page, 'Date of birth must include a year');
+      await hasError(page, 'Date of birth must include a month');
+      await hasError(page, 'Date of birth must include a day');
+      await hasError(page, 'Enter 9-digit SIN, for example 123 456 789');
+      await hasError(page, 'Checkbox must be selected');
+
+      await page.getByRole('textbox', { name: 'First name' }).fill('John');
+      await page.getByRole('textbox', { name: 'Last name' }).fill('Smith');
+      await fillOutDOB(page, '1995');
+
+      //check if sin is unique
+      await page.getByRole('textbox', { name: 'Social Insurance Number (SIN)' }).fill('900000001');
+      await hasError(page, 'The Social Insurance Number (SIN) must be unique');
+
+      await page.getByRole('textbox', { name: 'Social Insurance Number (SIN)' }).fill('800000002');
+      await page.getByRole('checkbox', { name: 'I confirm that my spouse or common-law partner is aware and has agreed to share their personal information.' }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Contact information page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/contact-information/);
+
+      //check for empty fields
+      await hasError(page, 'Enter mailing address, typically number and street');
+      await hasError(page, 'Select a country for your mailing address your mailing address');
+      await hasError(page, 'Enter a city or town for your mailing address');
+      await hasError(page, 'Enter home address, typically number and street');
+      await hasError(page, 'Select a country for your home address');
+      await hasError(page, 'Enter a city or town for your home address');
+
+      //invalid phone number
+      await page.getByRole('group', { name: 'Phone number' }).getByRole('textbox', { name: 'Phone number (optional)', exact: true }).fill('111');
+      await hasError(page, "Phone number does not exist. If it's an international phone number, add '+' in front");
+
+      await page.getByRole('group', { name: 'Phone number' }).getByRole('textbox', { name: 'Phone number (optional)', exact: true }).fill('2345678901');
+      await page.getByRole('group', { name: 'Phone number' }).getByRole('textbox', { name: 'Alternate phone number (optional)', exact: true }).fill('2345678902');
+
+      //invalid email
+      await page.getByRole('group', { name: 'Email' }).getByRole('textbox', { name: 'Email address (optional)', exact: true }).fill('123mail');
+      await hasError(page, 'Enter an email address in the correct format, such as name@example.com');
+
+      //email does not match
+      await page.getByRole('group', { name: 'Email' }).getByRole('textbox', { name: 'Email address (optional)', exact: true }).fill('123@mail.com');
+      await page.getByRole('group', { name: 'Email' }).getByRole('textbox', { name: 'Confirm email address (optional)', exact: true }).fill('124@mail.com');
+      await hasError(page, 'The email addresses entered do not match');
+
+      await page.getByRole('group', { name: 'Email' }).getByRole('textbox', { name: 'Confirm email address (optional)', exact: true }).fill('123@mail.com');
+
+      //fillout mailing address
+      await fillOutAddress(page, 'Mailing address', '123 street', '404', 'Canada', 'Ontario', 'Ottawa', 'H0H0H0');
+
+      //fillout home address
+      await fillOutAddress(page, 'Home address', '555 street', '', 'Canada', 'Quebec', 'Montreal', 'K1K1K1');
+
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Communication preference page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/communication-preference/);
+
+      //check for empty fields
+      await hasError(page, 'Select preferred language of communication');
+      await hasError(page, 'Select preferred method of communication');
+
+      await page.getByRole('group', { name: 'What is your preferred official language of communication?' }).getByRole('radio', { name: 'English' }).check();
+      await page.getByRole('group', { name: 'What is your preferred method of communication for Sun Life?' }).getByRole('radio', { name: 'Email' }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Dental insurance page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/dental-insurance/);
+
+      //check for empty fields
+      await hasError(page, 'Select whether you have access to dental insurance');
+
+      await page.getByRole('radio', { name: 'Yes, I have access to dental insurance or coverage', exact: true }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
+    await test.step('Access to other dental benefits page', async () => {
+      await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/federal-provincial-territorial-benefits/);
+
+      //check for empty fields
+      await hasError(page, 'Select whether you have federal dental benefits');
+      await hasError(page, 'Select whether you have provincial or territorial dental benefits');
+
+      await page.getByRole('group', { name: 'Federal benefits' }).getByRole('radio', { name: 'Yes, I have federal dental benefits' }).check();
+      await page.getByRole('group', { name: 'Provincial or territorial benefits' }).getByRole('radio', { name: 'Yes, I have provincial or territorial dental benefits' }).check();
+
+      // check for empty fields
+      await hasError(page, 'Select which federal program you are covered under');
+      await hasError(page, 'Select a province or territory');
+
+      await page.getByRole('group', { name: 'Federal benefits' }).getByRole('radio', { name: 'Correctional Service Canada Health Services' }).check();
+      await page.getByRole('group', { name: 'Provincial or territorial benefits' }).getByRole('combobox', { name: 'If yes, through which province or territory?' }).selectOption('Alberta');
+
+      await hasError(page, 'Select which provincial or territorial program you are covered under');
+      await page.getByRole('group', { name: 'Provincial or territorial benefits' }).getByRole('radio', { name: 'Alberta Adult Health Benefits' }).check();
+      await page.getByRole('button', { name: 'Continue' }).click();
+    });
+
     await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/review-information/);
     await page.getByRole('button', { name: 'Submit Application' }).click();
-
-    // should navigate to confirmationm page
     await expect(page).toHaveURL(/\/en\/apply\/[a-f0-9-]+\/adult\/confirmation/);
   });
 });
