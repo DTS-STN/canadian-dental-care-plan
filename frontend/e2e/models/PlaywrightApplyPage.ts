a32d5c208d3a01c726f007cfdb33e03fe89b0b1b
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-07-30 15:28:58 -0300

    refactor(frontend): add Page object models with Playwright

---
+++ frontend/e2e/models/PlaywrightApplyPage.ts
@@ -0,0 +1,32 @@
+import { PlaywrightBasePage } from './PlaywrightBasePage';
+
+export class PlaywrightApplyPage extends PlaywrightBasePage {
+  async gotoIndexPage() {
+    await this.page.goto('/en/apply');
+  }
+
+  async isLoaded(applyPage: 'index' | 'terms-and-conditions' | 'type-application', heading?: string | RegExp) {
+    let pageInfo: { url: string | RegExp; heading: string | RegExp } | undefined = undefined;
+
+    switch (applyPage) {
+      case 'index':
+        pageInfo = { url: /\/en\/apply/, heading: 'Terms and conditions of use and privacy notice statement' };
+        break;
+
+      case 'terms-and-conditions':
+        pageInfo = { url: /\/en\/apply\/[a-f0-9-]+\/terms-and-conditions/, heading: 'Terms and conditions of use and privacy notice statement' };
+        break;
+
+      case 'type-application':
+        pageInfo = { url: /\/en\/apply\/[a-f0-9-]+\/type-application/, heading: 'Type of application' };
+        break;
+
+      default:
+        pageInfo = undefined;
+        break;
+    }
+
+    if (!pageInfo) throw Error(`applyPage '${applyPage}' not implemented.`);
+    await super.isLoaded(pageInfo.url, heading ?? pageInfo.heading);
+  }
+}
---
+++ frontend/e2e/models/PlaywrightBasePage.ts
@@ -0,0 +1,15 @@
+import { expect } from '@playwright/test';
+import type { Page } from '@playwright/test';
+
+export class PlaywrightBasePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async isLoaded(url: string | RegExp, heading: string | RegExp) {
+    await expect(this.page).toHaveURL(url);
+    await expect(this.page.getByRole('heading', { level: 1, name: heading })).toBeVisible();
+  }
+}
