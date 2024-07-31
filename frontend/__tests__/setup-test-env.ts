ab375930d0069c5cd1843f08698a97fd8cd490e2
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-07-31 16:58:02 -0230

    Remove vitest hack

--- frontend/__tests__/setup-test-env.ts
+++ frontend/__tests__/setup-test-env.ts
@@ -1,14 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 
-import { JSDOM } from 'jsdom';
 import { afterEach } from 'vitest';
-
-// Workaround: For some reason FormData is not set to jsdom's by default
-const jsdom = new JSDOM(`<!doctype html>`);
-const { FormData } = jsdom.window;
-window.FormData = FormData;
-global.FormData = FormData;
 
 /**
  * Jest has their globals API enabled by default. Vitest does not. We can either enable globals via the globals
