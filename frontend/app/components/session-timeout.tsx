a581561ac32770672c43735d2fd7e5eb8ac4d3fd
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-08-08 10:58:47 -0300

    fix(frontend): use Remix navigation and form events to activate/reset SessionTimeout

--- frontend/app/components/session-timeout.tsx
+++ frontend/app/components/session-timeout.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { useFetchers, useLocation, useNavigation } from '@remix-run/react';
+
 import { useTranslation } from 'react-i18next';
 import type { IIdleTimerProps } from 'react-idle-timer';
 import { useIdleTimer } from 'react-idle-timer';
@@ -11,49 +13,105 @@ import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 const i18nNamespaces = getTypedI18nNamespaces('gcweb');
 
 export interface SessionTimeoutProps extends Required<Pick<IIdleTimerProps, 'promptBeforeIdle' | 'timeout'>> {
+  /**
+   * Function to call when the session end is triggered.
+   */
   onSessionEnd: () => Promise<void> | void;
+
+  /**
+   * Function to call when the session extend is triggered.
+   */
   onSessionExtend: () => Promise<void> | void;
 }
 
+/**
+ * SessionTimeout component to handle user session timeout prompts.
+ * This component uses the `react-IdleTimer` library to manage idle time and prompt the user
+ * before the session expires. It integrates with Remix's to activate the IdleTimer on route changes,
+ * fetcher submissions, and form submissions.
+ */
 const SessionTimeout = ({ promptBeforeIdle, timeout, onSessionEnd, onSessionExtend }: SessionTimeoutProps) => {
   const { t } = useTranslation(i18nNamespaces);
-  const [modalOpen, setModalOpen] = useState(false);
   const [timeRemaining, setTimeRemaining] = useState('');
 
-  const { activate, getRemainingTime } = useIdleTimer({
+  const { activate, isPrompted, getRemainingTime } = useIdleTimer({
+    // Disable default event listeners; The IdleTimer should only activate during route navigation and form
+    // submissions, as these actions will interact with the session and extend its lifespan.
+    events: [],
     onIdle: () => {
+      // Trigger the session end function when the user becomes idle.
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
       endSession();
-    },
-    onPrompt: () => {
-      setModalOpen(true);
     },
     promptBeforeIdle,
     timeout,
   });
 
+  // Track the current location and its key for detecting navigation changes.
+  const location = useLocation();
+  const locationKey = location.key;
+
+  useEffect(() => {
+    // Activate the IdleTimer whenever the location changes (indicating a route navigation).
+    activate();
+  }, [locationKey, activate]);
+
+  // Track fetcher states to activate the IdleTimer during submissions.
+  const fetchers = useFetchers();
+  const fetcherSubmitting = fetchers.filter(({ state }) => state === 'submitting').length > 0;
+
+  useEffect(() => {
+    // Activate the IdleTimer if any fetcher is submitting.
+    if (fetcherSubmitting) {
+      activate();
+    }
+  }, [fetcherSubmitting, activate]);
+
+  // Track Remix's <Form> submission state to activate the IdleTimer during <Form> submissions.
+  const navigation = useNavigation();
+  const formSubmitting = navigation.state === 'submitting';
+
+  useEffect(() => {
+    // Activate the IdleTimer if a <Form> is submitting.
+    if (formSubmitting) {
+      activate();
+    }
+  }, [formSubmitting, activate]);
+
+  /**
+   * Function to end the session.
+   */
   async function endSession() {
     await onSessionEnd();
-    setModalOpen(false);
-    activate();
   }
 
+  /**
+   * Function to extend the session.
+   */
   async function extendSession() {
     await onSessionExtend();
-    setModalOpen(false);
-    activate();
   }
 
+  /**
+   * Handler for when the dialog open state changes.
+   * If the dialog is closed, the session is extended.
+   */
   async function handleOnDialogOpenChange(open: boolean) {
     if (!open) {
       await extendSession();
     }
   }
 
+  /**
+   * Handler for when the "End Session" button is clicked.
+   */
   async function handleOnEndSessionButtonClick() {
     await endSession();
   }
 
+  /**
+   * Handler for when the "Continue Session" button is clicked.
+   */
   async function handleOnExtendSessionButtonClick() {
     await extendSession();
   }
@@ -76,7 +134,7 @@ const SessionTimeout = ({ promptBeforeIdle, timeout, onSessionEnd, onSessionExte
   }, [getRemainingTime]);
 
   return (
-    <Dialog open={modalOpen} onOpenChange={handleOnDialogOpenChange}>
+    <Dialog open={isPrompted()} onOpenChange={handleOnDialogOpenChange}>
       <DialogContent aria-describedby={undefined} className="sm:max-w-md">
         <DialogHeader>
           <DialogTitle>{t('session-timeout.header')}</DialogTitle>
