9548359a1b889071054e443726807da623fffd6c
shawn320 <azuneko1110@gmail.com>
2024-08-20 12:06:15 -0400

    teamcity reporter

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['playwright-teamcity-reporter']],
   use: { baseURL: `http://localhost:${port}/`, trace: 'on-first-retry' },
   projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   webServer: {
