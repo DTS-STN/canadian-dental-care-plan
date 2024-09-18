40f70f1760c7e085c681c3f8e1b6c2f5de26d6fc
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-09-18 11:59:03 -0300

    refactor(frontend): add preferred communication method service

--- frontend/app/utils/env-utils.server.ts
+++ frontend/app/utils/env-utils.server.ts
@@ -161,6 +161,7 @@ const serverEnv = clientEnvSchema.extend({
   LOOKUP_SVC_COUNTRY_CACHE_TTL_SECONDS: z.coerce.number().default(24 * 60 * 60),
   LOOKUP_SVC_FEDERAL_GOVERNMENT_INSURANCE_PLAN_CACHE_TTL_SECONDS: z.coerce.number().default(24 * 60 * 60),
   LOOKUP_SVC_MARITAL_STATUSE_CACHE_TTL_SECONDS: z.coerce.number().default(24 * 60 * 60),
+  LOOKUP_SVC_PREFERRED_COMMUNICATION_METHOD_CACHE_TTL_SECONDS: z.coerce.number().default(24 * 60 * 60),
   LOOKUP_SVC_PREFERRED_LANGUAGE_CACHE_TTL_SECONDS: z.coerce.number().default(24 * 60 * 60),
   GET_ALL_LETTER_TYPES_CACHE_TTL_SECONDS: z.coerce.number().default(24 * 60 * 60),
 
