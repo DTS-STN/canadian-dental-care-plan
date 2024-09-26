bc2a7bde7d40018cdee17986532c2430ae6a590e
shawn320 <azuneko1110@gmail.com>
2024-09-26 10:56:34 -0400

    refactor

--- frontend/app/routes/public/apply/$id/child/children/index.tsx
+++ frontend/app/routes/public/apply/$id/child/children/index.tsx
@@ -58,7 +58,7 @@ export async function loader({ context: { serviceProvider, session }, params, re
 
   const children = getChildrenState(state).map((child) => {
     const federalGovernmentInsurancePlanService = child.dentalBenefits?.federalSocialProgram ? serviceProvider.getFederalGovernmentInsurancePlanService().findById(child.dentalBenefits.federalSocialProgram) : undefined;
-    const provincialTerritorialSocialProgram = child.dentalBenefits?.provincialTerritorialSocialProgram ? serviceProvider.getProvincialGovernmentInsurancePlanService().findById(child.dentalBenefits.provincialTerritorialSocialProgram) : undefined;
+    const provincialTerritorialSocialProgram = child.dentalBenefits?.provincialTerritorialSocialProgram ? serviceProvider.getProvincialGovernmentInsurancePlanService().getPlanById(child.dentalBenefits.provincialTerritorialSocialProgram) : undefined;
 
     return {
       ...child,
