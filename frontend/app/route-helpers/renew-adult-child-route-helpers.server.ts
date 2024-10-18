b6892af6d1d211147c607505cf70777ae49f5e45
Frank Basham <frankabasham@gmail.com>
2024-10-18 10:00:35 -0400

    add renew adult child flow files

---
+++ frontend/app/route-helpers/renew-adult-child-route-helpers.server.ts
@@ -0,0 +1,137 @@
+import type { Session } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import type { Params } from '@remix-run/react';
+import { isRedirectResponse, isResponse } from '@remix-run/server-runtime/dist/responses';
+
+import { loadRenewState, saveRenewState } from './renew-route-helpers.server';
+import type { RenewState } from './renew-route-helpers.server';
+import { getLogger } from '~/utils/logging.server';
+import { getPathById } from '~/utils/route-utils';
+
+interface LoadRenewAdultChildStateArgs {
+  params: Params;
+  request: Request;
+  session: Session;
+}
+
+/**
+ * Loads renew AdultChild state.
+ * @param args - The arguments.
+ * @returns The loaded AdultChild state.
+ */
+export function loadRenewAdultChildState({ params, request, session }: LoadRenewAdultChildStateArgs) {
+  const log = getLogger('renew-adult-child-route-helpers.server/loadRenewAdultChildState');
+  const { pathname } = new URL(request.url);
+  const renewState = loadRenewState({ params, session });
+
+  if (renewState.typeOfRenewal !== 'adult-child') {
+    throw redirect(getPathById('public/renew/$id/type-renewal', params));
+  }
+
+  // Redirect to the confirmation page if the application has been submitted and
+  // the current route is not the confirmation page.
+  const confirmationRouteUrl = getPathById('public/renew/$id/adult-child/confirmation', params);
+  if (renewState.submissionInfo && !pathname.endsWith(confirmationRouteUrl)) {
+    log.warn('Redirecting user to "%s" since the application has been submitted; sessionId: [%s], ', renewState.id, confirmationRouteUrl);
+    throw redirect(confirmationRouteUrl);
+  }
+
+  // Redirect to the first flow page if the application has not been submitted and
+  // the current route is the confirmation page.
+  const termsAndConditionsRouteUrl = getPathById('public/renew/$id/terms-and-conditions', params);
+  if (!renewState.submissionInfo && pathname.endsWith(confirmationRouteUrl)) {
+    log.warn('Redirecting user to "%s" since the application has not been submitted; sessionId: [%s], ', renewState.id, termsAndConditionsRouteUrl);
+    throw redirect(termsAndConditionsRouteUrl);
+  }
+
+  return renewState;
+}
+
+interface LoadRenewAdultChildStateForReviewArgs {
+  params: Params;
+  request: Request;
+  session: Session;
+}
+
+/**
+ * Loads the renewal state for the review page. It validates the state and throws a redirect if invalid.
+ * If a redirect exception is thrown, state.editMode is set to false.
+ * @param args - The arguments.
+ * @returns The validated adult state.
+ */
+export function loadRenewAdultChildStateForReview({ params, request, session }: LoadRenewAdultChildStateForReviewArgs) {
+  const state = loadRenewAdultChildState({ params, request, session });
+
+  try {
+    return validateRenewAdultChildStateForReview({ params, state });
+  } catch (err) {
+    if (isResponse(err) && isRedirectResponse(err)) {
+      saveRenewState({ params, session, state: { editMode: false } });
+    }
+    throw err;
+  }
+}
+
+interface ValidateRenewAdultChildStateForReviewArgs {
+  params: Params;
+  state: RenewState;
+}
+
+export function validateRenewAdultChildStateForReview({ params, state }: ValidateRenewAdultChildStateForReviewArgs) {
+  const { maritalStatus, partnerInformation, contactInformation, editMode, id, submissionInfo, typeOfRenewal, communicationPreference, addressInformation, applicantInformation, dentalBenefits, dentalInsurance } = state;
+
+  if (typeOfRenewal === undefined) {
+    throw redirect(getPathById('public/renew/$id/type-renewal', params));
+  }
+
+  if (typeOfRenewal === 'delegate') {
+    throw redirect(getPathById('public/renew/$id/renewal-delegate', params));
+  }
+
+  if (typeOfRenewal !== 'adult-child') {
+    throw redirect(getPathById('public/renew/$id/type-renewal', params));
+  }
+
+  if (applicantInformation === undefined) {
+    throw redirect(getPathById('public/renew/$id/applicant-information', params));
+  }
+
+  if (maritalStatus === undefined) {
+    throw redirect(getPathById('public/renew/$id/adult-child/marital-status', params));
+  }
+
+  if (contactInformation === undefined) {
+    throw redirect(getPathById('public/renew/$id/adult-child/contact-information', params));
+  }
+
+  if (communicationPreference === undefined) {
+    throw redirect(getPathById('public/renew/$id/adult-child/communication-preference', params));
+  }
+
+  if (addressInformation === undefined) {
+    throw redirect(getPathById('public/renew/$id/adult-child/confirm-address', params));
+  }
+
+  if (dentalInsurance === undefined) {
+    throw redirect(getPathById('public/renew/$id/adult-child/dental-insurance', params));
+  }
+
+  if (dentalBenefits === undefined) {
+    throw redirect(getPathById('public/renew/$id/adult-child/federal-provincial-territorial-benefits', params));
+  }
+
+  return {
+    maritalStatus,
+    editMode,
+    id,
+    submissionInfo,
+    typeOfRenewal,
+    contactInformation,
+    communicationPreference,
+    applicantInformation,
+    dentalBenefits,
+    dentalInsurance,
+    addressInformation,
+    partnerInformation,
+  };
+}
---
+++ frontend/app/routes/public/renew/$id/adult-child/marital-status.tsx
@@ -0,0 +1,230 @@
+import type { ChangeEventHandler } from 'react';
+import { useMemo, useState } from 'react';
+
+import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
+import { useFetcher, useLoaderData, useParams } from '@remix-run/react';
+
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+
+import pageIds from '../../../../page-ids.json';
+import { Button, ButtonLink } from '~/components/buttons';
+import { useErrorSummary } from '~/components/error-summary';
+import { InputCheckbox } from '~/components/input-checkbox';
+import { InputPatternField } from '~/components/input-pattern-field';
+import type { InputRadiosProps } from '~/components/input-radios';
+import { InputRadios } from '~/components/input-radios';
+import { LoadingButton } from '~/components/loading-button';
+import { Progress } from '~/components/progress';
+import { loadRenewAdultChildState } from '~/route-helpers/renew-adult-child-route-helpers.server';
+import type { PartnerInformationState } from '~/route-helpers/renew-route-helpers.server';
+import { renewStateHasPartner, saveRenewState } from '~/route-helpers/renew-route-helpers.server';
+import { getEnv } from '~/utils/env-utils.server';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { getFixedT, getLocale } from '~/utils/locale-utils.server';
+import { getLogger } from '~/utils/logging.server';
+import { mergeMeta } from '~/utils/meta-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+import { getPathById } from '~/utils/route-utils';
+import { getTitleMetaTags } from '~/utils/seo-utils';
+import { formatSin, isValidSin, sinInputPatternFormat } from '~/utils/sin-utils';
+import { transformFlattenedError } from '~/utils/zod-utils.server';
+
+enum FormAction {
+  Continue = 'continue',
+  Cancel = 'cancel',
+  Save = 'save',
+}
+
+export const handle = {
+  i18nNamespaces: getTypedI18nNamespaces('renew-adult-child', 'renew', 'gcweb'),
+  pageIdentifier: pageIds.public.renew.adultChild.maritalStatus,
+  pageTitleI18nKey: 'renew-adult-child:marital-status.page-title',
+} as const satisfies RouteHandleData;
+
+export const meta: MetaFunction<typeof loader> = mergeMeta(({ data }) => {
+  return data ? getTitleMetaTags(data.meta.title) : [];
+});
+
+export async function loader({ context: { serviceProvider, session }, params, request }: LoaderFunctionArgs) {
+  const state = loadRenewAdultChildState({ params, request, session });
+  const t = await getFixedT(request, handle.i18nNamespaces);
+  const locale = getLocale(request);
+  const maritalStatuses = serviceProvider.getMaritalStatusService().listLocalizedMaritalStatuses(locale);
+  const { MARITAL_STATUS_CODE_COMMONLAW, MARITAL_STATUS_CODE_MARRIED } = getEnv();
+
+  const csrfToken = String(session.get('csrfToken'));
+  const meta = { title: t('gcweb:meta.title.template', { title: t('renew-adult-child:marital-status.page-title') }) };
+
+  return json({ csrfToken, defaultState: { maritalStatus: state.maritalStatus, ...state.partnerInformation }, editMode: state.editMode, id: state.id, maritalStatuses, meta, MARITAL_STATUS_CODE_COMMONLAW, MARITAL_STATUS_CODE_MARRIED });
+}
+
+export async function action({ context: { session }, params, request }: ActionFunctionArgs) {
+  const log = getLogger('renew/adult-child/marital-status');
+
+  const state = loadRenewAdultChildState({ params, request, session });
+  const t = await getFixedT(request, handle.i18nNamespaces);
+
+  const formData = await request.formData();
+  const expectedCsrfToken = String(session.get('csrfToken'));
+  const submittedCsrfToken = String(formData.get('_csrf'));
+
+  if (expectedCsrfToken !== submittedCsrfToken) {
+    log.warn('Invalid CSRF token detected; expected: [%s], submitted: [%s]', expectedCsrfToken, submittedCsrfToken);
+    throw new Response('Invalid CSRF token', { status: 400 });
+  }
+
+  // state validation schema
+  const maritalStatusSchema = z.object({
+    maritalStatus: z
+      .string({ errorMap: () => ({ message: t('renew-adult-child:marital-status.error-message.marital-status-required') }) })
+      .trim()
+      .min(1, t('renew-adult-child:marital-status.error-message.marital-status-required')),
+  });
+
+  const partnerInformationSchema = z.object({
+    confirm: z.boolean().refine((val) => val === true, t('renew-adult-child:marital-status.error-message.confirm-required')),
+    yearOfBirth: z.string().trim().min(1, t('renew-adult-child:marital-status.error-message.date-of-birth-year-required')),
+    socialInsuranceNumber: z
+      .string()
+      .trim()
+      .min(1, t('renew-adult-child:marital-status.error-message.sin-required'))
+      .refine(isValidSin, t('renew-adult-child:marital-status.error-message.sin-valid'))
+      .refine((sin) => isValidSin(sin) && formatSin(sin, '') !== state.partnerInformation?.socialInsuranceNumber, t('renew-adult-child:marital-status.error-message.sin-unique')),
+  }) satisfies z.ZodType<PartnerInformationState>;
+
+  const maritalStatusData = {
+    maritalStatus: formData.get('maritalStatus') ? String(formData.get('maritalStatus')) : undefined,
+  };
+  const partnerInformationData = {
+    confirm: formData.get('confirm') === 'yes',
+    yearOfBirth: String(formData.get('yearOfBirth') ?? ''),
+    socialInsuranceNumber: String(formData.get('socialInsuranceNumber') ?? ''),
+  };
+
+  const parsedMaritalStatus = maritalStatusSchema.safeParse(maritalStatusData);
+  const parsedPartnerInformation = partnerInformationSchema.safeParse(partnerInformationData);
+
+  if (!parsedMaritalStatus.success || (renewStateHasPartner(parsedMaritalStatus.data.maritalStatus) && !parsedPartnerInformation.success)) {
+    return json({
+      errors: {
+        ...(parsedMaritalStatus.error ? transformFlattenedError(parsedMaritalStatus.error.flatten()) : {}),
+        ...(parsedMaritalStatus.success && renewStateHasPartner(parsedMaritalStatus.data.maritalStatus) && parsedPartnerInformation.error ? transformFlattenedError(parsedPartnerInformation.error.flatten()) : {}),
+      },
+    });
+  }
+
+  saveRenewState({ params, session, state: { maritalStatus: parsedMaritalStatus.data.maritalStatus, partnerInformation: parsedPartnerInformation.data } });
+
+  if (state.editMode) {
+    return redirect(getPathById('public/renew/$id/adult-family/review-information', params));
+  }
+
+  return redirect(getPathById('public/renew/$id/adult-family/contact-information', params));
+}
+
+export default function RenewAdultChildMaritalStatus() {
+  const { t } = useTranslation(handle.i18nNamespaces);
+  const { csrfToken, defaultState, editMode, maritalStatuses, MARITAL_STATUS_CODE_COMMONLAW, MARITAL_STATUS_CODE_MARRIED } = useLoaderData<typeof loader>();
+  const params = useParams();
+  const fetcher = useFetcher<typeof action>();
+  const isSubmitting = fetcher.state !== 'idle';
+
+  const [marriedOrCommonlaw, setMarriedOrCommonlaw] = useState(defaultState.maritalStatus);
+
+  const errors = fetcher.data?.errors;
+  const errorSummary = useErrorSummary(errors, {
+    maritalStatus: 'input-radio-marital-status-option-0',
+    yearOfBirth: 'year-of-birth',
+    socialInsuranceNumber: 'social-insurance-number',
+    confirm: 'input-checkbox-confirm',
+  });
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setMarriedOrCommonlaw(e.target.value);
+  };
+
+  const maritalStatusOptions = useMemo<InputRadiosProps['options']>(() => {
+    return maritalStatuses.map((status) => ({ defaultChecked: status.id === defaultState.maritalStatus, children: status.name, value: status.id, onChange: handleChange }));
+  }, [defaultState, maritalStatuses]);
+
+  return (
+    <>
+      <div className="my-6 sm:my-8">
+        <Progress value={44} size="lg" label={t('renew:progress.label')} />
+      </div>
+      <div className="max-w-prose">
+        <p className="mb-4 italic">{t('renew:required-label')}</p>
+        <errorSummary.ErrorSummary />
+        <fetcher.Form method="post" noValidate>
+          <input type="hidden" name="_csrf" value={csrfToken} />
+          <div className="mb-8 space-y-6">
+            <InputRadios id="marital-status" name="maritalStatus" legend={t('renew-adult-child:marital-status.marital-status')} options={maritalStatusOptions} errorMessage={errors?.maritalStatus} required />
+
+            {(marriedOrCommonlaw === MARITAL_STATUS_CODE_COMMONLAW.toString() || marriedOrCommonlaw === MARITAL_STATUS_CODE_MARRIED.toString()) && (
+              <>
+                <h2 className="mb-6 font-lato text-2xl font-bold">{t('renew-adult-child:marital-status.spouse-or-commonlaw')}</h2>
+                <p className="mb-4">{t('renew-adult-child:marital-status.provide-sin')}</p>
+                <p className="mb-6">{t('renew-adult-child:marital-status.required-information')}</p>
+                <InputPatternField
+                  id="year-of-birth"
+                  name="yearOfBirth"
+                  inputMode="numeric"
+                  format="####"
+                  defaultValue={defaultState.yearOfBirth ?? ''}
+                  label={t('renew-adult-child:marital-status.year-of-birth')}
+                  errorMessage={errors?.yearOfBirth}
+                  required
+                />
+                <InputPatternField
+                  id="social-insurance-number"
+                  name="socialInsuranceNumber"
+                  format={sinInputPatternFormat}
+                  label={t('marital-status.sin')}
+                  inputMode="numeric"
+                  helpMessagePrimary={t('renew-adult-child:marital-status.help-message.sin')}
+                  helpMessagePrimaryClassName="text-black"
+                  defaultValue={defaultState.socialInsuranceNumber ?? ''}
+                  errorMessage={errors?.socialInsuranceNumber}
+                  required
+                />
+                <InputCheckbox id="confirm" name="confirm" value="yes" errorMessage={errors?.confirm} defaultChecked={defaultState.confirm === true} required>
+                  {t('renew-adult-child:marital-status.confirm-checkbox')}
+                </InputCheckbox>
+              </>
+            )}
+          </div>
+          {editMode ? (
+            <div className="flex flex-wrap items-center gap-3">
+              <Button id="save-button" name="_action" value={FormAction.Save} variant="primary" disabled={isSubmitting} data-gc-analytics-customclick="ESDC-EDSC:CDCP Renew Application Form-Adult:Save - Marital status click">
+                {t('renew-adult-child:marital-status.save-btn')}
+              </Button>
+              <Button id="cancel-button" name="_action" value={FormAction.Cancel} disabled={isSubmitting} data-gc-analytics-customclick="ESDC-EDSC:CDCP Renew Application Form-Adult:Cancel - Marital status click">
+                {t('renew-adult-child:marital-status.cancel-btn')}
+              </Button>
+            </div>
+          ) : (
+            <div className="flex flex-row-reverse flex-wrap items-center justify-end gap-3">
+              <LoadingButton
+                id="continue-button"
+                name="_action"
+                value={FormAction.Continue}
+                variant="primary"
+                loading={isSubmitting}
+                endIcon={faChevronRight}
+                data-gc-analytics-customclick="ESDC-EDSC:CDCP Renew Application Form-Adult:Continue - Marital status click"
+              >
+                {t('renew-adult-child:marital-status.continue-btn')}
+              </LoadingButton>
+              <ButtonLink id="back-button" routeId="public/renew/$id/type-renewal" params={params} disabled={isSubmitting} startIcon={faChevronLeft} data-gc-analytics-customclick="ESDC-EDSC:CDCP Renew Application Form-Adult:Back - Marital status click">
+                {t('renew-adult-child:marital-status.back-btn')}
+              </ButtonLink>
+            </div>
+          )}
+        </fetcher.Form>
+      </div>
+    </>
+  );
+}
