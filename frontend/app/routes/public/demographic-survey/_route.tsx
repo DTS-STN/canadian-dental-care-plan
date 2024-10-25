0d23275ee882f91200da05725b5b2f6e2cf2c5d6
Frank Basham <frankabasham@gmail.com>
2024-10-25 10:01:02 -0400

    add initial folder structure and files for demographic survey app

---
+++ frontend/app/routes/public/demographic-survey/_route.tsx
@@ -0,0 +1,54 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { Outlet, isRouteErrorResponse, useLoaderData, useRouteError } from '@remix-run/react';
+
+import { NotFoundError, PublicLayout, ServerError, i18nNamespaces as layoutI18nNamespaces } from '~/components/layouts/public-layout';
+import SessionTimeout from '~/components/session-timeout';
+import { useApiSession } from '~/utils/api-session-utils';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { getLocale } from '~/utils/locale-utils.server';
+import type { RouteHandleData } from '~/utils/route-utils';
+
+export const handle = {
+  i18nNamespaces: getTypedI18nNamespaces(...layoutI18nNamespaces),
+} as const satisfies RouteHandleData;
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function loader({ context: { configProvider, serviceProvider, session }, request }: LoaderFunctionArgs) {
+  const locale = getLocale(request);
+  const { SESSION_TIMEOUT_PROMPT_SECONDS, SESSION_TIMEOUT_SECONDS } = configProvider.getClientConfig();
+  return { locale, SESSION_TIMEOUT_PROMPT_SECONDS, SESSION_TIMEOUT_SECONDS };
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    switch (error.status) {
+      case 404: {
+        return <NotFoundError error={error} />;
+      }
+    }
+  }
+
+  return <ServerError error={error} />;
+}
+
+export default function Route() {
+  const { locale, SESSION_TIMEOUT_PROMPT_SECONDS, SESSION_TIMEOUT_SECONDS } = useLoaderData<typeof loader>();
+  const apiSession = useApiSession();
+
+  function handleOnSessionEnd() {
+    apiSession.submit({ action: 'end', locale, redirectTo: 'cdcp-website-status' });
+  }
+
+  function handleOnSessionExtend() {
+    apiSession.submit({ action: 'extend' });
+  }
+
+  return (
+    <PublicLayout>
+      <SessionTimeout promptBeforeIdle={SESSION_TIMEOUT_PROMPT_SECONDS * 1000} timeout={SESSION_TIMEOUT_SECONDS * 1000} onSessionEnd={handleOnSessionEnd} onSessionExtend={handleOnSessionExtend} />
+      <Outlet />
+    </PublicLayout>
+  );
+}
