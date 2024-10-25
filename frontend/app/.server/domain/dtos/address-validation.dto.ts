f8a8848dfe87a3667d2b0add30bcde0f6de78f48
Nicholas Ly <55502055+nicholas-ly@users.noreply.github.com>
2024-10-25 08:20:59 -0400

    Moving AddressCorrectionRequest interfaces into their respective DTO/entity types (#2359)

--- frontend/app/.server/domain/dtos/address-validation.dto.ts
+++ frontend/app/.server/domain/dtos/address-validation.dto.ts
@@ -1,4 +1,21 @@
 /**
+ * Represents a Data Transfer Object (DTO) for an address correction request.
+ */
+export type AddressCorrectionRequestDto = Readonly<{
+  /** The full or partial address. */
+  address: string;
+
+  /** The name of the city. */
+  city: string;
+
+  /** The 6-character postal code. */
+  postalCode: string;
+
+  /** The 2-character Canadian province or territorial code. */
+  provinceCode: string;
+}>;
+
+/**
  * Represents a Data Transfer Object (DTO) for an address correction result.
  */
 export type AddressCorrectionResultDto = Readonly<{
