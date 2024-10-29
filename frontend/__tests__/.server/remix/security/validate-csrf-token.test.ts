97800f982e8e4fe59f2a0776236cf5475929a80d
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-10-29 17:46:38 -0300

    refactor(frontend): add 'validateCsrfToken' function to 'remix' security domain

---
+++ frontend/__tests__/.server/remix/security/validate-csrf-token.test.ts
@@ -0,0 +1,56 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+
+import { describe, expect, it, vi } from 'vitest';
+
+import { validateCsrfToken } from '~/.server/remix/security/validate-csrf-token';
+import { CsrfTokenInvalidException } from '~/.server/web/exceptions/csrf-token-invalid.exception';
+
+describe('validateCsrfToken', () => {
+  it('should call the CSRF token validator with the request', async () => {
+    const mockValidateCsrfToken = vi.fn();
+    const mockRequest = {} as ActionFunctionArgs['request'];
+    const mockContext = {
+      webValidatorProvider: {
+        getCsrfTokenValidator: vi.fn().mockReturnValue({
+          validateCsrfToken: mockValidateCsrfToken,
+        }),
+      },
+    } as unknown as ActionFunctionArgs['context'];
+
+    await validateCsrfToken({ context: mockContext, request: mockRequest });
+
+    expect(mockValidateCsrfToken).toHaveBeenCalledWith(mockRequest);
+  });
+
+  it('should throw a 403 response if the CSRF token is invalid', async () => {
+    const mockRequest = {} as ActionFunctionArgs['request'];
+    const mockContext = {
+      webValidatorProvider: {
+        getCsrfTokenValidator: vi.fn().mockReturnValue({
+          validateCsrfToken: vi.fn().mockImplementation(() => {
+            throw new CsrfTokenInvalidException('Invalid CSRF token');
+          }),
+        }),
+      },
+    } as unknown as ActionFunctionArgs['context'];
+
+    const response = await validateCsrfToken({ context: mockContext, request: mockRequest }).catch((e) => e);
+
+    expect(response.status).toBe(403);
+  });
+
+  it('should rethrow any other errors', async () => {
+    const mockRequest = {} as ActionFunctionArgs['request'];
+    const mockContext = {
+      webValidatorProvider: {
+        getCsrfTokenValidator: vi.fn().mockReturnValue({
+          validateCsrfToken: vi.fn().mockImplementation(() => {
+            throw new Error('Something went wrong');
+          }),
+        }),
+      },
+    } as unknown as ActionFunctionArgs['context'];
+
+    await expect(validateCsrfToken({ context: mockContext, request: mockRequest })).rejects.toThrowError('Something went wrong');
+  });
+});
