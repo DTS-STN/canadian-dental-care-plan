6933e041b1dc1585ee195db05f75f4690af0f95a
Frank Basham <frankabasham@gmail.com>
2024-11-01 13:09:45 -0400

    add demographic survey submitted route

---
+++ frontend/app/routes/public/demographic-survey/$id/submitted.tsx
@@ -0,0 +1,74 @@
+import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
+import { useFetcher, useLoaderData } from '@remix-run/react';
+
+import { useTranslation } from 'react-i18next';
+
+import pageIds from '../../../page-ids.json';
+import { LoadingButton } from '~/components/loading-button';
+import { clearDemographicSurveyState } from '~/route-helpers/demographic-survey-route-helpers.server';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { getFixedT } from '~/utils/locale-utils.server';
+import { getLogger } from '~/utils/logging.server';
+import { mergeMeta } from '~/utils/meta-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+import { getTitleMetaTags } from '~/utils/seo-utils';
+
+export const handle = {
+  i18nNamespaces: getTypedI18nNamespaces('demographic-survey', 'gcweb'),
+  pageIdentifier: pageIds.public.demographicSurvey.submitted,
+  pageTitleI18nKey: 'demographic-survey:submitted.page-title',
+} as const satisfies RouteHandleData;
+
+export const meta: MetaFunction<typeof loader> = mergeMeta(({ data }) => {
+  return data ? getTitleMetaTags(data.meta.title) : [];
+});
+
+export async function loader({ context: { configProvider, serviceProvider, session }, params, request }: LoaderFunctionArgs) {
+  const csrfToken = String(session.get('csrfToken'));
+
+  const t = await getFixedT(request, handle.i18nNamespaces);
+  const meta = { title: t('gcweb:meta.title.template', { title: t('demographic-survey:submitted.page-title') }) };
+
+  return json({ csrfToken, meta });
+}
+
+export async function action({ context: { configProvider, serviceProvider, session }, params, request }: ActionFunctionArgs) {
+  const log = getLogger('demographic-survey/submitted');
+
+  const t = await getFixedT(request, handle.i18nNamespaces);
+
+  const formData = await request.formData();
+  const expectedCsrfToken = String(session.get('csrfToken'));
+  const submittedCsrfToken = String(formData.get('_csrf'));
+
+  if (expectedCsrfToken !== submittedCsrfToken) {
+    log.warn('Invalid CSRF token detected; expected: [%s], submitted: [%s]', expectedCsrfToken, submittedCsrfToken);
+    throw new Response('Invalid CSRF token', { status: 400 });
+  }
+
+  clearDemographicSurveyState({ params, session });
+  return redirect(t('demographic-survey:submitted.exit-link'));
+}
+
+export default function DemographicSurveySubmitted() {
+  const { t } = useTranslation(handle.i18nNamespaces);
+  const { csrfToken } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher<typeof action>();
+  const isSubmitting = fetcher.state !== 'idle';
+
+  return (
+    <div className="max-w-prose">
+      <div className="mb-8 space-y-4">
+        <p>{t('demographic-survey:submitted.response-submitted')}</p>
+        <p>{t('demographic-survey:submitted.thank-you')}</p>
+      </div>
+      <fetcher.Form method="post" noValidate className="flex flex-wrap items-center gap-3">
+        <input type="hidden" name="_csrf" value={csrfToken} />
+        <LoadingButton variant="primary" loading={isSubmitting} data-gc-analytics-customclick="ESDC-EDSC:CDCP Demographic Survey:Submitted - Exiting the application click">
+          {t('demographic-survey:submitted.close-btn')}
+        </LoadingButton>
+      </fetcher.Form>
+    </div>
+  );
+}
