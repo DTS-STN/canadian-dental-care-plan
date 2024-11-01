11ec290b4d92a02382ca0952ea1984f996a06105
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-11-01 11:57:50 -0230

    Add rate limiting to healthchecks

--- frontend/app/routes/api/health.ts
+++ frontend/app/routes/api/health.ts
@@ -4,24 +4,17 @@ import { json } from '@remix-run/node';
 import type { HealthCheck, HealthCheckOptions } from '@dts-stn/health-checks';
 import { HealthCheckConfig, execute, getHttpStatusCode } from '@dts-stn/health-checks';
 import { isEmpty } from 'moderndash';
+import moize from 'moize';
 
+import { getContainerConfigProvider, getContainerServiceProvider } from '~/.server/container';
 import { getBuildInfoService } from '~/services/build-info-service.server';
 
-function isAuthorized(request: Request): boolean {
-  return false; // TODO :: GjB :: add authentication check when AAD integration is ready
-}
+const { HEALTH_CACHE_TTL, SESSION_STORAGE_TYPE } = getContainerConfigProvider().getServerConfig();
 
-function toArray(str?: string): string[] | undefined {
-  const result = str?.split(',').filter(Boolean);
-  return isEmpty(result) ? undefined : result;
-}
+const redisCheckFn = async () => void (await getContainerServiceProvider().getRedisService()?.ping());
+const redisCheck = moize.promise(redisCheckFn, { maxAge: HEALTH_CACHE_TTL, transformArgs: () => [] });
 
-function toNumber(str?: string): number | undefined {
-  const num = parseInt(str ?? '');
-  return isNaN(num) ? undefined : num;
-}
-
-export async function loader({ context: { configProvider, serviceProvider, session }, request }: LoaderFunctionArgs) {
+export async function loader({ context: { serviceProvider }, request }: LoaderFunctionArgs) {
   const { include, exclude, timeout } = Object.fromEntries(new URL(request.url).searchParams);
   const { buildRevision: buildId, buildVersion: version } = getBuildInfoService().getBuildInfo();
 
@@ -37,21 +30,14 @@ export async function loader({ context: { configProvider, serviceProvider, sessi
   // TODO :: GjB ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   //
   //   - include details when isAuthorized()
-  //   - cache execute() response for a short period (ie: rate-limiting)
   //
   // TODO :: GjB ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   //
 
-  const redisHealthCheck: HealthCheck = {
-    name: 'redis',
-    check: async () => {
-      await serviceProvider.getRedisService()?.ping();
-    },
-  };
+  const redisHealthCheck: HealthCheck = { name: 'redis', check: redisCheck };
 
   // exclude the redis health check if the application is not using it
-  const useRedis = configProvider.getServerConfig().SESSION_STORAGE_TYPE === 'redis';
-  if (!useRedis) {
+  if (SESSION_STORAGE_TYPE !== 'redis') {
     healthCheckOptions.excludeComponents ??= [];
     healthCheckOptions.excludeComponents.push(redisHealthCheck.name);
   }
@@ -63,4 +49,29 @@ export async function loader({ context: { configProvider, serviceProvider, sessi
     headers: { 'Content-Type': HealthCheckConfig.responses.contentType },
     status: getHttpStatusCode(systemHealthSummary.status),
   });
+}
+
+/**
+ * Returns true if the incoming request is authorized to view detailed responses.
+ */
+function isAuthorized(request: Request): boolean {
+  return false; // TODO :: GjB :: add authentication check when AAD integration is ready
+}
+
+/**
+ * Transforms a comma-delimited string into an array of strings.
+ * Will return undefined if the resulting array is empty.
+ */
+function toArray(str?: string): string[] | undefined {
+  const result = str?.split(',').filter(Boolean);
+  return isEmpty(result) ? undefined : result;
+}
+
+/**
+ * Transforms a string into an ingeger.
+ * Will return undefined if the string can't be transformed.
+ */
+function toNumber(str?: string): number | undefined {
+  const num = parseInt(str ?? '');
+  return isNaN(num) ? undefined : num;
 }
