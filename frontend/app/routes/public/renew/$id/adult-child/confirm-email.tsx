1e7befa1975b5d23a8f2a8ca39afe6595f1739df
shawn320 <azuneko1110@gmail.com>
2024-11-05 16:12:10 -0500

    back button link fix

--- frontend/app/routes/public/renew/$id/adult-child/confirm-email.tsx
+++ frontend/app/routes/public/renew/$id/adult-child/confirm-email.tsx
@@ -250,7 +250,7 @@ export default function RenewAdultChildConfirmEmail() {
               </LoadingButton>
               <ButtonLink
                 id="back-button"
-                routeId="public/renew/$id/adult-child/marital-status"
+                routeId="public/renew/$id/adult-child/confirm-address"
                 params={params}
                 disabled={isSubmitting}
                 startIcon={faChevronLeft}
