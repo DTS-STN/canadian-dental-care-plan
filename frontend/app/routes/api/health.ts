68226bb499f9d9edb0cc86bf1c22a52eecc962b2
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2024-11-05 09:17:50 -0330

    Add details to redis health check (#2458)

--- frontend/app/routes/api/health.ts
+++ frontend/app/routes/api/health.ts
@@ -12,18 +12,19 @@ import { getAppContainerProvider } from '~/.server/app.container';
 import { SERVICE_IDENTIFIER } from '~/.server/constants';
 import { getBuildInfoService } from '~/services/build-info-service.server';
 
-const { HEALTH_CACHE_TTL } = getAppContainerProvider().get(SERVICE_IDENTIFIER.SERVER_CONFIG);
-const redisCheckFn = async () => void (await getAppContainerProvider().find(SERVICE_IDENTIFIER.REDIS_SERVICE)?.ping());
+const appContainerProvider = getAppContainerProvider();
+const { HEALTH_CACHE_TTL } = appContainerProvider.get(SERVICE_IDENTIFIER.SERVER_CONFIG);
 
 // memoize the result of redisCheckFn for a period of time
 // transformArgs is require to effectively ignore the abort signal sent from @dts-stn/health-checks when caching
 // for memoization to work, the call to moize must be done in module scope, so it only ever happens once
-const redisCheck = moize.promise(redisCheckFn, { maxAge: HEALTH_CACHE_TTL, transformArgs: () => [] });
+const redisCheck = moize.promise(async () => void (await appContainerProvider.find(SERVICE_IDENTIFIER.REDIS_SERVICE)?.ping()), { maxAge: HEALTH_CACHE_TTL, transformArgs: () => [] });
 
 export async function loader({ context: { appContainer }, request }: LoaderFunctionArgs) {
+  const serverConfig = appContainer.get(SERVICE_IDENTIFIER.SERVER_CONFIG);
+
   const { include, exclude, timeout } = Object.fromEntries(new URL(request.url).searchParams);
   const { buildRevision: buildId, buildVersion: version } = getBuildInfoService().getBuildInfo();
-  const { SESSION_STORAGE_TYPE } = appContainer.get(SERVICE_IDENTIFIER.SERVER_CONFIG);
 
   const healthCheckOptions: HealthCheckOptions = {
     excludeComponents: toArray(exclude),
@@ -33,10 +34,22 @@ export async function loader({ context: { appContainer }, request }: LoaderFunct
     timeoutMs: toNumber(timeout),
   };
 
-  const redisHealthCheck: HealthCheck = { name: 'redis', check: redisCheck };
+  const redisHealthCheck: HealthCheck = {
+    name: 'redis',
+    check: redisCheck,
+    metadata: {
+      REDIS_USERNAME: serverConfig.REDIS_USERNAME ?? '',
+      REDIS_STANDALONE_HOST: serverConfig.REDIS_STANDALONE_HOST,
+      REDIS_STANDALONE_PORT: serverConfig.REDIS_STANDALONE_PORT.toString(),
+      REDIS_SENTINEL_NAME: serverConfig.REDIS_SENTINEL_NAME ?? '',
+      REDIS_SENTINEL_HOST: serverConfig.REDIS_SENTINEL_HOST ?? '',
+      REDIS_SENTINEL_PORT: (serverConfig.REDIS_SENTINEL_PORT ?? '').toString(),
+      REDIS_MAX_RETRIES_PER_REQUEST: serverConfig.REDIS_MAX_RETRIES_PER_REQUEST.toString(),
+    },
+  };
 
   // exclude the redis health check if the application is not using it
-  if (SESSION_STORAGE_TYPE !== 'redis') {
+  if (serverConfig.SESSION_STORAGE_TYPE !== 'redis') {
     healthCheckOptions.excludeComponents ??= [];
     healthCheckOptions.excludeComponents.push(redisHealthCheck.name);
   }
@@ -56,15 +69,21 @@ export async function loader({ context: { appContainer }, request }: LoaderFunct
 async function isAuthorized(appContainer: AppContainerProvider, request: Request): Promise<boolean> {
   const log = appContainer.get(SERVICE_IDENTIFIER.LOG_FACTORY).createLogger('health/isAuthorized');
 
-  const authorization = request.headers.get('authorization');
-  const [scheme, accessToken] = authorization?.split(' ') ?? [];
+  const authorization = request.headers.get('authorization') ?? '';
+  const [scheme, accessToken] = authorization.split(' ');
 
   if (scheme.toLowerCase() !== 'bearer') {
     log.debug('Missing or invalid authorization header. Authorization failed.');
     return false;
   }
 
-  const { HEALTH_AUTH_JWKS_URI: jwksUri, HEALTH_AUTH_ROLE: authorizedRole, HEALTH_AUTH_TOKEN_AUDIENCE: audience, HEALTH_AUTH_TOKEN_ISSUER: issuer } = appContainer.get(SERVICE_IDENTIFIER.SERVER_CONFIG);
+  const {
+    // prettier-ignore
+    HEALTH_AUTH_JWKS_URI: jwksUri,
+    HEALTH_AUTH_ROLE: authorizedRole,
+    HEALTH_AUTH_TOKEN_AUDIENCE: audience,
+    HEALTH_AUTH_TOKEN_ISSUER: issuer,
+  } = appContainer.get(SERVICE_IDENTIFIER.SERVER_CONFIG);
 
   if (!jwksUri) {
     log.debug('JWK endpoint not configured. Authorization failed.');
@@ -72,7 +91,9 @@ async function isAuthorized(appContainer: AppContainerProvider, request: Request
   }
 
   try {
-    const { payload } = await jwtVerify<{ roles?: string[] }>(accessToken, createRemoteJWKSet(new URL(jwksUri)), { audience, issuer });
+    const getKeyFn = createRemoteJWKSet(new URL(jwksUri));
+    const jwtVerifyOptions = { audience: audience, issuer: issuer }; // require specific audience and issuer
+    const { payload } = await jwtVerify<{ roles?: string[] }>(accessToken, getKeyFn, jwtVerifyOptions);
     return payload.roles?.includes(authorizedRole) ?? false;
   } catch (error) {
     log.error('Error verifying JWT: %o. Authorization failed.', error);
