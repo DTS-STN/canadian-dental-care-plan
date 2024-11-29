8e75240f7214eac9306f15dc08e0019e84748968
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-11-29 18:07:37 -0330

    fix: suppress logging for /api/readyz

--- frontend/express-server/middleware.server.ts
+++ frontend/express-server/middleware.server.ts
@@ -1,11 +1,24 @@
 import type { RequestHandler } from 'express';
+import { minimatch } from 'minimatch';
 import morganMiddleware from 'morgan';
 
 import { getLogger } from '~/.server/utils/logging.utils';
 
+/**
+ * Checks if a given path should be ignored based on a list of ignore patterns.
+ *
+ * @param ignorePatterns - An array of glob patterns to match against the path.
+ * @param path - The path to check.
+ * @returns - True if the path should be ignored, false otherwise.
+ */
+function shouldIgnore(ignorePatterns: string[], path: string): boolean {
+  return ignorePatterns.some((entry) => minimatch(path, entry));
+}
+
 // @see: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html
 export function securityHeaders(): RequestHandler {
   const log = getLogger('express.server/securityHeadersRequestHandler');
+  const ignorePatterns: string[] = [];
 
   // prettier-ignore
   const permissionsPolicy = [
@@ -20,6 +33,11 @@ export function securityHeaders(): RequestHandler {
   ].join(', ');
 
   return (request, response, next) => {
+    if (shouldIgnore(ignorePatterns, request.path)) {
+      log.trace('Skipping adding security headers to response: [%s]', request.path);
+      return next();
+    }
+
     log.debug('Adding security headers to response');
 
     response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
@@ -37,6 +55,7 @@ export function securityHeaders(): RequestHandler {
 
 export function logging(isProduction: boolean): RequestHandler {
   const log = getLogger('express.server/loggingRequestHandler');
+  const ignorePatterns: string[] = ['/api/readyz'];
 
   const logFormat = isProduction ? 'tiny' : 'dev';
 
@@ -45,6 +64,7 @@ export function logging(isProduction: boolean): RequestHandler {
   });
 
   return (request, response, next) => {
+    if (shouldIgnore(ignorePatterns, request.path)) return next();
     return middleware(request, response, next);
   };
 }
