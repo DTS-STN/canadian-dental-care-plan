7187715fceaf1433a268da1eb2cf650596acd86c
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2024-12-01 14:33:00 -0330

    fix: broken terraform version

--- infrastructure/terraform/azure-ad-application/terraform.tf
+++ infrastructure/terraform/azure-ad-application/terraform.tf
@@ -4,7 +4,7 @@
 ###############################################################################
 
 terraform {
-  required_version = ">= 1.7.0, < 2.0.0"
+  required_version = ">= 1.7.0, < 1.10.0"
 
   required_providers {
     azuread = {
