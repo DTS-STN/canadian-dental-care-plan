1d4956c579da4f6d1f5f22e7fa9d8464ca26dd92
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2024-12-03 21:35:33 -0400

    refactor(frontend): remove mock service worker from app

--- frontend/app/mocks/node.ts
+++
@@ -1,6 +0,0 @@
-import { setupServer } from 'msw/node';
-
-import { mockEnabled } from '~/.server/utils/env.utils';
-import { getRaoidcMockHandlers } from '~/mocks/raoidc.server';
-
-export const server = setupServer(...(mockEnabled('raoidc') ? getRaoidcMockHandlers() : []));
