b0102d520e21dada4798638a064c9c1afc062e0f
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2024-12-19 12:50:04 -0500

    Updating public renewal and protected route paths

--- frontend/e2e/protected/letters/_index.spec.ts
+++ frontend/e2e/protected/letters/_index.spec.ts
@@ -3,12 +3,12 @@ import { sleep } from 'moderndash';
 
 test.describe('letters page', () => {
   test('should navigate to letters page', async ({ page }) => {
-    await page.goto('/en/letters');
-    await expect(page).toHaveURL('/en/letters');
+    await page.goto('/en/protected/letters');
+    await expect(page).toHaveURL('/en/protected/letters');
   });
 
   test('it should sort letters oldest to newest', async ({ page }) => {
-    await page.goto('/en/letters');
+    await page.goto('/en/protected/letters');
     await sleep(250); // wait 250ms for page to hydrate and onChange handlers to be bound
 
     const selectLocator = page.getByRole('combobox', { name: 'filter by' });
@@ -21,7 +21,7 @@ test.describe('letters page', () => {
   test('it should click on a pdf and successfully download', async ({ page }) => {
     // note: the default behaviour in the browser is to open the pdf in the tab
     // in chromium, however, this isn't the case. Instead, the pdf will fire a download event
-    await page.goto('/en/letters');
+    await page.goto('/en/protected/letters');
     const downloadPromise = page.waitForEvent('download');
     await page.getByRole('main').getByRole('listitem').first().getByRole('link').click();
     const download = await downloadPromise;
