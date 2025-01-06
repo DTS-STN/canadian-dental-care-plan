273628e790cc9ff5c7f0a23dbc39d5ce467a168e
Frank Basham <frankabasham@gmail.com>
2025-01-06 07:42:28 -0500

    update address validation routes with submitting conditional to render proper dialog component

--- frontend/app/routes/public/renew/$id/child/update-mailing-address.tsx
+++ frontend/app/routes/public/renew/$id/child/update-mailing-address.tsx
@@ -3,7 +3,7 @@ import type { SyntheticEvent } from 'react';
 
 import type { ActionFunctionArgs, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { data, redirect } from '@remix-run/node';
-import { useFetcher, useLoaderData, useParams } from '@remix-run/react';
+import { useLoaderData, useParams } from '@remix-run/react';
 
 import { faCheck, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { useTranslation } from 'react-i18next';
@@ -226,7 +226,7 @@ export default function RenewChildUpdateAddress() {
   const { defaultState, countryList, regionList, editMode } = useLoaderData<typeof loader>();
   const { CANADA_COUNTRY_ID, USA_COUNTRY_ID } = useClientEnv();
   const params = useParams();
-  const fetcher = useFetcher<typeof action>();
+  const fetcher = useEnhancedFetcher<typeof action>();
   const isSubmitting = fetcher.state !== 'idle';
   const [selectedMailingCountry, setSelectedMailingCountry] = useState(defaultState.mailingAddress?.country ?? CANADA_COUNTRY_ID);
   const [mailingCountryRegions, setMailingCountryRegions] = useState<typeof regionList>([]);
@@ -377,10 +377,14 @@ export default function RenewChildUpdateAddress() {
                     {t('renew-child:update-address.continue')}
                   </LoadingButton>
                 </DialogTrigger>
-                {addressDialogContent && addressDialogContent.status === 'address-suggestion' && (
-                  <AddressSuggestionDialogContent enteredAddress={addressDialogContent.enteredAddress} suggestedAddress={addressDialogContent.suggestedAddress} copyAddressToHome={copyAddressChecked} />
+                {!fetcher.isSubmitting && addressDialogContent && (
+                  <>
+                    {addressDialogContent.status === 'address-suggestion' && (
+                      <AddressSuggestionDialogContent enteredAddress={addressDialogContent.enteredAddress} suggestedAddress={addressDialogContent.suggestedAddress} copyAddressToHome={copyAddressChecked} />
+                    )}
+                    {addressDialogContent.status === 'address-invalid' && <AddressInvalidDialogContent invalidAddress={addressDialogContent.invalidAddress} copyAddressToHome={copyAddressChecked} />}
+                  </>
                 )}
-                {addressDialogContent && addressDialogContent.status === 'address-invalid' && <AddressInvalidDialogContent invalidAddress={addressDialogContent.invalidAddress} copyAddressToHome={copyAddressChecked} />}
               </Dialog>
 
               <ButtonLink
