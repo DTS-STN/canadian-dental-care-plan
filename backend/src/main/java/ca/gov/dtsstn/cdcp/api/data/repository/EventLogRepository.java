a924b987302736f9ddd94b0f1003a5a7049c0897
Greg Baker <48123208+gregory-j-baker@users.noreply.github.com>
2025-01-09 07:30:08 -0330

    chore(workspace): remove unused backend subproject (#2915)

--- backend/src/main/java/ca/gov/dtsstn/cdcp/api/data/repository/EventLogRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.gov.dtsstn.cdcp.api.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import ca.gov.dtsstn.cdcp.api.data.entity.EventLogEntity;
-
-@Repository
-public interface EventLogRepository extends JpaRepository<EventLogEntity, String> {}
