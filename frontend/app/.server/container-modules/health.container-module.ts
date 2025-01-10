ae1ec50527943d4eb00cd5862571c03c64bf5e2a
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2025-01-10 10:46:32 -0500

    Adding hCaptcha health check

--- frontend/app/.server/container-modules/health.container-module.ts
+++ frontend/app/.server/container-modules/health.container-module.ts
@@ -3,7 +3,14 @@ import { ContainerModule } from 'inversify';
 
 import type { ServerConfig } from '~/.server/configs';
 import { TYPES } from '~/.server/constants';
-import { AddressValidationHealthCheck, ApplicantHealthCheck, ApplicationStatusHealthCheck, LetterHealthCheck, RedisHealthCheck } from '~/.server/health';
+import { AddressValidationHealthCheck, ApplicantHealthCheck, ApplicationStatusHealthCheck, HCaptchaHealthCheck, LetterHealthCheck, RedisHealthCheck } from '~/.server/health';
+
+function featureEnabled(feature: ServerConfig['ENABLED_FEATURES'][number]) {
+  return ({ parentContext }: interfaces.Request) => {
+    const serverConfig = parentContext.container.get(TYPES.configs.ServerConfig);
+    return serverConfig.ENABLED_FEATURES.includes(feature);
+  };
+}
 
 function sessionTypeIs(sessionType: ServerConfig['SESSION_STORAGE_TYPE']) {
   return ({ parentContext }: interfaces.Request) => {
@@ -19,6 +26,7 @@ export const healthContainerModule = new ContainerModule((bind) => {
   bind(TYPES.health.HealthCheck).to(AddressValidationHealthCheck);
   bind(TYPES.health.HealthCheck).to(ApplicantHealthCheck);
   bind(TYPES.health.HealthCheck).to(ApplicationStatusHealthCheck);
+  bind(TYPES.health.HealthCheck).to(HCaptchaHealthCheck).when(featureEnabled('hcaptcha'));
   bind(TYPES.health.HealthCheck).to(LetterHealthCheck);
   bind(TYPES.health.HealthCheck).to(RedisHealthCheck).when(sessionTypeIs('redis'));
 });
--- frontend/app/.server/health/index.ts
+++ frontend/app/.server/health/index.ts
@@ -1,5 +1,6 @@
 export * from './address-validation.health';
 export * from './applicant.health';
 export * from './application-status.health';
+export * from './hcaptcha.health';
 export * from './letter.health';
 export * from './redis.health';
