73c42f6acd67d28737598f3a774fd968cbeb1242
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2025-01-16 17:06:41 -0500

    Fixed phone number validation for all flows

--- frontend/app/routes/public/renew/$id/child/confirm-phone.tsx
+++ frontend/app/routes/public/renew/$id/child/confirm-phone.tsx
@@ -86,13 +86,15 @@ export async function action({ context: { appContainer, session }, params, reque
         .string()
         .trim()
         .max(100)
-        .refine((val) => !val || isValidPhoneNumber(val, 'CA'), t('renew-child:confirm-phone.error-message.phone-number-valid'))
+        .refine((val) => !val || (val.replace(/[^\d]/g, '').length <= 11 ? isValidPhoneNumber(val, 'CA') : true), t('renew-child:confirm-phone.error-message.phone-number-valid'))
+        .refine((val) => !val || (val.replace(/[^\d]/g, '').length > 11 ? isValidPhoneNumber(val) : true), t('renew-child:confirm-phone.error-message.phone-number-valid-international'))
         .optional(),
       phoneNumberAlt: z
         .string()
         .trim()
         .max(100)
-        .refine((val) => !val || isValidPhoneNumber(val, 'CA'), t('renew-child:confirm-phone.error-message.phone-number-alt-valid'))
+        .refine((val) => !val || (val.replace(/[^\d]/g, '').length <= 11 ? isValidPhoneNumber(val, 'CA') : true), t('renew-child:confirm-phone.error-message.phone-number-alt-valid'))
+        .refine((val) => !val || (val.replace(/[^\d]/g, '').length > 11 ? isValidPhoneNumber(val) : true), t('renew-child:confirm-phone.error-message.phone-number-alt-valid-international'))
         .optional(),
     })
     .superRefine((val, ctx) => {
