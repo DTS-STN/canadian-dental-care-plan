bfbb8d44b7443e6e4c42ab661d5833fc17f5f1c7
Benoit Cloutier <benoit.bc.cloutier@gmail.com>
2025-01-22 17:34:44 -0500

    Reordered error messages to match form elements

--- frontend/app/routes/public/renew/$id/adult-child/marital-status.tsx
+++ frontend/app/routes/public/renew/$id/adult-child/marital-status.tsx
@@ -139,8 +139,8 @@ export default function RenewAdultChildMaritalStatus() {
   const errors = fetcher.data?.errors;
   const errorSummary = useErrorSummary(errors, {
     maritalStatus: 'input-radio-marital-status-option-0',
-    yearOfBirth: 'year-of-birth',
     socialInsuranceNumber: 'social-insurance-number',
+    yearOfBirth: 'year-of-birth',
     confirm: 'input-checkbox-confirm',
   });
 
