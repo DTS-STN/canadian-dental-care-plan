d36238646d82639b1725c646c033cb31be75b4e9
Frank <48450599+Fbasham@users.noreply.github.com>
2025-02-11 11:11:11 -0500

    fix navigation issue for ita clients in /protected/confirm-home-address (#3192)

--- frontend/app/routes/protected/renew/$id/confirm-home-address.tsx
+++ frontend/app/routes/protected/renew/$id/confirm-home-address.tsx
@@ -148,10 +148,9 @@ export async function action({ context: { appContainer, session }, params, reque
   const canProceedToReview = isNotCanada || isUseInvalidAddressAction || isUseSelectedAddressAction;
   if (canProceedToReview) {
     saveProtectedRenewState({ params, request, session, state: { homeAddress } });
-    if (state.clientApplication.isInvitationToApplyClient) {
+    if (state.editMode === false && state.clientApplication.isInvitationToApplyClient) {
       return redirect(getPathById('protected/renew/$id/ita/confirm-email', params));
     }
-
     return redirect(getPathById('protected/renew/$id/review-adult-information', params));
   }
 
@@ -210,6 +209,10 @@ export async function action({ context: { appContainer, session }, params, reque
 
   const idToken: IdToken = session.get('idToken');
   appContainer.get(TYPES.domain.services.AuditService).createAudit('update-data.renew.confirm-home-address', { userId: idToken.sub });
+
+  if (state.editMode === false && state.clientApplication.isInvitationToApplyClient) {
+    return redirect(getPathById('protected/renew/$id/ita/confirm-email', params));
+  }
 
   return redirect(getPathById('protected/renew/$id/review-adult-information', params));
 }
