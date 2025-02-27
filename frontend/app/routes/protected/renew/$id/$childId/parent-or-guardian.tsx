7075eb517afed5e47b598fb65cec510ef9e7a7eb
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2025-02-27 14:34:39 -0500

    Adding missing dcTermsTitle parameter when merging meta tags

--- frontend/app/routes/protected/renew/$id/$childId/parent-or-guardian.tsx
+++ frontend/app/routes/protected/renew/$id/$childId/parent-or-guardian.tsx
@@ -35,7 +35,7 @@ export const handle = {
 } as const satisfies RouteHandleData;
 
 export const meta: Route.MetaFunction = mergeMeta(({ data }) => {
-  return getTitleMetaTags(data.meta.title);
+  return getTitleMetaTags(data.meta.title, data.meta.dcTermsTitle);
 });
 
 export async function loader({ context: { appContainer, session }, params, request }: Route.LoaderArgs) {
