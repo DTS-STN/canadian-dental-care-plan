8e891260ae67786b0b6f7001e282fd6624e13d67
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2025-03-07 08:30:15 -0500

    Adding unique SIN validation for renewal marital status pages

--- frontend/app/routes/public/renew/$id/ita/marital-status.tsx
+++ frontend/app/routes/public/renew/$id/ita/marital-status.tsx
@@ -89,8 +89,18 @@ export async function action({ context: { appContainer, session }, params, reque
       .string()
       .trim()
       .min(1, t('renew-ita:marital-status.error-message.sin-required'))
-      .refine(isValidSin, t('renew-ita:marital-status.error-message.sin-valid'))
-      .refine((sin) => isValidSin(sin) && formatSin(sin, '') !== state.partnerInformation?.socialInsuranceNumber, t('renew-ita:marital-status.error-message.sin-unique')),
+      .superRefine((sin, ctx) => {
+        if (!isValidSin(sin)) {
+          ctx.addIssue({ code: z.ZodIssueCode.custom, message: t('renew-ita:marital-status.error-message.sin-valid') });
+        } else if (
+          [state.clientApplication?.applicantInformation.socialInsuranceNumber, ...(state.clientApplication?.children ? state.clientApplication.children.map((child) => child.information.socialInsuranceNumber) : [])]
+            .filter((sin) => sin !== undefined)
+            .map((sin) => formatSin(sin))
+            .includes(formatSin(sin))
+        ) {
+          ctx.addIssue({ code: z.ZodIssueCode.custom, message: t('renew-ita:marital-status.error-message.sin-unique') });
+        }
+      }),
   }) satisfies z.ZodType<PartnerInformationState>;
 
   const maritalStatusData = {
