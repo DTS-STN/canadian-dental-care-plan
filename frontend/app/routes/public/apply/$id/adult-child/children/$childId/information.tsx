48c1f3fdd2693e7d8aa803e96e53b6cc85c26cf9
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2025-03-19 10:56:20 -0400

    Added coalescing

--- frontend/app/routes/public/apply/$id/adult-child/children/$childId/information.tsx
+++ frontend/app/routes/public/apply/$id/adult-child/children/$childId/information.tsx
@@ -229,7 +229,7 @@ export default function ApplyFlowChildInformation({ loaderData, params }: Route.
 
   const fetcher = useFetcher<typeof action>();
   const isSubmitting = fetcher.state !== 'idle';
-  const [hasSocialInsuranceNumberValue, setHasSocialInsuranceNumberValue] = useState(true);
+  const [hasSocialInsuranceNumberValue, setHasSocialInsuranceNumberValue] = useState(defaultState?.hasSocialInsuranceNumber ?? true);
 
   const errors = fetcher.data?.errors;
   const errorSummary = useErrorSummary(errors, {
@@ -252,7 +252,7 @@ export default function ApplyFlowChildInformation({ loaderData, params }: Route.
     {
       children: <Trans ns={handle.i18nNamespaces} i18nKey="apply-adult-child:children.information.sin-yes" components={{ bold: <strong /> }} />,
       value: YES_NO_OPTION.yes,
-      defaultChecked: true,
+      defaultChecked: defaultState?.hasSocialInsuranceNumber ?? true,
       append: hasSocialInsuranceNumberValue === true && (
         <div className="mb-6 grid items-end gap-6 md:grid-cols-2">
           <InputPatternField
