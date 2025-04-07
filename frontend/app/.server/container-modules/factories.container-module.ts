f46594be0e60eb1fcdd59f6b57b5e9e4fc064228
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-04-07 16:10:09 -0300

    refactor(frontend): Remove ConfigFactory

--- frontend/app/.server/container-modules/factories.container-module.ts
+++ frontend/app/.server/container-modules/factories.container-module.ts
@@ -1,6 +1,5 @@
 import { ContainerModule } from 'inversify';
 
-import { DefaultConfigFactory } from '~/.server/configs/config.factory';
 import { TYPES } from '~/.server/constants';
 import { DefaultAddressValidatorFactory } from '~/.server/routes/validators';
 import { DefaultHomeAddressValidatorFactory } from '~/.server/routes/validators/home-address.validator.factory';
@@ -11,7 +10,6 @@ import { DefaultMailingAddressValidatorFactory } from '~/.server/routes/validato
  */
 export function createFactoriesContainerModule(): ContainerModule {
   return new ContainerModule((options) => {
-    options.bind(TYPES.configs.ConfigFactory).to(DefaultConfigFactory);
     options.bind(TYPES.routes.validators.AddressValidatorFactory).to(DefaultAddressValidatorFactory);
     options.bind(TYPES.routes.validators.MailingAddressValidatorFactory).to(DefaultMailingAddressValidatorFactory);
     options.bind(TYPES.routes.validators.HomeAddressValidatorFactory).to(DefaultHomeAddressValidatorFactory);
