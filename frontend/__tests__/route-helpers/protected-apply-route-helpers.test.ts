37d6f5a7bdfa01208a301c42048a74b1a63661c1
Nicholas Ly <55502055+nicholas-ly@users.noreply.github.com>
2025-04-11 11:06:16 -0400

    fix(frontend): correcting removal of path segments for Adobe Analytics URL transformation (#3541)

--- frontend/__tests__/route-helpers/protected-apply-route-helpers.test.ts
+++ frontend/__tests__/route-helpers/protected-apply-route-helpers.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { transformAdobeAnalyticsUrl, transformChildrenRouteAdobeAnalyticsUrl } from '~/route-helpers/protected-apply-route-helpers';
+import { transformAdobeAnalyticsUrl, transformAdultChildChildrenRouteAdobeAnalyticsUrl, transformChildChildrenRouteAdobeAnalyticsUrl } from '~/route-helpers/protected-apply-route-helpers';
 
 describe('protected-apply-route-helpers', () => {
   describe('transformAdobeAnalyticsUrl', () => {
@@ -20,7 +20,7 @@ describe('protected-apply-route-helpers', () => {
     });
   });
 
-  describe('transformChildrenRouteAdobeAnalyticsUrl', () => {
+  describe('transformAdultChildChildrenRouteAdobeAnalyticsUrl', () => {
     it.each([
       ['https://example.com/en/'],
       ['https://example.com/fr/'],
@@ -28,18 +28,42 @@ describe('protected-apply-route-helpers', () => {
       ['https://example.com/fr/demander'],
       ['https://example.com/en/protected/apply/00000000-0000-0000-0000-000000000000/somepage/'],
       ['https://example.com/en/protege/demander/00000000-0000-0000-0000-000000000000/somepage/'],
-    ])('should return as is for %s when not matching the protected apply route regex', (url) => {
-      const actual = transformChildrenRouteAdobeAnalyticsUrl(url);
+    ])('should return as is for %s when not matching the protected adult-child children route regex', (url) => {
+      const actual = transformAdultChildChildrenRouteAdobeAnalyticsUrl(url);
       expect(actual).toEqual(new URL(url));
     });
 
     it.each([
-      ['https://example.com/en/protected/apply/00000000-0000-0000-0000-000000000000/children/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/en/protected/apply/children/somepage/'],
-      ['https://example.com/fr/protected/apply/00000000-0000-0000-0000-000000000000/children/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/fr/protected/apply/children/somepage/'],
-      ['https://example.com/en/protege/demander/00000000-0000-0000-0000-000000000000/enfants/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/en/protege/demander/enfants/somepage/'],
-      ['https://example.com/fr/protege/demander/00000000-0000-0000-0000-000000000000/enfants/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/fr/protege/demander/enfants/somepage/'],
-    ])('should remove the session id path segment for %s when matching the protected apply children route regex', (url, expectedUrl) => {
-      const actual = transformChildrenRouteAdobeAnalyticsUrl(url);
+      ['https://example.com/en/protected/apply/00000000-0000-0000-0000-000000000000/adult-child/children/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/en/protected/apply/adult-child/children/somepage/'],
+      ['https://example.com/fr/protected/apply/00000000-0000-0000-0000-000000000000/adult-child/children/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/fr/protected/apply/adult-child/children/somepage/'],
+      ['https://example.com/en/protege/demander/00000000-0000-0000-0000-000000000000/adulte-enfant/enfants/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/en/protege/demander/adulte-enfant/enfants/somepage/'],
+      ['https://example.com/fr/protege/demander/00000000-0000-0000-0000-000000000000/adulte-enfant/enfants/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/fr/protege/demander/adulte-enfant/enfants/somepage/'],
+    ])('should remove the session id path segment for %s when matching the protected adult-child children route regex', (url, expectedUrl) => {
+      const actual = transformAdultChildChildrenRouteAdobeAnalyticsUrl(url);
+      expect(actual).toEqual(new URL(expectedUrl));
+    });
+  });
+
+  describe('transformChildChildrenRouteAdobeAnalyticsUrl', () => {
+    it.each([
+      ['https://example.com/en/'],
+      ['https://example.com/fr/'],
+      ['https://example.com/en/apply'],
+      ['https://example.com/fr/demander'],
+      ['https://example.com/en/protected/apply/00000000-0000-0000-0000-000000000000/somepage/'],
+      ['https://example.com/en/protege/demander/00000000-0000-0000-0000-000000000000/somepage/'],
+    ])('should return as is for %s when not matching the protected apply child children route regex', (url) => {
+      const actual = transformChildChildrenRouteAdobeAnalyticsUrl(url);
+      expect(actual).toEqual(new URL(url));
+    });
+
+    it.each([
+      ['https://example.com/en/protected/apply/00000000-0000-0000-0000-000000000000/child/children/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/en/protected/apply/child/children/somepage/'],
+      ['https://example.com/fr/protected/apply/00000000-0000-0000-0000-000000000000/child/children/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/fr/protected/apply/child/children/somepage/'],
+      ['https://example.com/en/protege/demander/00000000-0000-0000-0000-000000000000/enfant/enfants/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/en/protege/demander/enfant/enfants/somepage/'],
+      ['https://example.com/fr/protege/demander/00000000-0000-0000-0000-000000000000/enfant/enfants/00000000-0000-0000-0000-000000000000/somepage/', 'https://example.com/fr/protege/demander/enfant/enfants/somepage/'],
+    ])('should remove the session id path segment for %s when matching the protected apply child children route regex', (url, expectedUrl) => {
+      const actual = transformChildChildrenRouteAdobeAnalyticsUrl(url);
       expect(actual).toEqual(new URL(expectedUrl));
     });
   });
--- frontend/app/route-helpers/protected-apply-route-helpers.ts
+++ frontend/app/route-helpers/protected-apply-route-helpers.ts
@@ -4,8 +4,10 @@ import { removePathSegment } from '~/utils/url-utils';
  * Adobe Analytics needs us to clean up URLs before sending event data. This ensures their reports focus
  * on the core content, not things like tracking codes, because they don't want those to mess up their
  * website visitor categories.
- * @param url
- * @returns
+ *
+ * @param url The URL string or URL object to transform.
+ * @returns A new URL object with dynamic path segments removed if the URL matches the expected pattern;
+ *          otherwise, returns the original URL object.
  */
 export function transformAdobeAnalyticsUrl(url: string | URL) {
   const urlObj = new URL(url);
@@ -15,19 +17,37 @@ export function transformAdobeAnalyticsUrl(url: string | URL) {
 }
 
 /**
- * Adobe Analytics needs us to clean up URLs before sending event data. This ensures their reports focus
- * on the core content, not things like tracking codes, because they don't want those to mess up their
- * website visitor categories.
- * @param url
- * @returns
+ * Transforms URLs for the "adult-child/children" routes by removing dynamic path segments used to track user session or form state.
+ *
+ * @param url - The URL string or URL object to transform.
+ * @returns A new URL object with dynamic path segments removed if the URL matches the expected pattern;
+ *          otherwise, returns the original URL object.
  */
-export function transformChildrenRouteAdobeAnalyticsUrl(url: string | URL) {
+export function transformAdultChildChildrenRouteAdobeAnalyticsUrl(url: string | URL) {
   const urlObj = new URL(url);
-  const protectedApplyRouteRegex = /^\/(en|fr)\/(protected|protege)\/(apply|demander)\/.*\/(children|enfants)\//i;
+  const protectedApplyRouteRegex = /^\/(en|fr)\/(protected|protege)\/(apply|demander)\/.*\/(adult-child|adulte-enfant)\/(children|enfants)\//i;
   if (!protectedApplyRouteRegex.test(urlObj.pathname)) return urlObj;
-  // remove protected apply state id
+  // remove apply state id
   let transformedUrl = removePathSegment(urlObj, 3);
-  // remove protected apply child state id
-  transformedUrl = removePathSegment(transformedUrl, 4);
+  // remove apply children state id
+  transformedUrl = removePathSegment(transformedUrl, 5);
+  return new URL(transformedUrl);
+}
+
+/**
+ * Transforms URLs for the "child/children" routes by removing dynamic path segments to sanitize analytics data.
+ *
+ * @param url The URL string or URL object to transform.
+ * @returns A new URL object with dynamic path segments removed if the URL matches the expected pattern;
+ *          otherwise, returns the original URL object.
+ */
+export function transformChildChildrenRouteAdobeAnalyticsUrl(url: string | URL) {
+  const urlObj = new URL(url);
+  const protectedApplyRouteRegex = /^\/(en|fr)\/(protected|protege)\/(apply|demander)\/.*\/(child|enfant)\/(children|enfants)\//i;
+  if (!protectedApplyRouteRegex.test(urlObj.pathname)) return urlObj;
+  // remove apply state id
+  let transformedUrl = removePathSegment(urlObj, 3);
+  // remove apply children state id
+  transformedUrl = removePathSegment(transformedUrl, 5);
   return new URL(transformedUrl);
 }
