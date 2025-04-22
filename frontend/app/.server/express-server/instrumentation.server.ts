0d5aaf5671352879db4707fb3c3b211f6de3c0c2
Nicholas Ly <nicholas.ly@hrsdc-rhdcc.gc.ca>
2025-04-22 11:13:00 -0400

    fix(frontend): Fixing metric prefix transformation

--- frontend/app/.server/express-server/instrumentation.server.ts
+++ frontend/app/.server/express-server/instrumentation.server.ts
@@ -60,7 +60,7 @@ export function routeRequestCounter(): RequestHandler {
 
         if (routeId) {
           // Construct metric identifier (e.g., POST '/user/$id/profile' → 'user._id.profile.posts')
-          const metricPrefix = `${routeId.replaceAll('/', '.').replace(/.\$([^.]+)/g, '_$1')}.${req.method.toLowerCase()}s`;
+          const metricPrefix = `${routeId.replaceAll('/', '.').replace(/\$([^.]+)/g, '_$1')}.${req.method.toLowerCase()}s`;
           instrumentationService.countHttpStatus(metricPrefix, res.statusCode);
         }
       } catch (error) {
