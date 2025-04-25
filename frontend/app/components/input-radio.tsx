f817b3cc2da3b014f0cf2c5b031745217b0a3e53
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-04-25 14:39:39 -0300

    style(frontend): Increase checkbox and radio size

--- frontend/app/components/input-radio.tsx
+++ frontend/app/components/input-radio.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 import { cn } from '~/utils/tw-utils';
 
-const inputBaseClassName = 'mt-1 h-4 w-4 border-gray-500 bg-gray-50 text-blue-600 focus:outline-hidden focus:ring-2 focus:ring-blue-500';
+const inputBaseClassName = 'mt-0.5 size-5 border-gray-500 bg-gray-50 text-blue-600 focus:ring-2 focus:ring-blue-500 focus:outline-hidden';
 const inputDisabledClassName = 'pointer-events-none cursor-not-allowed opacity-70';
 const inputErrorClassName = 'border-red-500 text-red-700 focus:border-red-500 focus:ring-red-500';
 const inputReadOnlyClassName = 'pointer-events-none cursor-not-allowed opacity-70';
