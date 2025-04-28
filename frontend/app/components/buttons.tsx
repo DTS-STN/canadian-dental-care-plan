b964f6cc19b5e1bb581010d2492a65268b64d765
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-04-28 09:59:09 -0300

    style(frontend): Update button style variable names for better Tailwind CSS IntelliSense support

--- frontend/app/components/buttons.tsx
+++ frontend/app/components/buttons.tsx
@@ -8,7 +8,7 @@ import { cn } from '~/utils/tw-utils';
 type ButtonEndIconProps = ComponentProps<typeof ButtonEndIcon>;
 type ButtonStartIconProps = ComponentProps<typeof ButtonStartIcon>;
 
-const sizes = {
+const sizeStyles = {
   xs: 'px-3 py-2 text-xs',
   sm: 'px-3 py-2 text-sm',
   base: 'px-5 py-2.5 text-sm',
@@ -16,26 +16,26 @@ const sizes = {
   xl: 'px-6 py-3.5 text-base',
 };
 
-const variants = {
+const variantStyles = {
   alternative: 'border-gray-200 bg-white text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:bg-gray-100 focus:text-blue-700',
   default: 'border-gray-300 bg-gray-200 text-slate-700 hover:bg-neutral-300 focus:bg-neutral-300',
   dark: 'border-gray-800 bg-gray-800 text-white hover:bg-gray-900 focus:bg-gray-900',
   green: 'border-green-700 bg-green-700 text-white hover:bg-green-800 focus:bg-green-800',
   primary: 'border-slate-700 bg-slate-700 text-white hover:bg-sky-800 focus:bg-sky-800',
   red: 'border-red-700 bg-red-700 text-white hover:bg-red-800 focus:bg-red-800',
-  link: 'text-slate-700 underline hover:text-blue-700 focus:text-blue-700 border-none px-0',
+  link: 'border-none px-0 text-slate-700 underline hover:text-blue-700 focus:text-blue-700',
 };
 
-const baseClassName = 'inline-flex items-center justify-center rounded-sm border align-middle font-lato outline-offset-4';
+const baseStyles = 'inline-flex items-center justify-center rounded-sm border align-middle font-lato outline-offset-4';
 
 export interface ButtonProps extends ComponentProps<'button'> {
   endIcon?: ButtonEndIconProps['icon'];
   endIconProps?: OmitStrict<ButtonEndIconProps, 'icon'>;
   pill?: boolean;
-  size?: keyof typeof sizes;
+  size?: keyof typeof sizeStyles;
   startIcon?: ButtonStartIconProps['icon'];
   startIconProps?: OmitStrict<ButtonStartIconProps, 'icon'>;
-  variant?: keyof typeof variants;
+  variant?: keyof typeof variantStyles;
 }
 
 /**
@@ -46,9 +46,9 @@ export function Button({ children, className, endIcon, endIconProps, pill, size 
   return (
     <button
       className={cn(
-        baseClassName, //
-        sizes[size],
-        variants[variant],
+        baseStyles, //
+        sizeStyles[size],
+        variantStyles[variant],
         'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-70',
         pill && 'rounded-full',
         className,
@@ -66,8 +66,8 @@ export interface ButtonLinkProps extends ComponentProps<typeof AppLink> {
   disabled?: boolean;
   endIcon?: ButtonEndIconProps['icon'];
   endIconProps?: OmitStrict<ButtonEndIconProps, 'icon'>;
-  size?: keyof typeof sizes;
-  variant?: keyof typeof variants;
+  size?: keyof typeof sizeStyles;
+  variant?: keyof typeof variantStyles;
   startIcon?: ButtonStartIconProps['icon'];
   startIconProps?: OmitStrict<ButtonStartIconProps, 'icon'>;
   pill?: boolean;
@@ -81,7 +81,7 @@ export interface ButtonLinkProps extends ComponentProps<typeof AppLink> {
  * @see https://www.scottohara.me/blog/2021/05/28/disabled-links.html
  */
 export function ButtonLink({ children, className, disabled, endIcon, endIconProps, pill, size = 'base', routeId, startIcon, startIconProps, to, variant = 'default', ...props }: ButtonLinkProps) {
-  const buttonLinkClassName = cn(baseClassName, sizes[size], variants[variant], pill && 'rounded-full', className);
+  const buttonLinkStyles = cn(baseStyles, sizeStyles[size], variantStyles[variant], pill && 'rounded-full', className);
 
   const actualChildren = (
     <>
@@ -93,14 +93,14 @@ export function ButtonLink({ children, className, disabled, endIcon, endIconProp
 
   if (disabled) {
     return (
-      <a className={cn(buttonLinkClassName, 'pointer-events-none cursor-not-allowed opacity-70')} role="link" aria-disabled="true" {...props}>
+      <a className={cn(buttonLinkStyles, 'pointer-events-none cursor-not-allowed opacity-70')} role="link" aria-disabled="true" {...props}>
         {actualChildren}
       </a>
     );
   }
 
   return (
-    <AppLink className={buttonLinkClassName} routeId={routeId} to={to} {...props}>
+    <AppLink className={buttonLinkStyles} routeId={routeId} to={to} {...props}>
       {actualChildren}
     </AppLink>
   );
