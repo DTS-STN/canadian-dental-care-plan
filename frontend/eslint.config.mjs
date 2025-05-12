9e744d444ced033db8e79f4ff556d6cf77e76784
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-05-12 14:31:56 -0300

    fix(frontend): Eslint "unicorn/no-nested-ternary" rule

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -128,7 +128,6 @@ export default defineConfig(
       'unicorn/consistent-function-scoping': ['error', { checkArrowFunctions: false }],
       'unicorn/no-array-callback-reference': 'off',
       'unicorn/no-array-reduce': 'off',
-      'unicorn/no-nested-ternary': 'off',
       'unicorn/no-null': 'off',
       'unicorn/no-useless-undefined': ['error', { checkArguments: false }],
       'unicorn/prefer-global-this': 'off',
