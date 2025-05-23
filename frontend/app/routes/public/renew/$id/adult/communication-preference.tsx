a50ade96fa7ee166579adc9e6dc0d6039ea82006
shawn320 <azuneko1110@gmail.com>
2025-05-22 22:29:22 -0400

    update

--- frontend/app/routes/public/renew/$id/adult/communication-preference.tsx
+++ frontend/app/routes/public/renew/$id/adult/communication-preference.tsx
@@ -116,6 +116,7 @@ export default function ApplyFlowCommunicationPreferencePage({ loaderData, param
   return (
     <>
       <div className="my-6 sm:my-8">
+        {/* TODO: Update the progress value to reflect the actual progress of the application */}
         <Progress value={70} size="lg" label={t('renew:progress.label')} />
       </div>
       <div className="max-w-prose">
