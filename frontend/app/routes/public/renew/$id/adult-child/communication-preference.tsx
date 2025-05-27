5176bdee976ede96496dbfd0349a8531fc6529b7
shawn320 <azuneko1110@gmail.com>
2025-05-27 11:26:14 -0400

    update

--- frontend/app/routes/public/renew/$id/adult-child/communication-preference.tsx
+++ frontend/app/routes/public/renew/$id/adult-child/communication-preference.tsx
@@ -36,6 +36,9 @@ export const handle = {
 } as const satisfies RouteHandleData;
 
 export const meta: Route.MetaFunction = mergeMeta(({ data }) => {
+  if (!data) {
+    return [];
+  }
   return getTitleMetaTags(data.meta.title);
 });
 
