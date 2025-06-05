779877bd9a9f216604cc0946a2d3df61f5cf5154
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-06-05 09:42:33 -0300

    test(frontend): BrowserCompatibilityBanner unit tests

--- frontend/app/components/browser-compatibility-banner.tsx
+++ frontend/app/components/browser-compatibility-banner.tsx
@@ -4,8 +4,6 @@ import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
 
-import { Button } from '~/components/buttons';
-
 interface BrowserCompatibilityBannerProps {
   onDismiss?: () => void;
 }
@@ -14,19 +12,19 @@ export function BrowserCompatibilityBanner({ onDismiss }: BrowserCompatibilityBa
   const { t } = useTranslation(['gcweb']);
 
   return (
-    <div id="browser-compatibility-banner" className="border-b border-red-700 bg-red-50" role="alert">
+    <div id="browser-compatibility-banner" className="bg-red-50 shadow-md" role="alert">
       <div className="container">
         <div className="flex items-stretch">
           <div className="hidden items-center bg-red-200 p-4 sm:flex">
             <FontAwesomeIcon icon={faCircleExclamation} className="size-6 shrink-0 text-red-700" />
           </div>
-          <div className="grow p-4">
-            <h2 className="font-lato mb-1 font-semibold">{t('gcweb:browser-compatibility-banner.title')}</h2>
+          <div className="grow space-y-2 py-4 pl-4">
+            <h2 className="font-lato font-bold">{t('gcweb:browser-compatibility-banner.title')}</h2>
             <p>{t('gcweb:browser-compatibility-banner.content')}</p>
             <div className="text-right">
-              <Button variant="link" type="button" onClick={onDismiss}>
+              <button type="button" onClick={onDismiss} className="font-lato rounded-sm border border-red-300 px-3 py-2 text-sm text-red-700 outline-offset-4 hover:border-red-400 hover:bg-red-100 focus:border-red-400 focus:bg-red-100">
                 {t('gcweb:browser-compatibility-banner.dismiss')}
-              </Button>
+              </button>
             </div>
           </div>
         </div>
