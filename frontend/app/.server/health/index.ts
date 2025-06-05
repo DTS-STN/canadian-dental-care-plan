0e6dbac8d57318371deef61008d758880898dc3f
Frank Basham <frankabasham@gmail.com>
2025-06-05 09:36:42 -0400

    feat(frontend) add health check for country repository

--- frontend/app/.server/health/index.ts
+++ frontend/app/.server/health/index.ts
@@ -1,6 +1,7 @@
 export * from './address-validation.health';
 export * from './applicant.health';
 export * from './application-status.health';
+export * from './country.health';
 export * from './hcaptcha.health';
 export * from './letter.health';
 export * from './notification.health';
