c892ef8091ccd57046b5657e89d9e61e69a60b9b
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-08-08 09:53:15 -0300

    test(frontend): DefaultLanguageService unit tests

---
+++ frontend/__tests__/.server/domain/services/language.service.test.ts
@@ -0,0 +1,104 @@
+import { describe, expect, it, vi } from 'vitest';
+import { mock } from 'vitest-mock-extended';
+
+import type { LanguageDto, LanguageLocalizedDto } from '~/.server/domain/dtos';
+import { LanguageNotFoundException } from '~/.server/domain/exceptions';
+import type { LanguageDtoMapper } from '~/.server/domain/mappers';
+import { DefaultLanguageService } from '~/.server/domain/services';
+import type { DefaultLanguageService_ServiceConfig } from '~/.server/domain/services/language.service';
+
+vi.mock('moize');
+
+describe('DefaultLanguageService', () => {
+  const mockServerConfig: DefaultLanguageService_ServiceConfig = {
+    ENGLISH_LANGUAGE_CODE: 1,
+    FRENCH_LANGUAGE_CODE: 2,
+  };
+
+  describe('listLanguages', () => {
+    it('fetches all languages', () => {
+      const mockDtos: LanguageDto[] = [
+        { id: mockServerConfig.ENGLISH_LANGUAGE_CODE.toString(), code: 'en', nameEn: 'English', nameFr: 'Anglais' },
+        { id: mockServerConfig.FRENCH_LANGUAGE_CODE.toString(), code: 'en', nameEn: 'French', nameFr: 'Français' },
+      ];
+
+      const mockLanguageDtoMapper = mock<LanguageDtoMapper>();
+      const service = new DefaultLanguageService(mockLanguageDtoMapper, mockServerConfig);
+
+      const dtos = service.listLanguages();
+
+      expect(dtos).toEqual(mockDtos);
+    });
+  });
+
+  describe('getLanguage', () => {
+    it('fetches language by id', () => {
+      const id = '1';
+      const mockDto: LanguageDto = { id: mockServerConfig.ENGLISH_LANGUAGE_CODE.toString(), code: 'en', nameEn: 'English', nameFr: 'Anglais' };
+      const mockLanguageDtoMapper = mock<LanguageDtoMapper>();
+      const service = new DefaultLanguageService(mockLanguageDtoMapper, mockServerConfig);
+
+      const dto = service.getLanguageById(id);
+
+      expect(dto).toEqual(mockDto);
+    });
+
+    it('fetches language by id throws not found exception', () => {
+      const id = '999';
+
+      const mockLanguageDtoMapper = mock<LanguageDtoMapper>();
+
+      const service = new DefaultLanguageService(mockLanguageDtoMapper, mockServerConfig);
+
+      expect(() => service.getLanguageById(id)).toThrow(LanguageNotFoundException);
+    });
+  });
+
+  describe('listAndSortLocalizedLanguages', () => {
+    it('fetches and sorts localized languages with Canada first', () => {
+      const mockMappedLanguageLocalizedDtos: LanguageLocalizedDto[] = [
+        { id: '1', code: 'en', name: 'English' },
+        { id: '2', code: 'fr', name: 'French' },
+      ];
+
+      const expectedLanguageLocalizedDtos: LanguageLocalizedDto[] = [
+        { id: '1', code: 'en', name: 'English' },
+        { id: '2', code: 'fr', name: 'French' },
+      ];
+
+      const mockLanguageDtoMapper = mock<LanguageDtoMapper>();
+      mockLanguageDtoMapper.mapLanguageDtosToLanguageLocalizedDtos.mockReturnValueOnce(mockMappedLanguageLocalizedDtos);
+
+      const service = new DefaultLanguageService(mockLanguageDtoMapper, mockServerConfig);
+
+      const dtos = service.listAndSortLocalizedLanguages('en');
+
+      expect(dtos).toStrictEqual(expectedLanguageLocalizedDtos);
+      expect(mockLanguageDtoMapper.mapLanguageDtosToLanguageLocalizedDtos).toHaveBeenCalledOnce();
+    });
+  });
+
+  describe('getLocalizedLanguageById', () => {
+    it('fetches localized language by id', () => {
+      const id = '1';
+      const mockDto: LanguageLocalizedDto = { id: '1', code: 'en', name: 'English' };
+      const mockLanguageDtoMapper = mock<LanguageDtoMapper>();
+      mockLanguageDtoMapper.mapLanguageDtoToLanguageLocalizedDto.mockReturnValueOnce(mockDto);
+      const service = new DefaultLanguageService(mockLanguageDtoMapper, mockServerConfig);
+
+      const dto = service.getLocalizedLanguageById(id, 'en');
+
+      expect(dto).toEqual(mockDto);
+      expect(mockLanguageDtoMapper.mapLanguageDtoToLanguageLocalizedDto).toHaveBeenCalledOnce();
+    });
+
+    it('fetches localized language by id throws not found exception', () => {
+      const id = '1033';
+      const mockLanguageDtoMapper = mock<LanguageDtoMapper>();
+      const service = new DefaultLanguageService(mockLanguageDtoMapper, mockServerConfig);
+
+      expect(() => service.getLocalizedLanguageById(id, 'en')).toThrow(LanguageNotFoundException);
+      expect(mockLanguageDtoMapper.mapLanguageDtoToLanguageLocalizedDto).not.toHaveBeenCalled();
+    });
+  });
+});
--- frontend/app/.server/domain/services/index.ts
+++ frontend/app/.server/domain/services/index.ts
@@ -10,6 +10,7 @@ export * from './client-friendly-status.service';
 export * from './country.service';
 export * from './demographic-survey.service';
 export * from './federal-government-insurance-plan.service';
+export * from './language.service';
 export * from './letter-type.service';
 export * from './letter.service';
 export * from './province-territory-state.service';
