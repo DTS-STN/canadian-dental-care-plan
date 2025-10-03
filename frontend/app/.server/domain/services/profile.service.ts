6c6ca6a79138f34357e9c2838144d7bcbe3ab245
Frank <48450599+Fbasham@users.noreply.github.com>
2025-10-03 12:21:20 -0400

    refactor(frontend) add updatePhoneNumber to profile-service (#4097)

--- frontend/app/.server/domain/services/profile.service.ts
+++ frontend/app/.server/domain/services/profile.service.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'inversify';
 
 import { TYPES } from '~/.server/constants';
-import type { CommunicationPreferenceDto } from '~/.server/domain/dtos';
+import type { CommunicationPreferenceRequestDto, PhoneNumberRequestDto } from '~/.server/domain/dtos';
 import type { ProfileRepository } from '~/.server/domain/repositories';
 import type { AuditService } from '~/.server/domain/services';
 import type { Logger } from '~/.server/logging';
@@ -14,7 +14,15 @@ export interface ProfileService {
    * @param communicationPreferenceDto The communication preference dto
    * @returns A Promise that resolves when the update is complete
    */
-  updateCommunicationPreferences(communicationPreferenceDto: CommunicationPreferenceDto): Promise<void>;
+  updateCommunicationPreferences(communicationPreferenceDto: CommunicationPreferenceRequestDto): Promise<void>;
+
+  /**
+   * Updates phone numbers for a user in the protected route.
+   *
+   * @param phoneNumberDto The phone number dto
+   * @returns A Promise that resolves when the update is complete
+   */
+  updatePhoneNumbers(phoneNumberDto: PhoneNumberRequestDto): Promise<void>;
 }
 
 @injectable()
@@ -32,11 +40,19 @@ export class DefaultProfileService implements ProfileService {
     this.log.debug('DefaultProfileService initiated.');
   }
 
-  async updateCommunicationPreferences(communicationPreferenceDto: CommunicationPreferenceDto): Promise<void> {
+  async updateCommunicationPreferences(communicationPreferenceDto: CommunicationPreferenceRequestDto): Promise<void> {
     this.log.trace('Updating communication preferences for request [%j]', communicationPreferenceDto);
 
     await this.profileRepository.updateCommunicationPreferences(communicationPreferenceDto);
 
     this.log.trace('Successfully updated communication preferences');
+  }
+
+  async updatePhoneNumbers(phoneNumberDto: PhoneNumberRequestDto): Promise<void> {
+    this.log.trace('Updating phone numbers for request [%j]', phoneNumberDto);
+
+    await this.profileRepository.updatePhoneNumbers(phoneNumberDto);
+
+    this.log.trace('Successfully updated phone numbers');
   }
 }
