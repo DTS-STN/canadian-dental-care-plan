cda10f1691cd0bb4010a1b83f266a908ab81182b
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-10-23 11:13:12 -0300

    feat(frontend): Display full addresses in contact information page

--- frontend/app/routes/protected/profile/contact-information.tsx
+++ frontend/app/routes/protected/profile/contact-information.tsx
@@ -3,7 +3,9 @@ import { useTranslation } from 'react-i18next';
 import type { Route } from './+types/contact-information';
 
 import { TYPES } from '~/.server/constants';
-import { getFixedT } from '~/.server/utils/locale.utils';
+import { getFixedT, getLocale } from '~/.server/utils/locale.utils';
+import { Address } from '~/components/address';
+import type { AddressDetails } from '~/components/address';
 import { ButtonLink } from '~/components/buttons';
 import { DescriptionListItem } from '~/components/description-list-item';
 import { InlineLink } from '~/components/inline-link';
@@ -29,23 +31,50 @@ export async function loader({ context: { appContainer, session }, params, reque
   const meta = { title: t('gcweb:meta.title.msca-template', { title: t('protected-profile:contact-information.page-title') }) };
   const { SCCH_BASE_URI } = appContainer.get(TYPES.ClientConfig);
 
+  const locale = getLocale(request);
+
   const idToken = session.get('idToken');
   appContainer.get(TYPES.AuditService).createAudit('page-view.profile.contact-information', { userId: idToken.sub });
+
+  const provinceTerritoryStateService = appContainer.get(TYPES.ProvinceTerritoryStateService);
+  const countryService = appContainer.get(TYPES.CountryService);
+
+  const mailingAddressProvince = clientApplication.contactInformation.mailingProvince ? await provinceTerritoryStateService.getLocalizedProvinceTerritoryStateById(clientApplication.contactInformation.mailingProvince, locale) : undefined;
+  const mailingAddressCountry = clientApplication.contactInformation.mailingCountry ? await countryService.getLocalizedCountryById(clientApplication.contactInformation.mailingCountry, locale) : undefined;
+  const mailingAddressDetails: AddressDetails = {
+    address: clientApplication.contactInformation.mailingAddress,
+    apartment: clientApplication.contactInformation.mailingApartment,
+    city: clientApplication.contactInformation.mailingCity,
+    provinceState: mailingAddressProvince?.name,
+    postalZipCode: clientApplication.contactInformation.mailingPostalCode,
+    country: mailingAddressCountry?.name ?? '',
+  };
+
+  const homeAddressProvince = clientApplication.contactInformation.homeProvince ? await provinceTerritoryStateService.getLocalizedProvinceTerritoryStateById(clientApplication.contactInformation.homeProvince, locale) : undefined;
+  const homeAddressCountry = clientApplication.contactInformation.homeCountry ? await countryService.getLocalizedCountryById(clientApplication.contactInformation.homeCountry, locale) : undefined;
+  const homeAddressDetails: AddressDetails = {
+    address: clientApplication.contactInformation.homeAddress ?? '',
+    apartment: clientApplication.contactInformation.homeApartment,
+    city: clientApplication.contactInformation.homeCity ?? '',
+    provinceState: homeAddressProvince?.name,
+    postalZipCode: clientApplication.contactInformation.homePostalCode,
+    country: homeAddressCountry?.name ?? '',
+  };
 
   return {
     meta,
     phoneNumber: clientApplication.contactInformation.phoneNumber,
     altPhoneNumber: clientApplication.contactInformation.phoneNumberAlt,
     emailAddress: clientApplication.contactInformation.email,
-    mailingAddress: clientApplication.contactInformation.mailingAddress,
-    homeAddress: clientApplication.contactInformation.homeAddress,
+    mailingAddressDetails,
+    homeAddressDetails,
     SCCH_BASE_URI,
   };
 }
 
 export default function ViewContactInformation({ loaderData, params }: Route.ComponentProps) {
   const { t } = useTranslation(handle.i18nNamespaces);
-  const { phoneNumber, altPhoneNumber, emailAddress, mailingAddress, homeAddress, SCCH_BASE_URI } = loaderData;
+  const { phoneNumber, altPhoneNumber, emailAddress, mailingAddressDetails, homeAddressDetails, SCCH_BASE_URI } = loaderData;
 
   return (
     <div className="max-w-prose space-y-10">
@@ -70,7 +99,7 @@ export default function ViewContactInformation({ loaderData, params }: Route.Com
           </div>
         </DescriptionListItem>
         <DescriptionListItem term={t('protected-profile:contact-information.mailing-address')}>
-          <p>{mailingAddress}</p>
+          <Address address={mailingAddressDetails} />
           <div className="mt-4 sm:mt-6">
             <InlineLink id="update-contact-information-mailing-address" routeId="protected/profile/contact/mailing-address" params={params}>
               {t('protected-profile:contact-information.update-mailing-address-link-text')}
@@ -78,7 +107,7 @@ export default function ViewContactInformation({ loaderData, params }: Route.Com
           </div>
         </DescriptionListItem>
         <DescriptionListItem term={t('protected-profile:contact-information.home-address')}>
-          <p>{homeAddress}</p>
+          <Address address={homeAddressDetails} />
           <div className="mt-4 sm:mt-6">
             <InlineLink id="update-contact-information-home-address" routeId="protected/profile/contact/home-address" params={params}>
               {t('protected-profile:contact-information.update-home-address-link-text')}
