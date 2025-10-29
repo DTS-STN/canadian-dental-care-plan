02f2d2f3e81b93bd71773f158967adfc2e7430d1
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2025-10-29 12:03:29 -0400

    Added transformAdobeAnalyticsUrl

---
+++ frontend/app/route-helpers/protected-profile-route-helper.ts
@@ -0,0 +1,15 @@
+import { removePathSegment } from '~/utils/url-utils';
+
+/**
+ * Adobe Analytics needs us to clean up URLs before sending event data. This ensures their reports focus
+ * on the core content, not things like tracking codes, because they don't want those to mess up their
+ * website visitor categories.
+ * @param url
+ * @returns
+ */
+export function transformAdobeAnalyticsUrl(url: string | URL) {
+  const urlObj = new URL(url);
+  const protectedRenewRouteRegex = /^\/(en|fr)\/(protected|protege)\/(profile|profil)\//i;
+  if (!protectedRenewRouteRegex.test(urlObj.pathname)) return urlObj;
+  return new URL(removePathSegment(urlObj, 3));
+}
