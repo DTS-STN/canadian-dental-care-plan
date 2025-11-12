c7414c46933bbf341646c2027f40e907e6907aa1
Shawn Zhang <azuneko1110@gmail.com>
2025-11-12 14:08:37 -0500

    feat(frontend): Member eligibility dto (#4281)
    * Member eligibility dto
    
    * update eligibility dto
    
    * update
    
    * update dto, remove env
    
    * remove env
    
    * remove eligibility from profile domain
    
    * rename client eligibility

---
+++ frontend/app/.server/domain/dtos/client-eligibility.dto.ts
@@ -0,0 +1,10 @@
+export type ClientEligibilityDto = Readonly<{
+  clientId: string;
+  clientNumber: string;
+  firstName: string;
+  lastName: string;
+  earnings: ReadonlyArray<{
+    taxationYear: string;
+    isEligible: boolean;
+  }>;
+}>;
---
+++ frontend/app/.server/domain/entities/client-eligibility.entity.ts
@@ -0,0 +1,55 @@
+import type { ReadonlyDeep } from 'type-fest';
+
+export type ClientEligibilityEntity = ReadonlyDeep<{
+  BenefitApplication: {
+    Applicant: {
+      ApplicantDetail: {
+        PrivateDentalInsuranceIndicator: boolean;
+      };
+      ApplicantEarning: Array<{
+        BenefitApplicationYearIdentification: Array<{
+          IdentificationID: string;
+        }>;
+        BenefitEligibilityStatus: {
+          StatusCode: {
+            ReferenceDataId: string;
+          };
+        };
+        Coverage: Array<{
+          CoverageCategoryCode: {
+            ReferenceDataName: string;
+            CoverageTierCode: {
+              ReferenceDataId: string;
+            };
+          };
+        }>;
+        EarningIdentification: Array<{
+          IdentificationID: string;
+          IdentificationCategoryText: 'Earning ID';
+        }>;
+        EarningTaxationYear: {
+          YearDate: string;
+        };
+      }>;
+      ApplicantCategoryCode: { ReferenceDataID: string };
+      ApplicantEnrollmentStatus: {
+        StatusCode: { ReferenceDataID: string };
+      };
+      BenefitEligibilityStatus: {
+        StatusCode: { ReferenceDataID: string };
+      };
+      BenefitEligibilityNextYearStatus: {
+        StatusCode: { ReferenceDataID: string };
+      };
+      BenefitApplicationYearIdentification: [{ IdentificationID: string }];
+      ClientIdentification: Array<{
+        IdentificationID: string;
+        IdentificationCategoryText: 'Client Number' | 'Client ID';
+      }>;
+      PersonName: Array<{
+        PersonGivenName: Array<string>;
+        PersonSurName: string;
+      }>;
+    };
+  };
+}>;
