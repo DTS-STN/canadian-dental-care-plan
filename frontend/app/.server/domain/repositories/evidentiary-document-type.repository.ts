4eee9a6a47f4962ffba90adc3f3693551d5dd738
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2025-11-19 21:25:21 -0400

    fix(gitops): esdc_evidentiarydocumenttypes url $filter param

--- frontend/app/.server/domain/repositories/evidentiary-document-type.repository.ts
+++ frontend/app/.server/domain/repositories/evidentiary-document-type.repository.ts
@@ -64,7 +64,7 @@ export class DefaultEvidentiaryDocumentTypeRepository implements EvidentiaryDocu
 
     const url = new URL(`${this.baseUrl}/esdc_evidentiarydocumenttypes`);
     url.searchParams.set('$select', 'esdc_value,esdc_nameenglish,esdc_namefrench');
-    url.searchParams.set('$filter', 'esdc_displayonportal eq 1 and statecode eq 0');
+    url.searchParams.set('$filter', 'statecode eq 0');
     const response = await this.httpClient.instrumentedFetch('http.client.interop-api.evidentiary-document-types.gets', url, {
       method: 'GET',
       headers: {
