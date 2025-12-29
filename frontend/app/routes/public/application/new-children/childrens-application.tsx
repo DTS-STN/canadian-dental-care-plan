ac8e43e86ed5d9ccfc962d095ed347f987a6de86
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2025-12-29 04:09:13 -0500

    Added add/remove child buttons

--- frontend/app/routes/public/application/new-children/childrens-application.tsx
+++ frontend/app/routes/public/application/new-children/childrens-application.tsx
@@ -1,25 +1,37 @@
+import type { SyntheticEvent } from 'react';
+
+import { redirect, useFetcher } from 'react-router';
+
+import { invariant } from '@dts-stn/invariant';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { randomUUID } from 'node:crypto';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
 
 import type { Route } from './+types/childrens-application';
 
 import { TYPES } from '~/.server/constants';
-import { getPublicApplicationState, validateApplicationTypeAndFlow } from '~/.server/routes/helpers/public-application-route-helpers';
+import { getPublicApplicationState, savePublicApplicationState, validateApplicationTypeAndFlow } from '~/.server/routes/helpers/public-application-route-helpers';
 import { getFixedT, getLocale } from '~/.server/utils/locale.utils';
-import { ButtonLink } from '~/components/buttons';
+import { Button, ButtonLink } from '~/components/buttons';
 import { Card, CardAction, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/card';
+import { CsrfTokenInput } from '~/components/csrf-token-input';
 import { DescriptionListItem } from '~/components/description-list-item';
 import { NavigationButtonLink } from '~/components/navigation-buttons';
 import { ProgressStepper } from '~/components/progress-stepper';
 import { StatusTag } from '~/components/status-tag';
+import { useCurrentLanguage } from '~/hooks';
 import { useProgressStepper } from '~/hooks/use-progress-stepper';
 import { pageIds } from '~/page-ids';
 import { parseDateString, toLocaleDateString } from '~/utils/date-utils';
 import { getTypedI18nNamespaces } from '~/utils/locale-utils';
 import { mergeMeta } from '~/utils/meta-utils';
+import { getPathById } from '~/utils/route-utils';
 import type { RouteHandleData } from '~/utils/route-utils';
 import { getTitleMetaTags } from '~/utils/seo-utils';
 import { formatSin } from '~/utils/sin-utils';
+
+const FORM_ACTION = { add: 'add', remove: 'remove' } as const;
 
 export const handle = {
   i18nNamespaces: getTypedI18nNamespaces('application-new-child', 'application', 'gcweb'),
@@ -50,7 +62,7 @@ export async function loader({ context: { appContainer, session }, request, para
           ? await provincialGovernmentInsurancePlanService.getLocalizedProvincialGovernmentInsurancePlanById(child.dentalBenefits.provincialTerritorialSocialProgram, locale)
           : undefined;
 
-      return { ...child, dentalBenefits: { ...child.dentalBenefits, federalSocialProgram: federalGovernmentInsurancePlanProgram?.name, provincialTerritorialSocialProgram: provincialTerritorialSocialProgram?.name } };
+      return { ...child, dentalBenefits: child.dentalBenefits ? { ...child.dentalBenefits, federalSocialProgram: federalGovernmentInsurancePlanProgram?.name, provincialTerritorialSocialProgram: provincialTerritorialSocialProgram?.name } : undefined };
     }),
   );
 
@@ -62,10 +74,66 @@ export async function loader({ context: { appContainer, session }, request, para
   };
 }
 
+export async function action({ context: { appContainer, session }, params, request }: Route.ActionArgs) {
+  const state = getPublicApplicationState({ params, session });
+  validateApplicationTypeAndFlow(state, params, ['new-children']);
+
+  const formData = await request.formData();
+
+  const securityHandler = appContainer.get(TYPES.SecurityHandler);
+  securityHandler.validateCsrfToken({ formData, session });
+
+  const formAction = z.enum(FORM_ACTION).parse(formData.get('_action'));
+
+  if (formAction === FORM_ACTION.add) {
+    const childId = randomUUID();
+    const children = [...state.children, { id: childId }];
+
+    savePublicApplicationState({
+      params,
+      session,
+      state: {
+        children: children,
+      },
+    });
+  }
+
+  if (formAction === FORM_ACTION.remove) {
+    const removeChildId = formData.get('childId');
+    const children = [...state.children].filter((child) => child.id !== removeChildId);
+
+    savePublicApplicationState({
+      params,
+      session,
+      state: {
+        children: children,
+      },
+    });
+  }
+
+  return redirect(getPathById(`public/application/$id/${state.typeOfApplication}-${state.typeOfApplicationFlow}/childrens-application`, params));
+}
+
 export default function NewChildChildrensApplication({ loaderData, params }: Route.ComponentProps) {
+  const { currentLanguage } = useCurrentLanguage();
   const { state } = loaderData;
   const { t } = useTranslation(handle.i18nNamespaces);
   const { steps, currentStep } = useProgressStepper('new-children', 'childrens-application');
+
+  const fetcher = useFetcher<typeof action>();
+  const isSubmitting = fetcher.state !== 'idle';
+
+  async function handleSubmit(event: SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const submitter = event.nativeEvent.submitter as HTMLButtonElement | null;
+    invariant(submitter, 'Expected submitter to be defined');
+    formData.append(submitter.name, submitter.value);
+
+    await fetcher.submit(formData, { method: 'POST' });
+  }
 
   return (
     <div className="max-w-prose space-y-8">
@@ -80,12 +148,11 @@ export default function NewChildChildrensApplication({ loaderData, params }: Rou
           { id: 'child-dental-benefits', completed: child.dentalBenefits !== undefined },
         ] as const;
         const completedSections = sections.filter((section) => section.completed).map((section) => section.id);
-        //const allSectionsCompleted = completedSections.length === sections.length;
 
         return (
           <div key={child.id}>
             <h2 className="font-lato mb-4 text-2xl font-bold">{t('application-new-child:childrens-application.child-title', { childNumber: index })}</h2>
-            <div className="space-y-4">
+            <div className="my-2 space-y-4">
               <p>{t('application:required-label')}</p>
               <p>{t('application:sections-completed', { number: completedSections.length, count: sections.length })}</p>
             </div>
@@ -109,7 +176,7 @@ export default function NewChildChildrensApplication({ loaderData, params }: Rou
                       <p>{dateOfBirth}</p>
                     </DescriptionListItem>
                     <DescriptionListItem term={t('application-new-child:childrens-application.sin-title')}>
-                      <p>{child.information?.socialInsuranceNumber ? formatSin(child.information.socialInsuranceNumber) : ''}</p>
+                      <p>{child.information.socialInsuranceNumber ? formatSin(child.information.socialInsuranceNumber) : ''}</p>
                     </DescriptionListItem>
                     <DescriptionListItem term={t('application-new-child:childrens-application.parent-guardian-title')}>
                       <p>{child.information.isParent ? t('application-new-child:childrens-application.yes') : t('application-new-child:childrens-application.no')}</p>
@@ -118,8 +185,8 @@ export default function NewChildChildrensApplication({ loaderData, params }: Rou
                 )}
               </CardContent>
               <CardFooter className="border-t bg-zinc-100">
-                <ButtonLink id="edit-button" variant="link" className="p-0" routeId="public/application/$id/child-information" params={params} startIcon={faCirclePlus} size="lg">
-                  {child.information === undefined ? t('application-new-child:childrens-application.add-child-information') : t('application-new-child:childrens-application.edit-child-information')}
+                <ButtonLink id="edit-button" variant="link" className="p-0" routeId="public/application/$id/children/$childId/information" params={{ ...params, childId: child.id }} startIcon={faCirclePlus} size="lg">
+                  {child.information === undefined ? t('application-new-child:childrens-application.add-child-information') : t('application-new-child:childrens-application.edit-child-information', { childNumber: index })}
                 </ButtonLink>
               </CardFooter>
             </Card>
@@ -142,7 +209,7 @@ export default function NewChildChildrensApplication({ loaderData, params }: Rou
               </CardContent>
               <CardFooter className="border-t bg-zinc-100">
                 <ButtonLink id="edit-button" variant="link" className="p-0" routeId="public/application/$id/dental-insurance" params={params} startIcon={faCirclePlus} size="lg">
-                  {child.dentalInsurance === undefined ? t('application-new-child:childrens-application.add-child-dental-insurance') : t('application-new-child:childrens-application.edit-child-dental-insurance')}
+                  {child.dentalInsurance === undefined ? t('application-new-child:childrens-application.add-child-dental-insurance') : t('application-new-child:childrens-application.edit-child-dental-insurance', { childNumber: index })}
                 </ButtonLink>
               </CardFooter>
             </Card>
@@ -178,16 +245,38 @@ export default function NewChildChildrensApplication({ loaderData, params }: Rou
               </CardContent>
               <CardFooter className="border-t bg-zinc-100">
                 <ButtonLink id="edit-button" variant="link" className="p-0" routeId="public/application/$id/federal-provincial-territorial-benefits" params={params} startIcon={faCirclePlus} size="lg">
-                  {child.dentalBenefits === undefined ? t('application-new-child:childrens-application.add-child-dental-benefits') : t('application-new-child:childrens-application.edit-child-dental-benefits')}
+                  {child.dentalBenefits === undefined ? t('application-new-child:childrens-application.add-child-dental-benefits') : t('application-new-child:childrens-application.edit-child-dental-benefits', { childNumber: index })}
                 </ButtonLink>
               </CardFooter>
             </Card>
+            <fetcher.Form method="post" onSubmit={handleSubmit} noValidate>
+              <CsrfTokenInput />
+              <input type="hidden" name="childId" value={child.id} />
+              <Button
+                id="remove-child"
+                className="my-5"
+                name="_action"
+                value={FORM_ACTION.remove}
+                disabled={isSubmitting}
+                variant="secondary"
+                size="sm"
+                data-gc-analytics-customclick="ESDC-EDSC:CDCP Online Application Form-Child:Remove child - Child(ren) application click"
+              >
+                {t('application-new-child:childrens-application.remove-child')}
+              </Button>
+            </fetcher.Form>
           </div>
         );
       })}
+      <fetcher.Form method="post" onSubmit={handleSubmit} noValidate>
+        <CsrfTokenInput />
+        <Button variant="primary" id="add-child" name="_action" value={FORM_ACTION.add} disabled={isSubmitting} data-gc-analytics-customclick="ESDC-EDSC:CDCP Online Application Form-Child:Add child - Child(ren) application click">
+          {t('application-new-child:childrens-application.add-child')}
+        </Button>
+      </fetcher.Form>
 
       <div className="flex flex-row-reverse flex-wrap items-center justify-end gap-3">
-        <NavigationButtonLink /*disabled={!allSectionsCompleted}*/ variant="primary" direction="next" routeId="public/application/$id/new-children/submit" params={params}>
+        <NavigationButtonLink variant="primary" direction="next" routeId="public/application/$id/new-children/submit" params={params}>
           {t('application-new-child:childrens-application.submit-btn')}
         </NavigationButtonLink>
         <NavigationButtonLink variant="secondary" direction="previous" routeId="public/application/$id/new-children/parent-or-guardian" params={params}>
