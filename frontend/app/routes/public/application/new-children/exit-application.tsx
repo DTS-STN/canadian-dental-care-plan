1bc5ebd4f14813799ddc37cfd88daa8b6c4e03a0
Frank Basham <frankabasham@gmail.com>
2026-01-12 07:42:55 -0500

    feat(frontend) add public-application-child-route-helpers.ts

--- frontend/app/routes/public/application/new-children/exit-application.tsx
+++ frontend/app/routes/public/application/new-children/exit-application.tsx
@@ -6,7 +6,8 @@ import { useTranslation } from 'react-i18next';
 import type { Route } from './+types/exit-application';
 
 import { TYPES } from '~/.server/constants';
-import { clearPublicApplicationState, getPublicApplicationState, validateApplicationTypeAndFlow } from '~/.server/routes/helpers/public-application-route-helpers';
+import { loadPublicApplicationChildState } from '~/.server/routes/helpers/public-application-child-route-helpers';
+import { clearPublicApplicationState, validateApplicationTypeAndFlow } from '~/.server/routes/helpers/public-application-route-helpers';
 import { getFixedT } from '~/.server/utils/locale.utils';
 import { ButtonLink } from '~/components/buttons';
 import { CsrfTokenInput } from '~/components/csrf-token-input';
@@ -26,7 +27,7 @@ export const handle = {
 export const meta: Route.MetaFunction = mergeMeta(({ loaderData }) => getTitleMetaTags(loaderData.meta.title));
 
 export async function loader({ context: { appContainer, session }, params, request }: Route.LoaderArgs) {
-  const state = getPublicApplicationState({ params, session });
+  const state = loadPublicApplicationChildState({ params, request, session });
   validateApplicationTypeAndFlow(state, params, ['new-children']);
 
   const t = await getFixedT(request, handle.i18nNamespaces);
@@ -35,7 +36,7 @@ export async function loader({ context: { appContainer, session }, params, reque
 }
 
 export async function action({ context: { appContainer, session }, params, request }: Route.ActionArgs) {
-  const state = getPublicApplicationState({ params, session });
+  const state = loadPublicApplicationChildState({ params, request, session });
   validateApplicationTypeAndFlow(state, params, ['new-children']);
 
   const formData = await request.formData();
--- frontend/app/routes/public/application/new-children/submit.tsx
+++ frontend/app/routes/public/application/new-children/submit.tsx
@@ -7,7 +7,8 @@ import { z } from 'zod';
 import type { Route } from './+types/submit';
 
 import { TYPES } from '~/.server/constants';
-import { getPublicApplicationState, savePublicApplicationState, validateApplicationTypeAndFlow } from '~/.server/routes/helpers/public-application-route-helpers';
+import { loadPublicApplicationChildStateForReview } from '~/.server/routes/helpers/public-application-child-route-helpers';
+import { savePublicApplicationState, validateApplicationTypeAndFlow } from '~/.server/routes/helpers/public-application-route-helpers';
 import type { ApplyAdultState } from '~/.server/routes/mappers';
 import { getFixedT } from '~/.server/utils/locale.utils';
 import { transformFlattenedError } from '~/.server/utils/zod.utils';
@@ -39,7 +40,7 @@ export const handle = {
 export const meta: Route.MetaFunction = mergeMeta(({ loaderData }) => getTitleMetaTags(loaderData.meta.title));
 
 export async function loader({ context: { appContainer, session }, request, params }: Route.LoaderArgs) {
-  const state = getPublicApplicationState({ params, session });
+  const state = loadPublicApplicationChildStateForReview({ params, request, session });
   validateApplicationTypeAndFlow(state, params, ['new-children']);
 
   const t = await getFixedT(request, handle.i18nNamespaces);
@@ -47,7 +48,7 @@ export async function loader({ context: { appContainer, session }, request, para
 
   const children = [];
   for (const child of state.children) {
-    children.push(`${child.information?.firstName} ${child.information?.lastName}`);
+    children.push(`${child.information.firstName} ${child.information.lastName}`);
   }
 
   return {
@@ -61,7 +62,7 @@ export async function loader({ context: { appContainer, session }, request, para
 }
 
 export async function action({ context: { appContainer, session }, request, params }: Route.ActionArgs) {
-  const state = getPublicApplicationState({ params, session });
+  const state = loadPublicApplicationChildStateForReview({ params, request, session });
   validateApplicationTypeAndFlow(state, params, ['new-children']);
 
   const formData = await request.formData();
