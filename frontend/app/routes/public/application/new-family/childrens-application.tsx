41b43338c7723d365c372ddc54cfd3d0ec11086b
Frank <48450599+Fbasham@users.noreply.github.com>
2026-01-23 13:49:15 -0500

    refactor(frontend) childrens-application hub fixes (#4594)

--- frontend/app/routes/public/application/new-family/childrens-application.tsx
+++ frontend/app/routes/public/application/new-family/childrens-application.tsx
@@ -151,7 +151,7 @@ export default function NewFamilyChildrensApplication({ loaderData, params }: Ro
     await fetcher.submit(formData, { method: 'POST' });
   }
 
-  const allChildrenCompleted = state.children.every((child) => child.information !== undefined && child.dentalInsurance !== undefined && child.dentalBenefits !== undefined);
+  const allChildrenCompleted = state.children.length > 0 && state.children.every((child) => child.information !== undefined && child.dentalInsurance !== undefined && child.dentalBenefits !== undefined);
 
   return (
     <div className="max-w-prose space-y-8">
