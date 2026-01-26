1d2c9c5f1fe7f2a4738d90fce47803ac5f2cbc1e
Frank <48450599+Fbasham@users.noreply.github.com>
2026-01-26 10:31:38 -0500

    refactor(frontend) update renew-children flow (#4613)

--- frontend/app/routes/public/application/renew-children/submit.tsx
+++ frontend/app/routes/public/application/renew-children/submit.tsx
@@ -89,8 +89,8 @@ export async function action({ context: { appContainer, session }, request, para
     return data({ errors: transformFlattenedError(z.flattenError(parsedDataResult.error)) }, { status: 400 });
   }
 
-  const benefitApplicationDto = appContainer.get(TYPES.HubSpokeBenefitApplicationStateMapper).mapApplicationChildrenStateToBenefitApplicationDto(state);
-  const confirmationCode = await appContainer.get(TYPES.BenefitApplicationService).createBenefitApplication(benefitApplicationDto);
+  const benefitApplicationDto = appContainer.get(TYPES.HubSpokeBenefitRenewalStateMapper).mapBenefitRenewalChildStateToChildBenefitRenewalDto(state);
+  const confirmationCode = await appContainer.get(TYPES.BenefitRenewalService).createChildBenefitRenewal(benefitApplicationDto);
   const submissionInfo = { confirmationCode, submittedOn: new UTCDate().toISOString() };
   savePublicApplicationState({ params, session, state: { submitTerms: parsedDataResult.data, submissionInfo } });
 
