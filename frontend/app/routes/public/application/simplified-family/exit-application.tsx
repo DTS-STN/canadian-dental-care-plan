261c88a8958a56fad21c7447b0429fbb6c95df38
Shawn Zhang <azuneko1110@gmail.com>
2026-01-27 13:33:24 -0500

    feat(frontend): Simplified family submit (#4638)

--- frontend/app/routes/public/application/simplified-family/exit-application.tsx
+++ frontend/app/routes/public/application/simplified-family/exit-application.tsx
@@ -1,1 +1,85 @@
-export default function Route() {}
+import { redirect, useFetcher } from 'react-router';
+
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
+
+import type { Route } from './+types/exit-application';
+
+import { TYPES } from '~/.server/constants';
+import { clearPublicApplicationState, validateApplicationFlow } from '~/.server/routes/helpers/public-application-route-helpers';
+import { loadPublicApplicationSimplifiedFamilyState } from '~/.server/routes/helpers/public-application-simplified-family-route-helpers';
+import { getFixedT } from '~/.server/utils/locale.utils';
+import { ButtonLink } from '~/components/buttons';
+import { CsrfTokenInput } from '~/components/csrf-token-input';
+import { LoadingButton } from '~/components/loading-button';
+import { pageIds } from '~/page-ids';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { mergeMeta } from '~/utils/meta-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+import { getTitleMetaTags } from '~/utils/seo-utils';
+
+export const handle = {
+  i18nNamespaces: getTypedI18nNamespaces('application-simplified-family', 'application', 'gcweb'),
+  pageIdentifier: pageIds.public.application.renewFamily.exitApplication,
+  pageTitleI18nKey: 'application-simplified-family:exit-application.page-title',
+} as const satisfies RouteHandleData;
+
+export const meta: Route.MetaFunction = mergeMeta(({ loaderData }) => getTitleMetaTags(loaderData.meta.title));
+
+export async function loader({ context: { appContainer, session }, params, request }: Route.LoaderArgs) {
+  const state = loadPublicApplicationSimplifiedFamilyState({ params, request, session });
+  validateApplicationFlow(state, params, ['simplified-family']);
+
+  const t = await getFixedT(request, handle.i18nNamespaces);
+  const meta = { title: t('gcweb:meta.title.template', { title: t('application-simplified-family:exit-application.page-title') }) };
+  return { meta };
+}
+
+export async function action({ context: { appContainer, session }, params, request }: Route.ActionArgs) {
+  const state = loadPublicApplicationSimplifiedFamilyState({ params, request, session });
+  validateApplicationFlow(state, params, ['simplified-family']);
+
+  const formData = await request.formData();
+
+  const securityHandler = appContainer.get(TYPES.SecurityHandler);
+  securityHandler.validateCsrfToken({ formData, session });
+
+  const t = await getFixedT(request, handle.i18nNamespaces);
+
+  clearPublicApplicationState({ params, session });
+
+  return redirect(t('application-simplified-family:exit-application.exit-link'));
+}
+
+export default function SimplifiedFamilyExitApplication({ loaderData, params }: Route.ComponentProps) {
+  const { t } = useTranslation(handle.i18nNamespaces);
+
+  const fetcher = useFetcher<typeof action>();
+  const isSubmitting = fetcher.state !== 'idle';
+
+  return (
+    <div className="max-w-prose">
+      <div className="mb-8 space-y-4">
+        <p>{t('application-simplified-family:exit-application.are-you-sure')}</p>
+        <p>{t('application-simplified-family:exit-application.click-back')}</p>
+      </div>
+      <fetcher.Form method="post" noValidate className="flex flex-wrap items-center gap-3">
+        <CsrfTokenInput />
+        <ButtonLink
+          id="back-button"
+          variant="secondary"
+          routeId="public/application/$id/simplified-family/submit"
+          params={params}
+          disabled={isSubmitting}
+          startIcon={faChevronLeft}
+          data-gc-analytics-customclick="ESDC-EDSC:CDCP Online Application Form-Family:Back - Exiting the application click"
+        >
+          {t('application-simplified-family:exit-application.back-btn')}
+        </ButtonLink>
+        <LoadingButton variant="primary" loading={isSubmitting} data-gc-analytics-customclick="ESDC-EDSC:CDCP Online Application Form-Family:Exit - Exiting the application click">
+          {t('application-simplified-family:exit-application.exit-btn')}
+        </LoadingButton>
+      </fetcher.Form>
+    </div>
+  );
+}
