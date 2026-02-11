bad5c6ee6015fd9f933fc22f7166202d0184d8ca
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2026-02-11 16:55:50 -0400

    feat(frontend): add helpers for protected application routes

---
+++ frontend/app/.server/routes/helpers/protected-application-simplified-family-route-helpers.ts
@@ -0,0 +1,243 @@
+import { redirect } from 'react-router';
+
+import { createLogger } from '~/.server/logging';
+import { applicantInformationStateHasPartner, getAgeCategoryFromDateString, getChildrenState, getProtectedApplicationState } from '~/.server/routes/helpers/protected-application-route-helpers';
+import type { ApplicationStateParams, ChildrenState, ProtectedApplicationState } from '~/.server/routes/helpers/protected-application-route-helpers';
+import { getEnv } from '~/.server/utils/env.utils';
+import type { Session } from '~/.server/web/session';
+import { getPathById } from '~/utils/route-utils';
+
+interface LoadProtectedApplicationSimplifiedFamilyStateArgs {
+  params: ApplicationStateParams;
+  request: Request;
+  session: Session;
+}
+
+/**
+ * Loads protected application simplified family state.
+ * @param args - The arguments.
+ * @returns The loaded protected simplified family state.
+ */
+export function loadProtectedApplicationSimplifiedFamilyState({ params, request, session }: LoadProtectedApplicationSimplifiedFamilyStateArgs) {
+  const log = createLogger('protected-application-simplified-family-route-helpers/loadProtectedApplicationSimplifiedFamilyState');
+  const { pathname } = new URL(request.url);
+  const applicationState = getProtectedApplicationState({ params, session });
+
+  if (applicationState.inputModel !== 'simplified' || applicationState.typeOfApplication !== 'family') {
+    throw redirect(getPathById('protected/application/$id/type-application', params));
+  }
+
+  // Redirect to the confirmation page if the application has been submitted and
+  // the current route is not the confirmation page.
+  const confirmationRouteUrl = getPathById('protected/application/$id/simplified-family/confirmation', params);
+  if (applicationState.submissionInfo && !pathname.endsWith(confirmationRouteUrl)) {
+    log.warn('Redirecting user to "%s" since the application has been submitted; sessionId: [%s], ', confirmationRouteUrl, applicationState.id);
+    throw redirect(confirmationRouteUrl);
+  }
+
+  // Redirect to the first flow page if the application has not been submitted and
+  // the current route is the confirmation page.
+  const typeOfApplicationRouteUrl = getPathById('protected/application/$id/type-of-application', params);
+  if (!applicationState.submissionInfo && pathname.endsWith(confirmationRouteUrl)) {
+    log.warn('Redirecting user to "%s" since the application has not been submitted; sessionId: [%s], ', typeOfApplicationRouteUrl, applicationState.id);
+    throw redirect(typeOfApplicationRouteUrl);
+  }
+
+  return applicationState;
+}
+
+interface LoadProtectedApplicationSimplifiedFamilyStateForReviewArgs {
+  params: ApplicationStateParams;
+  request: Request;
+  session: Session;
+}
+
+/**
+ * Loads the simplified family state for the review page. It validates the state and throws a redirect if invalid.
+ * @param args - The arguments.
+ * @returns The validated family state.
+ */
+export function loadProtectedApplicationSimplifiedFamilyStateForReview({ params, request, session }: LoadProtectedApplicationSimplifiedFamilyStateForReviewArgs) {
+  const state = loadProtectedApplicationSimplifiedFamilyState({ params, request, session });
+  return validateProtectedApplicationFamilyStateForReview({ params, state });
+}
+
+interface ValidateProtectedApplicationFamilyStateForReviewArgs {
+  params: ApplicationStateParams;
+  state: ProtectedApplicationState;
+}
+
+export function validateProtectedApplicationFamilyStateForReview({ params, state }: ValidateProtectedApplicationFamilyStateForReviewArgs) {
+  const {
+    applicantInformation,
+    applicationYear,
+    context,
+    clientApplication,
+    communicationPreferences,
+    phoneNumber,
+    dentalBenefits,
+    dentalInsurance,
+    email,
+    emailVerified,
+    hasFiledTaxes,
+    homeAddress,
+    id,
+    isHomeAddressSameAsMailingAddress,
+    lastUpdatedOn,
+    livingIndependently,
+    mailingAddress,
+    maritalStatus,
+    partnerInformation,
+    submissionInfo,
+    termsAndConditions,
+    inputModel,
+    typeOfApplication,
+  } = state;
+
+  const { COMMUNICATION_METHOD_SUNLIFE_EMAIL_ID, COMMUNICATION_METHOD_GC_DIGITAL_ID } = getEnv();
+
+  if (clientApplication === undefined) {
+    throw redirect(getPathById('protected/application/$id/type-of-application', params));
+  }
+
+  if (termsAndConditions === undefined) {
+    throw redirect(getPathById('protected/application/$id/eligibility-requirements', params));
+  }
+
+  if (inputModel !== 'simplified') {
+    throw redirect(getPathById('protected/application/$id/type-of-application', params));
+  }
+
+  if (typeOfApplication !== 'family') {
+    throw redirect(getPathById('protected/application/$id/type-of-application', params));
+  }
+
+  if (hasFiledTaxes === undefined) {
+    throw redirect(getPathById('protected/application/$id/eligibility-requirements', params));
+  }
+
+  if (hasFiledTaxes === false) {
+    throw redirect(getPathById('protected/application/$id/eligibility-requirements', params));
+  }
+
+  if (applicantInformation === undefined) {
+    throw redirect(getPathById('protected/application/$id/type-of-application', params));
+  }
+
+  const ageCategory = getAgeCategoryFromDateString(applicantInformation.dateOfBirth);
+
+  if (ageCategory === 'children') {
+    throw redirect(getPathById('protected/application/$id/type-of-application', params));
+  }
+
+  if (applicantInformationStateHasPartner(maritalStatus) && !partnerInformation) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/marital-status', params));
+  }
+
+  if (!applicantInformationStateHasPartner(maritalStatus) && partnerInformation) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/marital-status', params));
+  }
+
+  if (phoneNumber === undefined) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/contact-information', params));
+  }
+
+  if (mailingAddress === undefined) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/contact-information', params));
+  }
+
+  if (communicationPreferences === undefined) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/contact-information', params));
+  }
+
+  if ((communicationPreferences.value?.preferredMethod === COMMUNICATION_METHOD_SUNLIFE_EMAIL_ID || communicationPreferences.value?.preferredMethod === COMMUNICATION_METHOD_GC_DIGITAL_ID) && !emailVerified) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/contact-information', params));
+  }
+
+  if (dentalInsurance === undefined) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/dental-insurance', params));
+  }
+
+  if (dentalBenefits === undefined) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/dental-insurance', params));
+  }
+
+  const children = validateChildrenStateForReview({ childrenState: state.children, params });
+
+  return {
+    ageCategory,
+    applicantInformation,
+    applicationYear,
+    clientApplication,
+    children,
+    context,
+    communicationPreferences,
+    phoneNumber,
+    dentalBenefits,
+    dentalInsurance,
+    email,
+    emailVerified,
+    hasFiledTaxes,
+    homeAddress,
+    id,
+    isHomeAddressSameAsMailingAddress,
+    lastUpdatedOn,
+    livingIndependently,
+    mailingAddress,
+    maritalStatus,
+    partnerInformation,
+    submissionInfo,
+    termsAndConditions,
+    inputModel,
+    typeOfApplication,
+  };
+}
+
+interface ValidateChildrenStateForReviewArgs {
+  childrenState: ChildrenState;
+  params: ApplicationStateParams;
+}
+
+function validateChildrenStateForReview({ childrenState, params }: ValidateChildrenStateForReviewArgs) {
+  const children = getChildrenState({ children: childrenState });
+
+  if (children.length === 0) {
+    throw redirect(getPathById('protected/application/$id/simplified-family/childrens-application', params));
+  }
+
+  return children.map(({ id, dentalBenefits, dentalInsurance, information }) => {
+    if (information === undefined) {
+      throw redirect(getPathById('protected/application/$id/simplified-family/childrens-application', params));
+    }
+
+    if (information.dateOfBirth === '') {
+      throw redirect(getPathById('protected/application/$id/simplified-family/childrens-application', params));
+    }
+
+    if (!information.isParent) {
+      throw redirect(getPathById('protected/application/$id/simplified-family/childrens-application', params));
+    }
+
+    const ageCategory = getAgeCategoryFromDateString(information.dateOfBirth);
+
+    if (ageCategory === 'adults' || ageCategory === 'seniors') {
+      throw redirect(getPathById('protected/application/$id/type-of-application', params));
+    }
+
+    if (dentalInsurance === undefined) {
+      throw redirect(getPathById('protected/application/$id/simplified-family/childrens-application', params));
+    }
+
+    if (dentalBenefits === undefined) {
+      throw redirect(getPathById('protected/application/$id/simplified-family/childrens-application', params));
+    }
+
+    return {
+      ageCategory,
+      id,
+      dentalBenefits,
+      dentalInsurance,
+      information,
+    };
+  });
+}
