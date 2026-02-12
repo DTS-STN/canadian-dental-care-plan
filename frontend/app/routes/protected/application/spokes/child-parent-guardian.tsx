6aa5a19e5bf70486d03a65df2a43c0f4fbd68ac9
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2026-02-11 19:55:18 -0500

    Created protected child parent guardian spoke page

--- frontend/app/routes/protected/application/spokes/child-parent-guardian.tsx
+++ frontend/app/routes/protected/application/spokes/child-parent-guardian.tsx
@@ -1,1 +1,88 @@
-export default function ChildParentGuardian() {}
+import { redirect, useFetcher } from 'react-router';
+
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
+
+import type { Route } from './+types/child-parent-guardian';
+
+import { TYPES } from '~/.server/constants';
+import { clearProtectedApplicationState, getProtectedApplicationState } from '~/.server/routes/helpers/protected-application-route-helpers';
+import { getFixedT } from '~/.server/utils/locale.utils';
+import { ButtonLink } from '~/components/buttons';
+import { CsrfTokenInput } from '~/components/csrf-token-input';
+import { LoadingButton } from '~/components/loading-button';
+import { pageIds } from '~/page-ids';
+import { getTypedI18nNamespaces } from '~/utils/locale-utils';
+import { mergeMeta } from '~/utils/meta-utils';
+import type { RouteHandleData } from '~/utils/route-utils';
+import { getTitleMetaTags } from '~/utils/seo-utils';
+
+export const handle = {
+  i18nNamespaces: getTypedI18nNamespaces('protected-application-spokes', 'protected-application', 'gcweb'),
+  pageIdentifier: pageIds.protected.application.spokes.childParentOrGuardian,
+  pageTitleI18nKey: 'protected-application-spokes:children.parent-or-guardian.page-title',
+} as const satisfies RouteHandleData;
+
+export const meta: Route.MetaFunction = mergeMeta(({ loaderData }) => getTitleMetaTags(loaderData.meta.title));
+
+export async function loader({ context: { appContainer, session }, params, request }: Route.LoaderArgs) {
+  getProtectedApplicationState({ params, session });
+  const t = await getFixedT(request, handle.i18nNamespaces);
+
+  const meta = { title: t('gcweb:meta.title.template', { title: t('protected-application-spokes:children.parent-or-guardian.page-title') }) };
+
+  return { meta };
+}
+
+export async function action({ context: { appContainer, session }, params, request }: Route.ActionArgs) {
+  const formData = await request.formData();
+
+  const securityHandler = appContainer.get(TYPES.SecurityHandler);
+  securityHandler.validateCsrfToken({ formData, session });
+
+  getProtectedApplicationState({ params, session });
+
+  const t = await getFixedT(request, handle.i18nNamespaces);
+
+  clearProtectedApplicationState({ params, session });
+  return redirect(t('protected-application-spokes:children.parent-or-guardian.exit-btn-link'));
+}
+
+export default function ChildParentGuardian({ loaderData, params }: Route.ComponentProps) {
+  const { t } = useTranslation(handle.i18nNamespaces);
+
+  const fetcher = useFetcher<typeof action>();
+  const isSubmitting = fetcher.state !== 'idle';
+
+  async function handleSubmit(event: React.SyntheticEvent<HTMLFormElement, SubmitEvent>) {
+    event.preventDefault();
+    await fetcher.submit(event.currentTarget, { method: 'POST' });
+    sessionStorage.removeItem('flow.state');
+  }
+
+  return (
+    <>
+      <div className="mb-8 space-y-4">
+        <p>{t('protected-application-spokes:children.parent-or-guardian.must-be')}</p>
+        <p>{t('protected-application-spokes:children.parent-or-guardian.unable-to-apply')}</p>
+      </div>
+      <fetcher.Form method="post" onSubmit={handleSubmit} noValidate className="flex flex-wrap items-center gap-3">
+        <CsrfTokenInput />
+        <ButtonLink
+          id="back-button"
+          variant="secondary"
+          routeId="protected/application/$id/children/$childId/information"
+          params={params}
+          disabled={isSubmitting}
+          startIcon={faChevronLeft}
+          data-gc-analytics-customclick="ESDC-EDSC:CDCP Protected Application Form-Child:Back - Child parent or guardian needs to apply click"
+        >
+          {t('protected-application-spokes:children.parent-or-guardian.back-btn')}
+        </ButtonLink>
+        <LoadingButton type="submit" variant="primary" loading={isSubmitting} data-gc-analytics-customclick="ESDC-EDSC:CDCP Protected Application Form-Child:Exit - Child parent or guardian needs to apply click">
+          {t('protected-application-spokes:children.parent-or-guardian.exit-btn')}
+        </LoadingButton>
+      </fetcher.Form>
+    </>
+  );
+}
