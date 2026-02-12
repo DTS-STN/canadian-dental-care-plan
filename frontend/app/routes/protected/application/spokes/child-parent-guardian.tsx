55c605cb8f3f8d8758e53940c0687c6162633239
AdamP-HRSDC-RHDCC <adam.pcolinsky@hrsdc-rhdcc.gc.ca>
2026-02-12 10:40:19 -0500

    Added validateAuthSession

--- frontend/app/routes/protected/application/spokes/child-parent-guardian.tsx
+++ frontend/app/routes/protected/application/spokes/child-parent-guardian.tsx
@@ -26,6 +26,9 @@ export const handle = {
 export const meta: Route.MetaFunction = mergeMeta(({ loaderData }) => getTitleMetaTags(loaderData.meta.title));
 
 export async function loader({ context: { appContainer, session }, params, request }: Route.LoaderArgs) {
+  const securityHandler = appContainer.get(TYPES.SecurityHandler);
+  await securityHandler.validateAuthSession({ request, session });
+
   getProtectedApplicationState({ params, session });
   const t = await getFixedT(request, handle.i18nNamespaces);
 
@@ -35,9 +38,9 @@ export async function loader({ context: { appContainer, session }, params, reque
 }
 
 export async function action({ context: { appContainer, session }, params, request }: Route.ActionArgs) {
-  const formData = await request.formData();
-
   const securityHandler = appContainer.get(TYPES.SecurityHandler);
+  await securityHandler.validateAuthSession({ request, session });
+  const formData = await request.formData();
   securityHandler.validateCsrfToken({ formData, session });
 
   getProtectedApplicationState({ params, session });
