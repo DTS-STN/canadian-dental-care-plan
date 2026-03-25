f763d0679b3f9461b8551e9c81f25e90111619f7
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2026-03-25 16:01:30 -0300

    test(frontend): move mocks for react-i18next and logging to the top

--- frontend/__tests__/setup-test-env.ts
+++ frontend/__tests__/setup-test-env.ts
@@ -1,20 +1,10 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 
-import { afterEach, beforeEach, vi } from 'vitest';
+import { afterEach, vi } from 'vitest';
 
-/**
- * Jest has their globals API enabled by default. Vitest does not. We can either enable globals via the globals
- * configuration setting or update your code to use imports from the vitest module instead.
- *
- * We decided to keep globals disabled.
- * @see https://vitest.dev/guide/migration.html#globals-as-a-default
- */
-
-beforeEach(() => {
-  vi.mock('react-i18next');
-  vi.mock('~/.server/logging');
-});
+vi.mock('react-i18next');
+vi.mock('~/.server/logging');
 
 afterEach(() => {
   cleanup();
