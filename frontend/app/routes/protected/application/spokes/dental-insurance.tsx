8c2a82fb503de5a9203f7dd0bac5d0250bb00431
Shawn Zhang <azuneko1110@gmail.com>
2026-03-26 11:01:56 -0400

    fix(frontend): a11y - adjust heading level on dental insurance spoke page (#5085)
    * adjust heading level on dental insurance spoke page

--- frontend/app/routes/protected/application/spokes/dental-insurance.tsx
+++ frontend/app/routes/protected/application/spokes/dental-insurance.tsx
@@ -191,7 +191,7 @@ export default function ApplicationSpokeDentalInsurance({ loaderData, params }: 
           {hasDentalInsurance === true && (
             <div className="mb-4 space-y-4">
               <ContextualAlert type="info">
-                <h3 className="font-lato mb-2 text-xl font-semibold">{t('dental-insurance.yes.alert-title')}</h3>
+                <h2 className="font-lato mb-2 text-xl font-semibold">{t('dental-insurance.yes.alert-title')}</h2>
                 <p>{t('dental-insurance.yes.alert-body')}</p>
               </ContextualAlert>
               <InputCheckbox
@@ -209,7 +209,7 @@ export default function ApplicationSpokeDentalInsurance({ loaderData, params }: 
           {hasDentalInsurance === false && (
             <div className="mb-4 space-y-4">
               <ContextualAlert type="info">
-                <h3 className="font-lato mb-2 text-xl font-semibold">{t('dental-insurance.no.alert-title')}</h3>
+                <h2 className="font-lato mb-2 text-xl font-semibold">{t('dental-insurance.no.alert-title')}</h2>
                 <Trans ns={handle.i18nNamespaces} i18nKey="protected-application-spokes:dental-insurance.no.alert-body" components={{ t4Href, t4aHref }} />
               </ContextualAlert>
               <InputCheckbox
