474a35dfd96828cb9bb9509a535d7e202536f7e0
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2026-04-30 13:52:19 -0300

    test(frontend): fix expected output order in class name merging test

--- frontend/__tests__/utils/tw-utils.test.ts
+++ frontend/__tests__/utils/tw-utils.test.ts
@@ -10,7 +10,7 @@ describe('cn function tests', () => {
 
   it('should merge class names correctly', () => {
     const result = cn('bg-red hover:bg-dark-red px-2 py-1', 'bg-[#B91C1C] p-3');
-    expect(result).toEqual('hover:bg-dark-red p-3 bg-[#B91C1C]');
+    expect(result).toEqual('hover:bg-dark-red bg-[#B91C1C] p-3');
   });
 
   it('should handle arrays of class names', () => {
